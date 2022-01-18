ac4c1a4f9fdac0007a10cb594f3c5063eb962c55
ANSYS\spearson <sean.pearson@ansys.com>
2022-01-18 14:52:24 +0000

    parametric

--- ansys/fluent/addons/parametric/ParametricStudy.py
+++ ansys/fluent/addons/parametric/ParametricStudy.py
@@ -3,42 +3,112 @@
 ###   
 
 
-class ParametricStudy(object):
+class DesignPointStatus(object):
+    pass
 
-    def __init__(self, launcher, case_file_name, design_point_count):
-        self.__launcher = launcher
-        self.__case_file_name = case_file_name
-        self.__design_points = design_point_count * [(dict(), dict())]
 
-    def update_all(self):
-        for design_point in self.__design_points:
-            self.__run_design_point(design_point)
-            
-    def update_design_point(self, design_point):
-        self.__run_design_point(design_point)
+class DesignPoint(object):
 
-    def set_parameter(self, design_point_idx, name, value):
-        self.__design_points[design_point_idx][0][name] = value
+    def __init__(self, name: str, base_design_point=None):
+        self.__name = name
+        if base_design_point:
+            self.__inputs = base_design_point.inputs().copy()
+            self.__outputs = base_design_point.outputs().copy()
+        else:
+            self.__inputs = dict()
+            self.__outputs = dict()
+        self.__status = DesignPointStatus()
+
+    def name(self):
+        return self.__name
+
+    def inputs(self):
+        return self.__inputs
         
-    def input_parameter(self, design_point_idx, name):
-        return self.__parameter(design_point_idx, True, name)
+    def outputs(self):
+        return self.__outputs
         
-    def output_parameter(self, design_point_idx, name):
-        return self.__parameter(design_point_idx, False, name)
+    def set_input(self, parameter_name, value):
+        self.__inputs[parameter_name] = value
 
-    # can be static
-    def __run_design_point(self, design_point):
+    def update_outputs(self, outputs):
+        self.__outputs = outputs
+
+
+class DesignPointTable(list):
+
+    def __init__(self, base_design_point_name: str):
+        self.append(DesignPoint(base_design_point_name))
+
+    def add_design_point(self, design_point_name: str) -> DesignPoint:
+        self.append(DesignPoint(design_point_name, self[0]))
+        return self[-1]
+        
+    def find_design_point(self, idx_or_name) -> DesignPoint:
+        if isinstance(idx_or_name, int):
+            return self[idx_or_name]
+        for design_point in self:
+            if idx_or_name == design_point.name():
+                return design_point
+        raise RuntimeError("Design point not found: " + repr(idx_or_name))
+
+
+class ParametricSession(object):
+
+    def __init__(self, fluent_session):
+        self.__fluent_session = fluent_session
+
+    def input_parameter_table(self):
+        return dict()
+        
+    def output_parameter_table(self):
+        return dict()
+
+    def set_input_parameter(self, parameter_name: str, value):
+        self.__fluent_session.tui.define.parameters.input_parameters.edit(parameter_name, parameter_name, value)
+
+    def initialize_with_case(self, case_file_name: str):
+        self.__fluent_session.tui.file.read_case(case_file_name=case_file_name)
+
+    def update(self):
+        self.__fluent_session.tui.solve.initialize.initialize_flow()
+        self.__fluent_session.tui.solve.iterate()
+        
+    def __del__(self):
+        self.__fluent_session.exit()
+
+class FluentLauncher(object):
+
+    def __call__(self):
         import ansys.fluent.solver as pyfluent
         session = pyfluent.launch_fluent()
-        session.tui.file.read_case(case_file_name=self.__case_file_name)
-        edit = session.tui.define.parameters.input_parameters.edit
-        for name, value in design_point[0].items():
-            edit(name, name, value)
-        session.tui.solve.initialize.initialize_flow()
-        session.tui.solve.iterate()
-        #query parameter values here
-        session.exit()
+        parametric_session = ParametricSession(fluent_session=session)
+        return parametric_session
 
-    def __parameter(self, design_point_idx, is_input, name):
-        return self.__design_points[design_point_idx][0 if is_input else 1][name]
-        
+
+class ParametricStudy(object):
+
+    def __init__(self, case_file_name: str, base_design_point_name: str = "Base DP", launcher = FluentLauncher()):
+        self.__session = launcher()
+        self.__session.initialize_with_case(case_file_name)
+        self.__design_point_table = DesignPointTable(base_design_point_name)
+
+    def update_all(self):
+        for design_point in self.__design_point_table:
+            self.update_design_point(design_point)
+
+    def update_design_point(self, design_point):
+        for parameter_name, value in design_point.inputs().items():
+            self.__session.set_input_parameter(parameter_name, value)
+        self.__session.update()
+        outputs = self.__session.output_parameter_table()
+        design_point.update_outputs(outputs.copy())
+
+    def add_design_point(self, design_point_name: str) -> DesignPoint:
+        return self.__design_point_table.add_design_point(design_point_name)
+
+    def set_input_parameter(self, design_point_idx_or_name, parameter_name, value):
+        self.__design_point(design_point_idx_or_name).set_input(parameter_name, value)
+
+    def __design_point(self, idx_or_name) -> DesignPoint:
+        return self.__design_point_table.find_design_point(idx_or_name)
