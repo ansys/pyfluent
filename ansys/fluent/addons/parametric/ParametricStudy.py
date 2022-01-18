d7e5464dc4b050729d832ada5b56a200e1a5cee5
ANSYS\spearson <sean.pearson@ansys.com>
2022-01-18 17:23:56 +0000

    parameteric

--- ansys/fluent/addons/parametric/ParametricStudy.py
+++ ansys/fluent/addons/parametric/ParametricStudy.py
@@ -2,9 +2,14 @@
 ### Copyright 1987-2022 ANSYS, Inc. All Rights Reserved.
 ###   
 
+from enum import Enum
 
-class DesignPointStatus(object):
-    pass
+class DesignPointStatus(Enum):
+    needs_update = 1
+    updating = 2
+    updated = 3
+    failed = 4
+    do_not_update = 5
 
 
 class DesignPoint(object):
@@ -17,7 +22,7 @@ class DesignPoint(object):
         else:
             self.__inputs = dict()
             self.__outputs = dict()
-        self.__status = DesignPointStatus()
+        self.__status = DesignPointStatus.needs_update
 
     def name(self):
         return self.__name
@@ -30,6 +35,9 @@ class DesignPoint(object):
         
     def set_input(self, parameter_name, value):
         self.__inputs[parameter_name] = value
+        
+    def update_intputs(self, inputs):
+        self.__inputs = inputs
 
     def update_outputs(self, outputs):
         self.__outputs = outputs
@@ -37,8 +45,8 @@ class DesignPoint(object):
 
 class DesignPointTable(list):
 
-    def __init__(self, base_design_point_name: str):
-        self.append(DesignPoint(base_design_point_name))
+    def __init__(self, base_design_point: DesignPoint):
+        self.append(base_design_point)
 
     def add_design_point(self, design_point_name: str) -> DesignPoint:
         self.append(DesignPoint(design_point_name, self[0]))
@@ -53,16 +61,42 @@ class DesignPointTable(list):
         raise RuntimeError("Design point not found: " + repr(idx_or_name))
 
 
+class FluentParameterAccessor(object):
+
+    def __init__(self, fluent_session):
+        self.__list_parameters = fluent_session.tui.define.parameters.list_parameters
+        
+    def input_parameters(self) -> dict:
+        return self.__parameter_table_to_dict(self.__list_parameters.input_parameters())
+        
+    def output_parameters(self) -> dict:
+        return self.__parameter_table_to_dict(self.__list_parameters.output_parameters())
+    
+    # static
+    def __parameter_table_to_dict(self, table: str) -> dict:
+        print('table ', table)
+        data_lines = table.splitlines()[3:]
+        print('data_lines ', data_lines)
+        table_as_dict = {}
+        for line in data_lines:
+            print('line ', line)
+            line_as_list = line.split()
+            print('line_as_list ', line_as_list)
+            table_as_dict[line_as_list[0]] = line_as_list[1]
+        return table_as_dict
+            
+
 class ParametricSession(object):
 
     def __init__(self, fluent_session):
         self.__fluent_session = fluent_session
+        self.__parameter_accessor = FluentParameterAccessor(fluent_session)
 
-    def input_parameter_table(self):
-        return dict()
+    def input_parameters(self):
+        return self.__parameter_accessor.input_parameters()
         
-    def output_parameter_table(self):
-        return dict()
+    def output_parameters(self):
+        return self.__parameter_accessor.output_parameters()
 
     def set_input_parameter(self, parameter_name: str, value):
         self.__fluent_session.tui.define.parameters.input_parameters.edit(parameter_name, parameter_name, value)
@@ -91,7 +125,9 @@ class ParametricStudy(object):
     def __init__(self, case_file_name: str, base_design_point_name: str = "Base DP", launcher = FluentLauncher()):
         self.__session = launcher()
         self.__session.initialize_with_case(case_file_name)
-        self.__design_point_table = DesignPointTable(base_design_point_name)
+        base_design_point = DesignPoint(base_design_point_name)
+        base_design_point.update_intputs(self.__session.input_parameters().copy())
+        self.__design_point_table = DesignPointTable(base_design_point)
 
     def update_all(self):
         for design_point in self.__design_point_table:
@@ -101,14 +137,14 @@ class ParametricStudy(object):
         for parameter_name, value in design_point.inputs().items():
             self.__session.set_input_parameter(parameter_name, value)
         self.__session.update()
-        outputs = self.__session.output_parameter_table()
+        outputs = self.__session.output_parameters()
         design_point.update_outputs(outputs.copy())
 
     def add_design_point(self, design_point_name: str) -> DesignPoint:
         return self.__design_point_table.add_design_point(design_point_name)
 
     def set_input_parameter(self, design_point_idx_or_name, parameter_name, value):
-        self.__design_point(design_point_idx_or_name).set_input(parameter_name, value)
+        self.design_point(design_point_idx_or_name).set_input(parameter_name, value)
 
-    def __design_point(self, idx_or_name) -> DesignPoint:
+    def design_point(self, idx_or_name) -> DesignPoint:
         return self.__design_point_table.find_design_point(idx_or_name)
