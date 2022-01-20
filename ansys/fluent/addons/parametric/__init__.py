a34d05d34ac9f788e8f6616641942797bbc103e3
ANSYS\spearson <sean.pearson@ansys.com>
2022-01-20 13:06:54 +0000

    some formatting issues

--- ansys/fluent/addons/parametric/__init__.py
+++ ansys/fluent/addons/parametric/__init__.py
@@ -6,32 +6,46 @@ Classes for running a parametric study in Fluent.
 
 Example
 -------
-from ansys.fluent.addons.parametric import ParametricStudy, DesignPointStatus
-# instantiate the study, specifying the case
-study = ParametricStudy(case_file_name=my_case_file_name)
-# add one new design point and set an input parameter
-dp1 = study.add_design_point('DP1')
-dp1.set_input('parameter_1', 0.235)
-dp1.set_input('velocity_inlet_5_y_velocity', 0.772)
-# the solver has not been run yet so no ouputs are computed
-# the design point is out of date
-assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
-# get the base design point
-base = study.design_point('Base DP')
-# check that block updates works
-dp1.block_updates()
-base.block_updates()
-assert(dp1.status == DesignPointStatus.BLOCKED)
-study.update_all()
-assert(dp1.outputs == base.outputs)
-# update the design points
-dp1.allow_updates()
-base.allow_updates()
-assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
-study.update_all()
-# check that dp1 is up to date and that its outputs differ from the base design point
-assert(dp1.status == DesignPointStatus.UPDATED)
-assert(dp1.outputs != base.outputs)
+>>> from ansys.fluent.addons.parametric import ParametricStudy, DesignPointStatus
+
+Instantiate the study, specifying the case
+
+>>> study = ParametricStudy(case_file_name=my_case_file_name)
+
+Add one new design point and set an input parameter
+
+>>> dp1 = study.add_design_point('DP1')
+>>> dp1.set_input('parameter_1', 0.235)
+>>> dp1.set_input('velocity_inlet_5_y_velocity', 0.772)
+
+The solver has not been run yet so no ouputs are computed
+The design point is out of date
+
+>>> assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
+
+Get the base design point
+
+>>> base = study.design_point('Base DP')
+
+Check that block updates works
+
+>>> dp1.block_updates()
+>>> base.block_updates()
+>>> assert(dp1.status == DesignPointStatus.BLOCKED)
+>>> study.update_all()
+>>> assert(dp1.outputs == base.outputs)
+
+Update the design points
+
+>>> dp1.allow_updates()
+>>> base.allow_updates()
+>>> assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
+>>> study.update_all()
+
+Check that dp1 is up to date and that the outputs differ
+
+>>> assert(dp1.status == DesignPointStatus.UPDATED)
+>>> assert(dp1.outputs != base.outputs)
 """
 
 from enum import Enum
@@ -66,9 +80,11 @@ class DesignPoint:
     name : str
         Name of the design point as a str
     outputs : dict
-        Dict of output parameters (name of parameter to value).
+        Dict of output parameters 
+        (name of parameter to value).
     inputs : dict
-        Dict of input parameters (name of parameter to value).
+        Dict of input parameters 
+        (name of parameter to value).
     status : DesignPointStatus
         Current status of the design point.
 
@@ -171,7 +187,8 @@ class DesignPointTable(list):
 
 class FluentParameterAccessor:
     """
-    Extracts parameter name to value dicts from table strs currenty returned by the API
+    Extracts parameter name to value dicts from table strs 
+    currenty returned by the API
 
     Methods
     -------
@@ -182,7 +199,8 @@ class FluentParameterAccessor:
     """
 
     def __init__(self, fluent_session):
-        self.__list_parameters = fluent_session.tui.define.parameters.list_parameters
+        self.__list_parameters = \
+            fluent_session.tui.define.parameters.list_parameters
 
     def input_parameters(self) -> dict:
         return FluentParameterAccessor.__parameter_table_to_dict(
@@ -234,7 +252,8 @@ class ParametricSession:
             parameter_name, parameter_name, value)
 
     def initialize_with_case(self, case_file_name: str):
-        self.__fluent_session.tui.file.read_case(case_file_name=case_file_name)
+        self.__fluent_session.tui.file.read_case(
+            case_file_name=case_file_name)
 
     def update(self):
         self.__fluent_session.tui.solve.initialize.initialize_flow()
@@ -259,17 +278,19 @@ class FluentLauncher:
 
 class ParametricStudy:
     """
-    Parametric study that manages design points to parametrize a Fluent solver
-    set-up. Provides ability to run Fluent for a series of design points, and
-    access the inputs and outputs.
+    Parametric study that manages design points to parametrize a 
+    Fluent solver set-up. Provides ability to run Fluent for a series 
+    of design points, and access the inputs and outputs.
 
     Methods
     -------
     update_all()
-        Bring all design point outputs up to date by running the solver on each design point.
-        Ignores BLOCKED design points (not yet implemented).
+        Bring all design point outputs up to date by running the 
+        solver on each design point. Ignores BLOCKED design points 
+        (not yet implemented).
     update_design_point()
-        Bring the outputs of the specified design point up to date by running the solver.
+        Bring the outputs of the specified design point up to date 
+        by running the solver.
     add_design_point(design_point_name: str) -> DesignPoint
         Add a design point
     design_point(idx_or_name)
@@ -301,10 +322,12 @@ class ParametricStudy:
         for parameter_name, value in design_point.inputs.items():
             self.__session.set_input_parameter(parameter_name, value)
         self.__session.update()
-        design_point.on_end_updating(outputs=self.__session.output_parameters().copy())
+        design_point.on_end_updating(
+            outputs=self.__session.output_parameters().copy())
 
     def add_design_point(self, design_point_name: str) -> DesignPoint:
-        return self.__design_point_table.add_design_point(design_point_name)
+        return self.__design_point_table.add_design_point(
+            design_point_name)
 
     def design_point(self, idx_or_name) -> DesignPoint:
         return self.__design_point_table.find_design_point(idx_or_name)
