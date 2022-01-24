00b36fb023e4a5814195d6066f1c0cf1dfe7edad
Mainak Kundu <mainak.kundu@ansys.com>
2022-01-24 20:46:09 +0530

    execute-query rpc, python versioning and some formatting changes

--- ansys/fluent/core/core.py
+++ ansys/fluent/core/core.py
@@ -1,9 +1,6 @@
-###
-### Copyright 1987-2022 ANSYS, Inc. All Rights Reserved.
-###
-
-import os
 import keyword
+import os
+
 from ansys.api.fluent.v0 import datamodel_pb2 as DataModelProtoModule
 
 MODULE_NAME_ALIAS = "pyfluent"
@@ -47,39 +44,42 @@ def convert_gvalue_to_value(gval):
             val[k] = convert_gvalue_to_value(v)
         return val
 
+
 # import_ -> import
 # type_flag -> type?
-def convert_fname_to_tui_menu(fname : str):
-    if fname.endswith('_') and keyword.iskeyword(fname[:-1]):
+def convert_fname_to_tui_menu(fname: str):
+    if fname.endswith("_") and keyword.iskeyword(fname[:-1]):
         return fname[:-1]
     # TODO: Following is actually done in the server-side - consolidate
-    #if fname.endswith('_flag'):
+    # if fname.endswith('_flag'):
     #    return fname.rstrip('_flag') + '?'
     return fname
 
+
 # import -> import_
 # type? -> type_flag
-def convert_tui_menu_to_fname(menu : str):
+def convert_tui_menu_to_fname(menu: str):
     if keyword.iskeyword(menu):
-        return menu + '_'
-    if menu.endswith('?'):
-        return menu[:-1] + '_flag'
+        return menu + "_"
+    if menu.endswith("?"):
+        return menu[:-1] + "_flag"
     return menu
 
+
 def convert_path_to_grpc_path(path):
-    grpc_path = ''
+    grpc_path = ""
     for comp in path:
-        grpc_path += '/' + convert_fname_to_tui_menu(comp[0])
+        grpc_path += "/" + convert_fname_to_tui_menu(comp[0])
         if comp[1]:
-            grpc_path += ':' + comp[1]
+            grpc_path += ":" + comp[1]
     return grpc_path
 
+
 def extend_menu_path_to_command_path(path, command):
-    return path + '/' + command
+    return path + "/" + command
 
 
 class DatamodelService:
-
     def __init__(self, stub, password: str):
         self.stub = stub
         self.__password = password
@@ -89,7 +89,8 @@ class DatamodelService:
 
     def get_attribute_value(self, request):
         return self.stub.GetAttributeValue(
-            request, metadata=self.__get_metadata())
+            request, metadata=self.__get_metadata()
+        )
 
     def get_state(self, request):
         return self.stub.GetState(request, metadata=self.__get_metadata())
@@ -99,7 +100,11 @@ class DatamodelService:
 
     def execute_command(self, request):
         return self.stub.ExecuteCommand(
-            request, metadata=self.__get_metadata())
+            request, metadata=self.__get_metadata()
+        )
+
+    def execute_query(self, request):
+        return self.stub.ExecuteQuery(request, metadata=self.__get_metadata())
 
 
 def start_journal(filename: str):
@@ -107,8 +112,8 @@ def start_journal(filename: str):
     JOURNAL_FILENAME = filename
     if os.path.exists(filename):
         os.remove(filename)
-    with open(JOURNAL_FILENAME, 'w', encoding='utf8') as f:
-        f.write(f'import {__name__} as {MODULE_NAME_ALIAS}\n')
+    with open(JOURNAL_FILENAME, "w", encoding="utf8") as f:
+        f.write(f"import {__name__} as {MODULE_NAME_ALIAS}\n")
 
 
 def stop_journal():
@@ -117,11 +122,10 @@ def stop_journal():
 
 
 def read_journal(filename: str):
-    exec(open(filename, encoding='utf8').read())
+    exec(open(filename, encoding="utf8").read())
 
 
 class PyMenuJournaler:
-
     def __init__(self, path=None):
         self.pypath = ""
         if not path:
@@ -130,65 +134,68 @@ class PyMenuJournaler:
             if self.pypath:
                 self.pypath += "."
             if comp[1]:
-                self.pypath += f'{comp[0]}[{repr(comp[1])}]'
+                self.pypath += f"{comp[0]}[{repr(comp[1])}]"
             else:
                 self.pypath += comp[0]
 
     def __write_to_file(self, code):
         if not JOURNAL_FILENAME:
             return
-        with open(JOURNAL_FILENAME, 'a', encoding='utf8') as f:
+        with open(JOURNAL_FILENAME, "a", encoding="utf8") as f:
             f.write(code)
 
     def journal_set_state(self, state):
         self.__write_to_file(
-            f'{MODULE_NAME_ALIAS}.{self.pypath} = {repr(state)}\n')
+            f"{MODULE_NAME_ALIAS}.{self.pypath} = {repr(state)}\n"
+        )
 
     def journal_rename(self, new_name):
         self.__write_to_file(
-            f'{MODULE_NAME_ALIAS}.{self.pypath}.rename({repr(new_name)})\n')
+            f"{MODULE_NAME_ALIAS}.{self.pypath}.rename({repr(new_name)})\n"
+        )
 
     def journal_delete(self, child_name):
         self.__write_to_file(
-            f'del {MODULE_NAME_ALIAS}.{self.pypath}[{repr(child_name)}]\n')
+            f"del {MODULE_NAME_ALIAS}.{self.pypath}[{repr(child_name)}]\n"
+        )
 
     def journal_execute(self, args=None, kwargs=None):
-        self.__write_to_file(f'{MODULE_NAME_ALIAS}.{self.pypath}(')
+        self.__write_to_file(f"{MODULE_NAME_ALIAS}.{self.pypath}(")
         first = True
         if args is not None:
             for arg in args:
                 if not first:
-                    self.__write_to_file(', ')
+                    self.__write_to_file(", ")
                 else:
                     first = False
                 self.__write_to_file(repr(arg))
         if kwargs is not None:
             for k, v in kwargs.items():
                 if not first:
-                    self.__write_to_file(', ')
+                    self.__write_to_file(", ")
                 else:
                     first = False
-                self.__write_to_file(f'{k}={repr(v)}')
-        self.__write_to_file(')\n')
+                self.__write_to_file(f"{k}={repr(v)}")
+        self.__write_to_file(")\n")
 
     def journal_global_fn_call(self, func_name, args=None, kwargs=None):
-        self.__write_to_file(f'{MODULE_NAME_ALIAS}.{func_name}(')
+        self.__write_to_file(f"{MODULE_NAME_ALIAS}.{func_name}(")
         first = True
         if args is not None:
             for arg in args:
                 if not first:
-                    self.__write_to_file(', ')
+                    self.__write_to_file(", ")
                 else:
                     first = False
                 self.__write_to_file(repr(arg))
         if kwargs is not None:
             for k, v in kwargs.items():
                 if not first:
-                    self.__write_to_file(', ')
+                    self.__write_to_file(", ")
                 else:
                     first = False
-                self.__write_to_file(f'{k}={repr(v)}')
-        self.__write_to_file(')\n')
+                self.__write_to_file(f"{k}={repr(v)}")
+        self.__write_to_file(")\n")
 
 
 class PyMenu:
@@ -203,9 +210,9 @@ class PyMenu:
         request = DataModelProtoModule.GetAttributeValueRequest()
         request.path = path
         request.attribute = DataModelProtoModule.Attribute.CUSTOM
-        request.args['is_extended_tui'] = 1
+        request.args["is_extended_tui"] = 1
         if include_unavailable:
-            request.args['include_unavailable'] = 1
+            request.args["include_unavailable"] = 1
         response = self.service.get_attribute_value(request)
         return convert_gvalue_to_value(response.value)
 
@@ -214,17 +221,18 @@ class PyMenu:
         request.path = path
         request.attribute = DataModelProtoModule.Attribute.DATA_TYPE
         if include_unavailable:
-            request.args['include_unavailable'] = 1
+            request.args["include_unavailable"] = 1
         response = self.service.get_attribute_value(request)
-        return (convert_gvalue_to_value(response.value) ==
-                "NamedObjectContainer")
+        return (
+            convert_gvalue_to_value(response.value) == "NamedObjectContainer"
+        )
 
     def get_child_names(self, path, include_unavailable=False):
         request = DataModelProtoModule.GetAttributeValueRequest()
         request.path = path
         request.attribute = DataModelProtoModule.Attribute.CHILD_NAMES
         if include_unavailable:
-            request.args['include_unavailable'] = 1
+            request.args["include_unavailable"] = 1
         response = self.service.get_attribute_value(request)
         return convert_gvalue_to_value(response.value)
 
@@ -241,14 +249,23 @@ class PyMenu:
         ret = self.service.set_state(request)
         return ret
 
-    def execute(self, path, *args, **kwargs):
+    def execute(self, path: str, *args, **kwargs):
         request = DataModelProtoModule.ExecuteCommandRequest()
         request.path = path
         if kwargs:
             for k, v in kwargs.items():
                 convert_value_to_gvalue(v, request.args.fields[k])
         else:
-            convert_value_to_gvalue(args, request.args.fields['tui_args'])
+            convert_value_to_gvalue(args, request.args.fields["tui_args"])
+        if path.startswith("/query/"):
+            ret = self.service.execute_query(request)
+            return convert_gvalue_to_value(ret.result)
+        else:
+            ret = self.service.execute_command(request)
+            return PyMenu.ExecuteCommandResult(
+                convert_gvalue_to_value(ret.result)
+            )
+
         ret = self.service.execute_command(request)
         return PyMenu.ExecuteCommandResult(convert_gvalue_to_value(ret.result))
 
@@ -257,7 +274,7 @@ class PyMenu:
         request.path = path
         request.attribute = DataModelProtoModule.Attribute.HELP_STRING
         if include_unavailable:
-            request.args['include_unavailable'] = 1
+            request.args["include_unavailable"] = 1
         response = self.service.get_attribute_value(request)
         return convert_gvalue_to_value(response.value)
 
@@ -265,7 +282,8 @@ class PyMenu:
         request = DataModelProtoModule.SetStateRequest()
         request.path = path
         convert_value_to_gvalue(
-            new_name, request.state.struct_value.fields['name'])
+            new_name, request.state.struct_value.fields["name"]
+        )
         ret = self.service.set_state(request)
         return ret
 
@@ -280,9 +298,10 @@ class PyMenu:
         request = DataModelProtoModule.SetStateRequest()
         request.path = path
         convert_value_to_gvalue(value, request.state)
-        if request.state.HasField('null_value'): # creation with default value
+        if request.state.HasField("null_value"):  # creation with default value
             convert_value_to_gvalue(
-                name, request.state.struct_value.fields['name'])
+                name, request.state.struct_value.fields["name"]
+            )
         ret = self.service.set_state(request)
         return ret
 
--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 """Setup file for ansys-fluent-solver"""
 import os
-from setuptools import setup, find_namespace_packages
+
+from setuptools import find_namespace_packages, setup
 
 # Get version from version info
 __version__ = None
 THIS_FILE = os.path.dirname(__file__)
 VERSION_FILE = os.path.join(
-    THIS_FILE, "ansys", "fluent", "solver", "_version.py")
-with open(VERSION_FILE, mode='r', encoding='utf8') as fd:
+    THIS_FILE, "ansys", "fluent", "solver", "_version.py"
+)
+with open(VERSION_FILE, mode="r", encoding="utf8") as fd:
     # execute file from raw string
     exec(fd.read())
 
 install_requires = [
-    'grpcio>=1.30.0',
+    "grpcio>=1.30.0",
     #'ansys-api-fluent-v0>=0.0.1'
-    ]
+]
 
 packages = []
 for package in find_namespace_packages(include="ansys*"):
@@ -22,23 +24,23 @@ for package in find_namespace_packages(include="ansys*"):
         packages.append(package)
 
 setup(
-    name='ansys-fluent-solver',
+    name="ansys-fluent-solver",
     packages=packages,
     version=__version__,
     description="Fluent's SolverAPI exposed in Python",
-    long_description=open('README.rst', encoding='utf8').read(),
-    long_description_content_type='text/x-rst',
-    url='https://github.com/mkundu1/pyfluent',
-    license='MIT',
-    author='ANSYS, Inc.',
-    maintainer='Mainak Kundu',
-    maintainer_email='mainak.kundu@ansys.com',
+    long_description=open("README.rst", encoding="utf8").read(),
+    long_description_content_type="text/x-rst",
+    url="https://github.com/pyansys/pyfluent",
+    license="MIT",
+    author="ANSYS, Inc.",
+    maintainer="Mainak Kundu",
+    maintainer_email="mainak.kundu@ansys.com",
     install_requires=install_requires,
-    python_requires='>=3.5',
+    python_requires=">3.6",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
