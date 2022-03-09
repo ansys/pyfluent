53d1cf3118acdedae016d6db1716a0f9877a8f80
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2022-03-09 10:00:10 +0530

    Add a test for session creation (#133)

---
+++ tests/test_session.py
@@ -0,0 +1,28 @@
+from concurrent import futures
+
+import grpc
+
+from ansys.api.fluent.v0 import health_pb2, health_pb2_grpc
+from ansys.fluent.services.health_check import HealthCheckService
+from ansys.fluent.session import Session
+
+
+class MockHealthServicer(health_pb2_grpc.HealthServicer):
+    def Check(self, request, context):  # noqa N802
+        return health_pb2.HealthCheckResponse(
+            status=health_pb2.HealthCheckResponse.ServingStatus.SERVING
+        )
+
+
+def test_session():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    ip = "127.0.0.1"
+    port = 50051
+    server.add_insecure_port(f"{ip}:{port}")
+    health_pb2_grpc.add_HealthServicer_to_server(MockHealthServicer(), server)
+    server.start()
+    session = Session(ip=ip, port=port, cleanup_on_exit=False)
+    assert session.check_health() == HealthCheckService.Status.SERVING.name
+    server.stop(None)
+    session.exit()
+    assert session.check_health() == HealthCheckService.Status.NOT_SERVING.name
