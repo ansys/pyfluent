145e892fde177860a13bfb58c520db34274c4d0f
Dan Williams <dan.williams@ansys.com>
2022-03-22 09:31:23 -0400

    Add note about documentation builds to contributing guide

--- doc/make.bat
+++ doc/make.bat
@@ -33,7 +33,8 @@ if errorlevel 9009 (
 goto end
 
 :clean
-rmdir /s /q %BUILDDIR% > /NUL 2>&1 
+rmdir /s /q %BUILDDIR% > /NUL 2>&1
+rmdir /s /q %SOURCEDIR%\examples > /NUL 2>&1
 for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
 goto end
 
