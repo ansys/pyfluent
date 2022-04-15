b3ddd617d0642c7b70c8bd10f57be57c131e90a3
Aseem Jain <aseem.jain@ansys.com>
2022-04-15 21:10:23 +0530

    dash

--- src/ansys/fluent/core/utils/dash/widgets.py
+++ src/ansys/fluent/core/utils/dash/widgets.py
@@ -6,95 +6,66 @@ import dash_vtk
 from dash.dependencies import Input, Output, State
 
 from dash.exceptions import PreventUpdate
+import re
 
 
 class GraphicsWidget:
     _fun = None
     _exe_method = None
-    def __init__(self, app, obj, update_vtk_fun, vtk_view=None):
+    def __init__(self, app, obj, update_vtk_fun):
+        self.__refresh_bcs=[]
         self._app = app
-        self._object = obj
-        self._create_vtk_view = False
-        self._vtk_view_id = "vtk-view-"+self._object._name
-        if not vtk_view:
-            self._create_vtk_view = True
-            vtk_view = dash_vtk.View(
-                id=self._vtk_view_id,    
-                pickingModes=["hover"],
-                children=[       
-                ],
-            )        
-        self._vtk_view = vtk_view
-        self._all_widgets = {}        
-        self.__refresh_bcs=[] 
+        self._object = obj        
+        self._vtk_view_id = self.get_unique_name("vtk-view-")        
+        self._vtk_view = dash_vtk.View(
+            id=self._vtk_view_id,    
+            pickingModes=["hover"],
+            children=[       
+            ],
+        )        
         self._update_vtk_fun =  update_vtk_fun
+        self._all_widgets = {}                
         self.store_all_widgets(self._object)   
-        self._all_widgets["display"+self._object._name]= self.get_button("display"+self._object._name)               
-   
-    def update_widgets(self):
-        self.__widgets=[]   
-        self.populate_widgets(self._object)
-        self.__widgets.append(self.get_button("display"+self._object._name))  
-        
-        
-    def refresh(self):
-        print(list(self._all_widgets.keys())[:-1])
-        self.create_callback()  
-        self.update_widgets()   
-              
-        if self._create_vtk_view:
-            return dbc.Row(
-                                
-             [
-              dbc.Col(
-                html.Div(
-                html.Div(self._vtk_view, style={"height": "100%", "width": "100%"}),
-                style={"height": "100%", "width": "100%"},
-                ), md=10),        
-                dbc.Col(dbc.Card(children = self.__widgets, body=True, id=f"{self._object._name}-container"), md=2),#
-          
-              ],
-              style={"height": "50rem"},
-            )        
-        
-        else:
-            rv = dbc.Card(children = self.__widgets, body=True)
-        #print(rv)
-        return rv
-        
+        self._all_widgets[self.get_unique_name("display")]= self.get_button(self.get_unique_name("display")) 
+        self.create_callback()   
+         
+ 
+    def get_label(self, name):
+        name_list  = re.split('[^a-zA-Z]', name)
+        return " ".join([name.capitalize()  for name in name_list])
     
-    def _need_to_refresh(self):
-        return not self.__compare_dicts(self.__old_defn, self._object())
+    def get_unique_name(self, name):
+        return name+self._object._name 
+               
+    def refresh(self):                                     
+
+        return dbc.Row(
+                            
+         [
+          dbc.Col(
+            html.Div(
+            html.Div(self._vtk_view, style={"height": "100%", "width": "100%"}),
+            style={"height": "100%", "width": "100%"},
+            ), md=10),        
+            dbc.Col(dbc.Card(children = list(self._all_widgets.values()), body=True), md=2),#      
+          ],
+          style={"height": "50rem"},
+        )        
         
-    def __compare_dicts(self, old, new):
-        for key, value in new.items():
-          if not key in old:        
-              return False
-          if isinstance(value, dict):
-              rv = self.__compare_dicts(old[key], value)          
-              if not rv:
-                  return False
-        return True          
+       
               
     def create_callback(self):
 
         inputs =[Input(name+"data", "value") for name in list(self._all_widgets.keys())[:-1]] 
         outputs =[Output(name+"container", "style") for name in list(self._all_widgets.keys())[:-1]] 
-        outputs =outputs + [Output(name, "value") for name in list(self._all_widgets.keys())[:-1]] 
-        #inputs =[Input(name, "value") for name in ['fieldcontour-1', 'surfaces_listcontour-1',]]        
-        @self._app.callback(
-          #Output(f"{self._object._name}-container", "children"), 
+        outputs =outputs + [Output(name, "value") for name in list(self._all_widgets.keys())[:-1]]         
+        @self._app.callback(          
           *outputs,          
           *inputs                   
         )
-        def update_oject2(*args): 
-
-            #for cb in self.__refresh_bcs:
-            #    cb()
-            print('update_oject2')                                 
-            #if self._need_to_refresh():
+        def callback(*args): 
             self._visible_widgets = []
-            self.visible_widgets(self._object)
+            self.update_visible_widgets(self._object)
             visible_widgets = [pair[0] for pair in self._visible_widgets]
             widgets_value_map = {pair[0]:pair[1] for pair in self._visible_widgets}
             widget_values = []
@@ -107,20 +78,7 @@ class GraphicsWidget:
             return ([{'display': 'block'} if name in  visible_widgets else {'display': 'none'} for name in list(self._all_widgets.keys())[:-1]] +  
                   widget_values)
                                      
-            #else:
-            #   print('PreventUpdate...') 
-            #   raise PreventUpdate
-               
-        def update_oject2(*args): 
-            print('update_oject2')                                 
-            if self._need_to_refresh():
-               self.update_widgets() 
-               print('update_widgets...')               
-               #return []
-               return [self.__widgets] 
-            else:
-               print('PreventUpdate...') 
-               raise PreventUpdate               
+            
              
         
 
@@ -131,42 +89,13 @@ class GraphicsWidget:
             
             if value.__class__.__class__.__name__ in ("PyLocalPropertyMeta", "PyLocalObjectMeta"):               
                 if value.__class__.__class__.__name__ == "PyLocalPropertyMeta":
-                    widget =  self.get_widget(value, value._type, name+self._object._name, getattr(value, "attributes", None) )
-                    #if widget:
-                    if isinstance(widget, list):
-                        self._all_widgets[name+self._object._name]=widget
-                    else:                    
-                        self._all_widgets[name+self._object._name]=widget
+                    widget =  self.get_widget(value, value._type, name, self.get_unique_name(name), getattr(value, "attributes", None) )                    
+                    self._all_widgets[self.get_unique_name(name)]=widget
                 else:
                     self.store_all_widgets(value)
                    
-                    
-    def populate_widgets(self, obj):              
-        for name,value in obj.__dict__.items():       
-            if name=="_parent":
-                continue
-            
-            if value.__class__.__class__.__name__ in ("PyLocalPropertyMeta", "PyLocalObjectMeta"):
-                availability = (
-                    getattr(obj, "_availability")(name)
-                    if hasattr(obj, "_availability")
-                    else True
-                )  
-                #if not availability:
-                #    continue
-                #print(name, value, value.__class__.__class__.__name__)    
-                if value.__class__.__class__.__name__ == "PyLocalPropertyMeta":
-                    widget =  self.get_widget(value, value._type, name+self._object._name, getattr(value, "attributes", None) )
-                    #if widget:
-                    if isinstance(widget, list):
-                        self.__widgets.extend(widget)
-                    else:                    
-                        self.__widgets.append(widget)
-                else:
-                    self.populate_widgets(value) 
-                    
-                    
-    def visible_widgets(self, obj):              
+                                                           
+    def update_visible_widgets(self, obj):              
         for name,value in obj.__dict__.items():       
             if name=="_parent":
                 continue
@@ -182,22 +111,19 @@ class GraphicsWidget:
                 #print(name, value, value.__class__.__class__.__name__)    
                 if value.__class__.__class__.__name__ == "PyLocalPropertyMeta":
                    
-                    self._visible_widgets.append((name+self._object._name, value()))
+                    self._visible_widgets.append((self.get_unique_name(name), value()))
                 else:
-                    self.visible_widgets(value)                     
+                    self.update_visible_widgets(value)                     
                    
                          
         
-
-                   
-                   
-                    
-    def get_button(self, description):
-        widget = self._all_widgets.get(description) 
+                                                          
+    def get_button(self, unique_name):
+        widget = self._all_widgets.get(unique_name) 
         if widget is not None:            
             return widget    
                      
-        button = html.Button(description, id=description,  n_clicks=0) 
+        button = html.Button(unique_name, id=unique_name,  n_clicks=0) 
         
         
         @self._app.callback( 
@@ -205,34 +131,34 @@ class GraphicsWidget:
                 Output(self._vtk_view_id, "children"),
                 Output(self._vtk_view_id, "triggerResetCamera"),
             ],            
-            Input(description, "n_clicks"),                     
+            Input(unique_name, "n_clicks"),                     
         )
         def fun(n_clicks):                    
             return self._update_vtk_fun(self._object)      
        
         return button        
     
-    def get_widget(self, obj, type, description, attributes): 
-        widget = self._all_widgets.get(description) 
+    def get_widget(self, obj, type, name, unique_name, attributes): 
+        widget = self._all_widgets.get(unique_name) 
         if widget is not None:            
             return widget        
-        #print(str(type), description)
+        #print(str(type), unique_name)
         if str(type)=="<class 'str'>":
             if attributes and "allowed_values" in attributes:
                 widget = dcc.Dropdown(
-                    id=description,
+                    id=unique_name,
                     options=getattr(obj, "allowed_values"),
                     value= obj()                    
                 )                
             else:
                 widget = dcc.Input(
-                    id=description,
+                    id=unique_name,
                     type="text",
                     value = obj()
                 )
         elif str(type)=="typing.List[str]":
             widget = dcc.Dropdown(
-                id=description,
+                id=unique_name,
                 options=getattr(obj, "allowed_values"),
                 value= obj(),
                 multi=True
@@ -240,16 +166,16 @@ class GraphicsWidget:
             #print('widget', widget)            
         elif str(type)=="<class 'bool'>":    
             widget = dcc.Checklist(
-               id=description,
+               id=unique_name,
                options={
-                    'selected': description,                   
+                    'selected': self.get_label(name),                   
                },
                value=['selected'] if obj() else []
             )                    
         elif str(type)=="<class 'float'>":  
             if attributes and "range" in attributes:
                 widget = dcc.Input(
-                    id=description,
+                    id=unique_name,
                     type="number",
                     value = obj(),
                     min = getattr(obj, "range")[0],
@@ -257,14 +183,14 @@ class GraphicsWidget:
                 )                            
             else:
                 widget = dcc.Input(
-                    id=description,
+                    id=unique_name,
                     type="number",
                     value = obj()
                 )                
         elif str(type)=="<class 'int'>":  
             if attributes and "range" in attributes:          
                 widget = dcc.Input(
-                    id=description,
+                    id=unique_name,
                     type="number",
                     value = obj(),
                     min = getattr(obj, "range")[0],
@@ -272,7 +198,7 @@ class GraphicsWidget:
                 )                
             else:
                 widget = dcc.Input(
-                    id=description,
+                    id=unique_name,
                     type="number",
                     value = obj()
                 ) 
@@ -282,11 +208,11 @@ class GraphicsWidget:
         #    return 
             
         @self._app.callback(         
-          Output(description+"data", "value"),            
-          Input(description, "value"),                     
+          Output(unique_name+"data", "value"),            
+          Input(unique_name, "value"),                     
         )
         def update_oject(value): 
-            print('update_oject', description, value)
+            print('update_oject', unique_name, value)
             self.__old_defn = self._object()   
             if str(type)=="<class 'bool'>": 
                value =True if value else False
@@ -304,7 +230,7 @@ class GraphicsWidget:
             return str(value)        
                                        
         def refresh_bc(widget, obj):
-            print('refresh_bc', description, obj())           
+            print('refresh_bc', unique_name, obj())           
             if str(type)=="<class 'bool'>":                
                 widget.value = ["selected"] if obj() else []      
             else:            
@@ -312,15 +238,24 @@ class GraphicsWidget:
             
         w=widget    
                   
-        self.__refresh_bcs.append(partial(refresh_bc, w, obj))            
-        widget = html.Div(
-            [
-                description,
-                widget,
-                html.Data(id = description+"data")
-            ],
-            id = description+"container"
-        )                  
+        self.__refresh_bcs.append(partial(refresh_bc, w, obj)) 
+        if str(type)=="<class 'bool'>":   
+            widget = html.Div(
+                [
+                    widget,
+                    html.Data(id = unique_name+"data")
+                ],
+                id = unique_name+"container"
+            )
+        else:        
+            widget = html.Div(
+                [
+                    self.get_label(name),
+                    widget,
+                    html.Data(id = unique_name+"data")
+                ],
+                id = unique_name+"container"
+            )                  
         return widget    
                         
         
