8d5cceb968c942d0a110b9be1e4626fcb724704f
ANSYS\spearson <sean.pearson@ansys.com>
2022-04-27 15:50:18 +0100

    test refactoring

--- tests/test_mixing_elbow.py
+++ tests/test_mixing_elbow.py
@@ -35,7 +35,6 @@ def execute_task_with_pre_and_postcondition_checks(workflow, task_name):
     check_task_execute_preconditions(task_state)
     # Some tasks are wrongly retutning False in meshing workflow itself
     # so we add a temporary caveat below
-    # Will add issue and contact Mahendar
     result = task.Execute()
     if task_name not in ("Add Local Sizing", "Add Boundary Layers"):
         assert result is True
