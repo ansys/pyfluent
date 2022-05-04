b08d328340b67f61ee481a2df2e5ea9f4db625f7
Aseem Jain <aseem.jain@ansys.com>
2022-05-04 10:31:26 +0530

    Authentication

--- src/ansys/fluent/core/utils/dash/dash_app.py
+++ src/ansys/fluent/core/utils/dash/dash_app.py
@@ -8,6 +8,7 @@ import time
 import uuid
 import dash
 import dash_bootstrap_components as dbc
+import dash_auth
 import numpy as np
 import plotly.graph_objs as go
 from dash import Input, Output, State, dcc, html, ALL
@@ -15,7 +16,7 @@ from dash.exceptions import PreventUpdate
 from tree_view import TreeView
 from local_property_editor import LocalPropertyEditor
 import plotly.io as pio
-
+from flask import request
 pio.templates.default = "plotly_white"
 
 from ansys.fluent.core.utils.dash.sessions_manager import SessionsManager
@@ -28,10 +29,30 @@ from local_property_editor import (
 from PropertyEditor import PropertyEditor
 
 
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'user1': 'abc',
+    'user2': 'abc',
+    'user3': 'abc',
+    'user4': 'abc'
+}
+user_name_to_session_map={}
+
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
 app = dash.Dash(
+    __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True,
 )
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
 import dash_treeview_antd
 
 app.config.suppress_callback_exceptions = True
@@ -48,23 +69,45 @@ SIDEBAR_STYLE = {
     "overflow-y": "scroll",
 }
 
+def get_side_bar(app, connection_id):
 
-sidebar = html.Div(
-    [
-        html.H6("Outline"),
-        html.Div(
-            id="tree-view-container",
-        ),
-    ],
-    style=SIDEBAR_STYLE,
-)
+    session_id = user_name_to_session_map.get(connection_id,[None])[0]
+    tree_view = []
+    if session_id:
+        tree_nodes, keys = TreeView(
+            app, connection_id, session_id, SessionsManager
+        ).get_tree_nodes()
+        tree_view = dash_treeview_antd.TreeView(
+            id="tree-view",
+            multiple=False,
+            expanded=keys,
+            data=tree_nodes,
+        )
+
+
+    return  html.Div(
+      [
+          html.H5(
+          [
+          dbc.Badge(f"Welcome {connection_id}", color="primary", className="me-1"),
+          ]
+          ),
+          html.H6("Outline"),
+          html.Div(
+              id="tree-view-container",
+              children = tree_view
+          ),
+      ],
+      style=SIDEBAR_STYLE,
+    )
 
 
 _max_session_count = 1
 
 
 def serve_layout():
-    connection_id = str(uuid.uuid4())
+    #connection_id = str(uuid.uuid4())
+    connection_id = request.authorization['username'] 
     for session_id in range(_max_session_count):
         SessionsManager(app, connection_id, f"session-{session_id}")
     PropertyEditor(app, SessionsManager)
@@ -127,8 +170,8 @@ def serve_layout():
                     dbc.Col(
                         dcc.Dropdown(
                             id="session-id",
-                            options=[],
-                            value=None,
+                            options=user_name_to_session_map.get(connection_id,[]),
+                            value=user_name_to_session_map.get(connection_id,[None])[0],
                             style={"width": "200px"},
                         ),
                         width="auto",
@@ -143,7 +186,7 @@ def serve_layout():
             html.Hr(),
             dbc.Row(
                 children=[
-                    dbc.Col(sidebar, align="start", width="auto"),
+                    dbc.Col(get_side_bar(app, connection_id), align="start", width="auto"),
                     dbc.Col(
                         id="property-editor",
                         width="auto",
@@ -203,9 +246,20 @@ print("serve_layout done")
     State("session-id", "options"),
 )
 def create_session(n_clicks, session_token, connection_id, options):
-    if n_clicks == 0:
-        raise PreventUpdate
+
+    ctx = dash.callback_context
+    triggered_value = ctx.triggered[0]["value"]
+    triggered_from = ctx.triggered[0]["prop_id"].split(".")[0]
+    print('create_session', triggered_from, triggered_value)
+    if n_clicks == 0 or triggered_value is None:
+        raise PreventUpdate    
+    
+    user_sessions= user_name_to_session_map.get(connection_id)
+    if not user_sessions:
+        user_sessions =user_name_to_session_map[connection_id] = [] 
+        
     session_id = f"session-{len(options)}"
+    user_sessions.append(session_id)
     sessions_manager = SessionsManager(app, connection_id, session_id)
     sessions_manager.add_session(session_token)
     sessions = []
@@ -222,14 +276,16 @@ def create_session(n_clicks, session_token, connection_id, options):
     Input("save-button-clicked", "value"),
     Input("delete-button-clicked", "value"),
     State("object-id", "value"),
-    prevent_initial_call=True,
+    #prevent_initial_call=True,
 )
 def update_tree(connection_id, session_id, save_n_clicks, delete_n_clicks, object_id):
     ctx = dash.callback_context
     triggered_value = ctx.triggered[0]["value"]
+    triggered_from = ctx.triggered[0]["prop_id"].split(".")[0]
+    print('update_tree', triggered_from, triggered_value)
     if session_id is None or connection_id is None or triggered_value is None:
         raise PreventUpdate
-    triggered_from = ctx.triggered[0]["prop_id"].split(".")[0]
+    
 
     if triggered_from == "save-button-clicked":
         object_location, object_type, object_index = object_id.split(":")
