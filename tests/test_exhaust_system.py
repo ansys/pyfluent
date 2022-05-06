b079930c8232b7f0c10febe5952dcb5183c8b64e
Yogesh Patel <yogesh.patel@ansys.com>
2022-05-05 20:01:22 -0400

    Fixing codespell error

--- tests/test_exhaust_system.py
+++ tests/test_exhaust_system.py
@@ -5,7 +5,7 @@ Exhaust System: Fault-tolerant Meshing
 
 This test covers the setup and solution of a three-dimensional
 turbulent fluid flow in a manifold exhaust system using fault
-tolerant mehing worflow.
+tolerant mehing workflow.
 
 This test queries the following using PyTest:
 
