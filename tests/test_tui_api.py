5033ba572b2549096d560a0e70c8545f9cbd07da
Mainak Kundu <mainak.kundu@ansys.com>
2022-05-09 15:27:46 +0530

    Fix unittest

--- tests/test_tui_api.py
+++ tests/test_tui_api.py
@@ -4,7 +4,7 @@ from util.solver_workflow import new_solver_session  # noqa: F401
 def test_report_system_proc_stats_tui(new_solver_session, capsys) -> None:
     new_solver_session.start_transcript()
     # Issue: Transcript missing for the first TUI command
-    new_solver_session.tui.solver.report.system.proc_stats()
-    new_solver_session.tui.solver.report.system.proc_stats()
+    new_solver_session.tui.solver.report.system.sys_stats()
+    new_solver_session.tui.solver.report.system.sys_stats()
     captured = capsys.readouterr()
-    assert "Virtual Mem Usage" in captured.out
+    assert "CPU/Memory Usage" in captured.out
