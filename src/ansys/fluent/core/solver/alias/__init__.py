8a63fb46de38d226bdd9f94ebc9bdc044dbd978b
ANSYS\spearson <sean.pearson@ansys.com>
2022-05-20 21:21:23 +0100

    object aliasing

--- src/ansys/fluent/core/solver/alias/__init__.py
+++ src/ansys/fluent/core/solver/alias/__init__.py
@@ -1,14 +1,19 @@
+from functools import partial
 import json
 import os
 import sys
 
 from ..flobject import NamedObject
 
-__alias_session = None
-__fns = sys.modules[__name__].__dict__
+print("aliases")
 
+__alias_session = None
+
+__alias_to_path = None
 with open(os.path.join(os.path.dirname(__file__), "aliases.json")) as json_file:
-    alias_to_path = json.load(json_file)
+    __alias_to_path = json.load(json_file)
+
+print("__alias_to_path", __alias_to_path)
 
 
 def set_alias_session(session):
@@ -16,15 +21,31 @@ def set_alias_session(session):
     __alias_session = session
 
 
-def _resolve_alias(path, session, name):
+def __resolve_alias(alias, session=None, name=None):
+    print("__resolve_alias", alias, session, name)
+    print("__alias_to_path", __alias_to_path)
+    path = __alias_to_path[alias]
+    print("__resolve_alias", alias, path, session, name)
     session = session or __alias_session
-    obj = eval("session.solver.root." + path)
+    full_path = "session.solver.root." + path
+    print("full_path", full_path)
+    obj = eval(full_path)
+    print("obj", obj)
     if not name:
+        print("return obj")
         return obj
     if isinstance(obj, NamedObject):
+        print("return obj[", name, "]")
         return obj[name]
     raise RuntimeError("Only named objects have names.")
 
 
-for alias, path in alias_to_path.items():
-    __fns[alias] = lambda session=None, name=None: _resolve_alias(path, session, name)
+print("__alias_to_path", __alias_to_path)
+
+for alias in __alias_to_path:
+    if alias in sys.modules[__name__].__dict__:
+        raise RuntimeError(f"Cannot re-register alias {alias}")
+    print("assign to module", alias)
+    sys.modules[__name__].__dict__[alias] = partial(__resolve_alias, alias)
+
+print("module", sys.modules[__name__].__dict__)
