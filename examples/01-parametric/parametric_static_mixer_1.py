16d52c5087cbb150bb84920dd282c6defd20eade
Anurag Kulshrestha <anurag.kulshrestha@ansys.com>
2022-05-25 19:14:58 +0530

    Refactoring parametric example

--- examples/01-parametric/parametric_static_mixer_1.py
+++ examples/01-parametric/parametric_static_mixer_1.py
@@ -9,8 +9,7 @@ This parametric study workflow example performs these steps:
 - Instantiates a design point study
 - Accesses and modifies the input parameters of
   the base design point (DP)
-- Updates the current DP
-- Accesses output parameters of the base DP
+- Updates design points
 - Creates, updates, and deletes more DPs
 - Creates, renames, duplicates and deletes parametric studies
 """
@@ -20,7 +19,7 @@ from pathlib import Path
 
 import ansys.fluent.core as pyfluent
 from ansys.fluent.core import examples
-from ansys.fluent.parametric import ParametricStudy
+from ansys.fluent.parametric import ParametricProject, ParametricStudy
 from ansys.fluent.post import set_config
 from ansys.fluent.post.pyvista import Graphics
 
@@ -128,30 +127,19 @@ study_1.design_points["Base DP"].input_parameters = input_parameters_update
 study_1.update_current_design_point()
 
 ###########################################################################
-# Change value of specific design points
+# Add a new design points
 
 design_point_1 = study_1.add_design_point()
 design_point_1_input_parameters = study_1.design_points["DP1"].input_parameters
-design_point_1_input_parameters["inlet1_temp"] = 450
-design_point_1_input_parameters["inlet1_vel"] = 0.25
-design_point_1_input_parameters["inlet2_vel"] = 0.25
+design_point_1_input_parameters["inlet1_temp"] = 500
+design_point_1_input_parameters["inlet1_vel"] = 1
+design_point_1_input_parameters["inlet2_vel"] = 1
 study_1.design_points["DP1"].input_parameters = design_point_1_input_parameters
-
-###########################################################################
-# Add another design point with different values of the input parameters
-
-design_point_2 = study_1.add_design_point()
-design_point_2_input_parameters = study_1.design_points["DP2"].input_parameters
-design_point_2_input_parameters["inlet1_temp"] = 500
-design_point_2_input_parameters["inlet1_vel"] = 1
-design_point_2_input_parameters["inlet2_vel"] = 1
-study_1.design_points["DP2"].input_parameters = design_point_2_input_parameters
-
 
 ##########################################################################
 # Duplicate design points
 
-design_point_3 = study_1.duplicate_design_point(design_point_2)
+design_point_2 = study_1.duplicate_design_point(design_point_1)
 
 #########################################################################
 # Update all design points for study 1
@@ -184,7 +172,7 @@ study_1.export_design_table(design_point_table)
 ##########################################################################
 # Delete design points
 
-study_1.delete_design_points([design_point_1, design_point_2])
+study_1.delete_design_points([design_point_1])
 
 ##########################################################################
 # Create a new parametric study by duplicating the current one
@@ -202,11 +190,51 @@ study_2.rename("New Study")
 study_1.delete()
 
 #########################################################################
-# Save parametric project
+# Save parametric project and close Fluent
 
 project_filepath = str(Path(pyfluent.EXAMPLES_PATH) / "static_mixer_study.flprj")
 
 session.solver.tui.file.parametric_project.save_as(project_filepath)
+
+session.exit()
+
+#########################################################################
+# Launch Fluent again and read the previously saved project
+
+session = pyfluent.launch_fluent(precision="double", processor_count=2)
+project_filepath_read = str(Path(pyfluent.EXAMPLES_PATH) / "static_mixer_study.flprj")
+
+proj = ParametricProject(
+    session.solver.root.file.parametric_project,
+    session.solver.root.parametric_studies,
+    project_filepath_read,
+)
+
+#########################################################################
+# Save the current project
+
+proj.save()
+
+#########################################################################
+# Save the current project to a different file name
+
+project_filepath_save_as = str(
+    Path(pyfluent.EXAMPLES_PATH) / "static_mixer_study_save_as.flprj"
+)
+proj.save_as(project_filepath=project_filepath_save_as)
+
+#########################################################################
+# Export the current project
+
+project_filepath_export = str(
+    Path(pyfluent.EXAMPLES_PATH) / "static_mixer_study_export.flprj"
+)
+proj.export(project_filepath=project_filepath_export)
+
+#########################################################################
+# Archive the current project
+
+proj.archive()
 
 #########################################################################
 # Close Fluent
