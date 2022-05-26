3bcadddbc062422898c2436b87c487f7668ff869
ANSYS\spearson <sean.pearson@ansys.com>
2022-05-26 13:43:09 +0100

    disable codegen for doc

---
+++ codegen/allapigen_doc.py
@@ -0,0 +1,11 @@
+import datamodelgen
+import print_fluent_version
+
+# import settingsgen
+import tuigen
+
+if __name__ == "__main__":
+    print_fluent_version.generate()
+    tuigen.generate()
+    datamodelgen.generate()
+    # settingsgen.generate()
