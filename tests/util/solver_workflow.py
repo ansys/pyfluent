63db59933c9ead71e1b5fefaffa94fc640629558
Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>
2022-05-31 13:35:39 +0100

    Feat/transcript by default (#444)
    * transript by default
    
    * transript by default
    
    * transript by default
    
    * test start transcript

--- tests/util/solver_workflow.py
+++ tests/util/solver_workflow.py
@@ -3,12 +3,19 @@ import pytest
 import ansys.fluent.core as pyfluent
 
 
-def create_solver_session():
-    return pyfluent.launch_fluent()
+def create_solver_session(*args, **kwargs):
+    return pyfluent.launch_fluent(**kwargs)
 
 
 @pytest.fixture
 def new_solver_session(with_running_pytest):
     solver = create_solver_session()
+    yield solver
+    solver.exit()
+
+
+@pytest.fixture
+def new_solver_session_no_transcript(with_running_pytest):
+    solver = create_solver_session(start_transcript=False)
     yield solver
     solver.exit()
