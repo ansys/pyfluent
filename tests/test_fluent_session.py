72492e4b42e79b5d9b670e85ca7f65343e2e6247
ANSYS\spearson <sean.pearson@ansys.com>
2022-05-31 10:20:31 +0100

    transript by default

--- tests/test_fluent_session.py
+++ tests/test_fluent_session.py
@@ -11,8 +11,9 @@ def test_session_starts_transcript_by_default(new_solver_session) -> None:
 
     print_transcript.out = None
     session._print_transcript = print_transcript
-    # session.scheme_eval.string_eval("(+ 2 3)")
-    # assert print_transcript.out == "5"
+
+    session.scheme_eval.string_eval("(+ 2 3)")
+    assert print_transcript.out == "5"
 
     """
 def test_session_starts_no_transcript_if_disabled(new_solver_session_no_transcript) -> None:
