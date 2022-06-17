8c13a6b2cce06dd8f4163de914afc03ed7ff5015
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-06-17 19:42:25 +0530

    Information updated about TUI entries usage in the TUI API

--- codegen/data/tui_menu_descriptions.py
+++ codegen/data/tui_menu_descriptions.py
@@ -11,6 +11,16 @@ session is easier if you install a tool such as
 both command line completion and history.  You can also use Python standard
 `help` and `dir` commands on any object in the API to inspect it further.
 
+The arguments to a TUI API call are just those that would be passed in direct interaction with the
+Fluent TUI, but in a Pythonic style. For instance the TUI interaction /define/models/energy? #t becomes
+_session_.solver.tui.define.energy(False).
+
+Care must be taken wherever the TUI expects a quoted string as input: in such cases the Python argument
+should be provided as a double quoted string embedded inside single quotes. For instance
+/define/boundary_conditions/velocity_inlet "IF(t<=10e-6[sec],3.58[m/s]*cos(PI*t/30e-6[s]),0[m/s])"
+in the TUI would be written as session.solver.tui.define.boundary_conditions.velocity_inlet("inlet2", "no", "no", "yes", "yes", "no",
+'"IF(t<=10e-6[sec],3.58[m/s]*cos(PI*t/30e-6[s]),0[m/s])"', "no", 0, "no", 300, "no", "no", "no", "yes", 10, 0.04) in Python.
+
 The TUI based examples in our gallery provide a guide for how to use this API.
 """
 }
