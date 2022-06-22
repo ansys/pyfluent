5beb076c2081ba6a86dd2cff01e7131da710607e
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2022-06-22 11:18:53 +0530

    version bump to 0.9.dev0 (#511)

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 8, "dev0"
+version_info = 0, 9, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
