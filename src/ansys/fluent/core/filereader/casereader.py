e9df220112d4c9af4cc8f56ed23af94915608528
ANSYS\spearson <sean.pearson@ansys.com>
2022-07-21 19:12:03 +0100

    file reader

---
+++ src/ansys/fluent/core/filereader/casereader.py
@@ -0,0 +1,115 @@
+"""Reader for Fluent case files.
+
+Example
+-------
+
+>>> from ansys.fluent.parametric.local.filereader.casereader import CaseReader
+
+Instantiate a case reader
+
+>>> reader = CaseReader(case_filepath=case_filepath)
+
+Get lists of input and output parameters
+
+>>> input_parameters = reader.input_parameters()
+>>> output_parameters = reader.output_parameters()
+"""
+
+from typing import List
+
+import h5py
+
+from . import lispy
+
+
+class InputParameter:
+    """Class to represent an input parameter.
+
+    Attributes
+    ----------
+    name : str
+    value
+        The value of this input parameter, usually
+        a string, qualified by units
+    """
+
+    def __init__(self, raw_data):
+        self.name, self.name = None, None
+        for k, v in raw_data:
+            if k == "name":
+                self.name = v
+            elif k == "definition":
+                self.value = v
+
+
+class OutputParameter:
+    """Class to represent an output parameter.
+
+    Attributes
+    ----------
+    name : str
+    """
+
+    def __init__(self, raw_data):
+        parameter = raw_data[1]
+        for elem in parameter:
+            if len(elem) and elem[0] == "name":
+                self.name = elem[1][1]
+
+
+class CaseReader:
+    """Class to read a Fluent case file.
+
+    Methods
+    -------
+    input_parameters
+        Get a list of input parameter objects
+    output_parameters
+        Get a list of output parameter objects
+    """
+
+    def __init__(self, case_filepath: str):
+        file = h5py.File(case_filepath)
+        settings = file["settings"]
+        rpvars = settings["Rampant Variables"][0]
+        rp_vars_str = rpvars.decode()
+        self._rp_vars = lispy.parse(rp_vars_str)[1]
+        self._rp_var_cache = {}
+
+    def input_parameters(self) -> List[InputParameter]:
+        exprs = self._named_expressions()
+        input_params = []
+        for expr in exprs:
+            for attr in expr:
+                if attr[0] == "input-parameter" and attr[1] is True:
+                    input_params.append(InputParameter(expr))
+        return input_params
+
+    def output_parameters(self) -> List[OutputParameter]:
+        parameters = self._find_rp_var("parameters/output-parameters")
+        return [OutputParameter(param) for param in parameters]
+
+    def num_dimensions(self) -> int:
+        for attr in self._case_config():
+            if attr[0] == "rp-3d?":
+                return 3 if attr[1] is True else 2
+
+    def precision(self) -> int:
+        for attr in self._case_config():
+            if attr[0] == "rp-double?":
+                return 2 if attr[1] is True else 1
+
+    def _named_expressions(self):
+        return self._find_rp_var("named-expressions")
+
+    def _case_config(self):
+        return self._find_rp_var("case-config")
+
+    def _find_rp_var(self, name: str):
+        try:
+            return self._rp_var_cache[name]
+        except KeyError:
+            for var in self._rp_vars:
+                if type(var) == list and len(var) and var[0] == name:
+                    self._rp_var_cache[name] = var[1]
+                    return var[1]
---
+++ tests/test_casereader.py
@@ -0,0 +1,37 @@
+from ansys.fluent.core import examples
+from ansys.fluent.core.filereader.casereader import CaseReader
+
+
+def test_casereader():
+
+    case_filepath = examples.download_file(
+        "Static_Mixer_Parameters.cas.h5", "pyfluent/static_mixer"
+    )
+
+    reader = CaseReader(case_filepath=case_filepath)
+
+    input_parameters = reader.input_parameters()
+
+    assert reader.precision() == 2
+
+    assert reader.num_dimensions() == 3
+
+    assert len(input_parameters) == 4
+
+    input_parameter_dict = {p.name: p.value for p in input_parameters}
+
+    assert input_parameter_dict["inlet1_temp"] == "300 [K]"
+
+    assert input_parameter_dict["inlet1_vel"] == "1 [m/s]"
+
+    assert input_parameter_dict["inlet2_temp"] == "350 [K]"
+
+    assert input_parameter_dict["inlet2_vel"] == "1 [m/s]"
+
+    output_parameters = reader.output_parameters()
+
+    assert len(output_parameters) == 2
+
+    assert {"outlet-temp-avg-op", "outlet-vel-avg-op"} == {
+        p.name for p in output_parameters
+    }
