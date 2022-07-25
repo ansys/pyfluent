a45ecc6db3b2b05ed1f17572830d4dafb555f7a4
ANSYS\spearson <sean.pearson@ansys.com>
2022-07-25 17:19:35 +0100

    got the command server -> client

--- codegen/allapigen.py
+++ codegen/allapigen.py
@@ -5,6 +5,8 @@ import tuigen
 
 if __name__ == "__main__":
     print_fluent_version.generate()
-    tuigen.generate()
-    datamodelgen.generate()
-    settingsgen.generate()
+    for mod in (tuigen, datamodelgen, settingsgen):
+        try:
+            mod.generate()
+        except BaseException as ex:
+            print("skipping generation of", str(mod), str(ex))
