83c80e08d2d18894e06f17050161a2ad5ecfafed
Elijah Gravenhorst <elijah.gravenhorst@ansys.com>
2022-07-25 12:44:47 -0400

    Added new optiSLang integration test scenario

--- tests/util/fixture_fluent.py
+++ tests/util/fixture_fluent.py
@@ -54,3 +54,26 @@ def load_mixing_elbow_case_dat(with_launching_container):
     )
     yield session
     session.exit()
+
+
+_mixing_elbow_param_case_filename = None
+_mixing_elbow_param_dat_filename = None
+
+@pytest.fixture
+def load_mixing_elbow_param_case_dat(with_launching_container):
+    session = pyfluent.launch_fluent(precision="double", processor_count=2)
+    global _mixing_elbow_param_case_filename
+    if not _mixing_elbow_param_case_filename:
+        _mixing_elbow_param_case_filename = download_file(
+            filename="elbow_param.cas.h5", directory="pyfluent/mixing_elbow"
+        )
+    global _mixing_elbow_param_dat_filename
+    if not _mixing_elbow_param_dat_filename:
+        _mixing_elbow_param_dat_filename = download_file(
+            filename="elbow_param.dat.h5", directory="pyfluent/mixing_elbow"
+        )
+    session.solver.root.file.read(
+        file_type="case-data", file_name=_mixing_elbow_param_case_filename
+    )
+    yield session
+    session.exit()
