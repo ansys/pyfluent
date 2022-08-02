31e940db906fb048e3fe3633e3f37e605e56c567
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-08-02 11:32:39 +0530

    Updated example - mixing_elbow_settings_api.py to match the new launch mode of fluent

--- examples/00-fluent/mixing_elbow_settings_api.py
+++ examples/00-fluent/mixing_elbow_settings_api.py
@@ -37,7 +37,7 @@ import_filename = examples.download_file("mixing_elbow.msh.h5", "pyfluent/mixing
 # Launch Fluent as a service in meshing mode with double precision running on
 # two processors.
 
-session = pyfluent.launch_fluent(precision="double", processor_count=2)
+solver = pyfluent.launch_fluent(precision="double", processor_count=2, mode="solver")
 
 ###############################################################################
 # Import mesh and perform mesh check
@@ -48,8 +48,8 @@ session = pyfluent.launch_fluent(precision="double", processor_count=2)
 # in the mesh are reported. Ensure that the minimum volume is not negative because
 # Fluent cannot begin a calculation when this is the case.
 
-session.solver.root.file.read(file_type="case", file_name=import_filename)
-session.solver.tui.mesh.check()
+solver.root.file.read(file_type="case", file_name=import_filename)
+solver.tui.mesh.check()
 
 ###############################################################################
 # Set working units for mesh
@@ -59,21 +59,21 @@ session.solver.tui.mesh.check()
 # in this example. If you want working units for length to be other than inches
 # (for example, millimeters), make the appropriate change.
 
-session.solver.tui.define.units("length", "in")
+solver.tui.define.units("length", "in")
 
 ###############################################################################
 # Enable heat transfer
 # ~~~~~~~~~~~~~~~~~~~~
 # Enable heat transfer by activating the energy equation.
 
-session.solver.root.setup.models.energy.enabled = True
+solver.root.setup.models.energy.enabled = True
 
 ###############################################################################
 # Create material
 # ~~~~~~~~~~~~~~~
 # Create a material named ``"water-liquid"``.
 
-session.solver.root.setup.materials.copy_database_material_by_name(
+solver.root.setup.materials.copy_database_material_by_name(
     type="fluid", name="water-liquid"
 )
 
@@ -83,7 +83,7 @@ session.solver.root.setup.materials.copy_database_material_by_name(
 # Set up the cell zone conditions for the fluid zone (elbow-fluid). Set ``material``
 # to ``"water-liquid"``.
 
-session.solver.root.setup.cell_zone_conditions.fluid[
+solver.root.setup.cell_zone_conditions.fluid[
     "elbow-fluid"
 ].material = "water-liquid"
 
@@ -101,20 +101,20 @@ session.solver.root.setup.cell_zone_conditions.fluid[
 # Hydraulic Diameter: 4 [inch]
 # Temperature: 293.15 [K]
 
-session.solver.root.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = {
+solver.root.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = {
     "option": "constant or expression",
     "constant": 0.4,
 }
-session.solver.root.setup.boundary_conditions.velocity_inlet[
+solver.root.setup.boundary_conditions.velocity_inlet[
     "cold-inlet"
 ].ke_spec = "Intensity and Hydraulic Diameter"
-session.solver.root.setup.boundary_conditions.velocity_inlet[
+solver.root.setup.boundary_conditions.velocity_inlet[
     "cold-inlet"
 ].turb_intensity = 5
-session.solver.root.setup.boundary_conditions.velocity_inlet[
+solver.root.setup.boundary_conditions.velocity_inlet[
     "cold-inlet"
 ].turb_hydraulic_diam = "4 [in]"
-session.solver.root.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = {
+solver.root.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = {
     "option": "constant or expression",
     "constant": 293.15,
 }
@@ -127,17 +127,17 @@ session.solver.root.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = {
 # Hydraulic Diameter: 1 [inch]
 # Temperature: 313.15 [K]
 
-session.solver.root.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = {
+solver.root.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = {
     "option": "constant or expression",
     "constant": 1.2,
 }
-session.solver.root.setup.boundary_conditions.velocity_inlet[
+solver.root.setup.boundary_conditions.velocity_inlet[
     "hot-inlet"
 ].ke_spec = "Intensity and Hydraulic Diameter"
-session.solver.root.setup.boundary_conditions.velocity_inlet[
+solver.root.setup.boundary_conditions.velocity_inlet[
     "hot-inlet"
 ].turb_hydraulic_diam = "1 [in]"
-session.solver.root.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = {
+solver.root.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = {
     "option": "constant or expression",
     "constant": 313.15,
 }
@@ -146,7 +146,7 @@ session.solver.root.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = {
 # Backflow Turbulent Intensity: 5 [%]
 # Backflow Turbulent Viscosity Ratio: 4
 
-session.solver.root.setup.boundary_conditions.pressure_outlet[
+solver.root.setup.boundary_conditions.pressure_outlet[
     "outlet"
 ].turb_viscosity_ratio = 4
 
@@ -155,42 +155,42 @@ session.solver.root.setup.boundary_conditions.pressure_outlet[
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Disable plotting of residuals during the calculation.
 
-session.solver.tui.solve.monitors.residual.plot("no")
+solver.tui.solve.monitors.residual.plot("no")
 
 ###############################################################################
 # Initialize flow field
 # ~~~~~~~~~~~~~~~~~~~~~
 # Initialize the flow field using the hybrid initialization.
 
-session.solver.root.solution.initialization.hybrid_initialize()
+solver.root.solution.initialization.hybrid_initialize()
 
 ###############################################################################
 # Solve for 150 iterations
 # ~~~~~~~~~~~~~~~~~~~~~~~~
 # Solve for 150 iterations.
 
-session.solver.root.solution.run_calculation.iterate.get_attr("arguments")
-session.solver.root.solution.run_calculation.iterate(number_of_iterations=150)
+solver.root.solution.run_calculation.iterate.get_attr("arguments")
+solver.root.solution.run_calculation.iterate(number_of_iterations=150)
 
 ###############################################################################
 # Create velocity vectors
 # ~~~~~~~~~~~~~~~~~~~~~~~
 # Create and display velocity vectors on the symmetry-xyplane plane.
 
-session.solver.root.results.graphics.vector["velocity_vector_symmetry"] = {}
-session.solver.root.results.graphics.vector["velocity_vector_symmetry"].print_state()
-session.solver.root.results.graphics.vector[
+solver.root.results.graphics.vector["velocity_vector_symmetry"] = {}
+solver.root.results.graphics.vector["velocity_vector_symmetry"].print_state()
+solver.root.results.graphics.vector[
     "velocity_vector_symmetry"
 ].field = "temperature"
-session.solver.root.results.graphics.vector[
+solver.root.results.graphics.vector[
     "velocity_vector_symmetry"
 ].surfaces_list = [
     "symmetry-xyplane",
 ]
-session.solver.root.results.graphics.vector[
+solver.root.results.graphics.vector[
     "velocity_vector_symmetry"
 ].scale.scale_f = 4
-session.solver.root.results.graphics.vector["velocity_vector_symmetry"].style = "arrow"
+solver.root.results.graphics.vector["velocity_vector_symmetry"].style = "arrow"
 
 ###############################################################################
 # .. image:: /_static/mixing_elbow_016.png
@@ -202,21 +202,21 @@ session.solver.root.results.graphics.vector["velocity_vector_symmetry"].style = 
 # ~~~~~~~~~~~~~~~~~~~~~~
 # Compute the mass flow rate.
 
-session.solver.root.solution.report_definitions.flux["mass_flow_rate"] = {}
-session.solver.root.solution.report_definitions.flux[
+solver.root.solution.report_definitions.flux["mass_flow_rate"] = {}
+solver.root.solution.report_definitions.flux[
     "mass_flow_rate"
 ].zone_names.get_attr("allowed-values")
-session.solver.root.solution.report_definitions.flux["mass_flow_rate"].zone_names = [
+solver.root.solution.report_definitions.flux["mass_flow_rate"].zone_names = [
     "cold-inlet",
     "hot-inlet",
     "outlet",
 ]
-session.solver.root.solution.report_definitions.flux["mass_flow_rate"].print_state()
-session.solver.root.solution.report_definitions.compute(report_defs=["mass_flow_rate"])
+solver.root.solution.report_definitions.flux["mass_flow_rate"].print_state()
+solver.root.solution.report_definitions.compute(report_defs=["mass_flow_rate"])
 
 #########################################################################
 # Close Fluent
 # ~~~~~~~~~~~~
 # Close Fluent.
 
-session.exit()
+solver.exit()
