f130ca0a5ad2acd0a34d795795d84217876c81c9
Anurag Kulshrestha <103188356+akulshre-qa@users.noreply.github.com>
2022-08-03 17:51:55 +0530

    New test scenario added to test the new changes (#652)
    * Resolving git conflicts
    
    * Added path variable
    
    * Resolving conflicts
    
    * skipiing 23.1 specific test to run for now

--- tests/test_solvermode/test_boundaries.py
+++ tests/test_solvermode/test_boundaries.py
@@ -1,4 +1,9 @@
+import json
+import os
+from unittest import TestCase
+
 import pytest
+from util.fixture_fluent import get_name_info
 
 
 @pytest.mark.integration
@@ -76,3 +81,54 @@ def test_boundaries_elbow(load_mixing_elbow_mesh):
         ].turb_viscosity_ratio()
         == 4
     )
+
+
+@pytest.mark.integration
+@pytest.mark.setup
+@pytest.mark.skip(reason="skip to run using 22.2")
+def test_boundaries_periodic(load_periodic_rot_cas):
+    session = load_periodic_rot_cas
+    print(__file__)
+    _THIS_DIR = os.path.dirname(__file__)
+    _DATA_FILE = os.path.join(_THIS_DIR, "boundaries_periodic_expDict")
+    pysession = session.solver.root
+    boundary_exp = json.load(open(_DATA_FILE))
+    boundary_test = dict()
+    boundary_tested = dict()
+    for name, boundary in pysession.setup.boundary_conditions.items():
+        boundary_test[name] = boundary()
+    boundary_tested["val_1"] = boundary_test
+    TestCase().assertDictEqual(boundary_tested["val_1"], boundary_exp["val_1"])
+
+    boundary_test = dict()
+    for boundary_type in pysession.setup.boundary_conditions.get_active_child_names():
+        if boundary_type == "matching_tolerance":
+            continue
+        for name, boundary in getattr(
+            pysession.setup.boundary_conditions, boundary_type
+        ).items():
+            boundary_test[name] = boundary()
+    boundary_tested["val_2"] = boundary_test
+    TestCase().assertDictEqual(boundary_tested["val_2"], boundary_exp["val_2"])
+
+    boundaries_check = ["inlet", "outlet", "pipe_2_wall"]
+    selected_bou_test = get_name_info(boundary_tested["val_1"], boundaries_check)
+    selected_bou_exp = get_name_info(boundary_exp["val_1"], boundaries_check)
+    TestCase().assertDictEqual(selected_bou_test, selected_bou_exp)
+
+    pysession.setup.boundary_conditions.wall["pipe_2_wall"].rename("pipe2_wall")
+    pysession.setup.boundary_conditions.wall.rename("out", "outlet")
+    pysession.setup.boundary_conditions.velocity_inlet["inlet"].vmag = 5.0
+    pysession.setup.boundary_conditions["inlet"].vmag = 10.0
+    boundaries_check = ["inlet", "out", "pipe2_wall"]
+    boundary_test = dict()
+    for name, boundary in pysession.setup.boundary_conditions.items():
+        boundary_test[name] = boundary()
+    boundary_tested["val_3"] = boundary_test
+    TestCase().assertDictEqual(boundary_tested["val_3"], boundary_exp["val_3"])
+
+    selected_bou_test = get_name_info(boundary_tested["val_3"], boundaries_check)
+    selected_bou_exp = get_name_info(boundary_exp["val_3"], boundaries_check)
+    TestCase().assertDictEqual(selected_bou_test, selected_bou_exp)
+    with open("boundaries_periodic_outDict.py", "a") as f:
+        json.dump(boundary_tested, f, sort_keys=True, indent=4)
