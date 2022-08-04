e91892f2268d96ec93f994604af17f95e601471b
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-08-04 12:43:13 +0530

    Compatibility update

--- tests/test_session.py
+++ tests/test_session.py
@@ -8,7 +8,7 @@ import pytest
 from ansys.api.fluent.v0 import health_pb2, health_pb2_grpc
 from ansys.fluent.core import launch_fluent
 from ansys.fluent.core.services.health_check import HealthCheckService
-from ansys.fluent.core.session import Session
+from ansys.fluent.core.session import BaseSession
 
 
 class MockHealthServicer(health_pb2_grpc.HealthServicer):
@@ -34,7 +34,7 @@ def test_create_session_by_passing_ip_and_port(
     health_pb2_grpc.add_HealthServicer_to_server(MockHealthServicer(), server)
     monkeypatch.setenv("PYFLUENT_LAUNCHED_FROM_FLUENT", "1")
     server.start()
-    session = Session(ip=ip, port=port, cleanup_on_exit=False)
+    session = BaseSession(ip=ip, port=port, cleanup_on_exit=False)
     assert session.check_health() == HealthCheckService.Status.SERVING.name
     server.stop(None)
     session.exit()
@@ -53,7 +53,7 @@ def test_create_session_by_setting_ip_and_port_env_var(
     server.start()
     monkeypatch.setenv("PYFLUENT_FLUENT_IP", ip)
     monkeypatch.setenv("PYFLUENT_FLUENT_PORT", str(port))
-    session = Session(cleanup_on_exit=False)
+    session = BaseSession(cleanup_on_exit=False)
     assert session.check_health() == HealthCheckService.Status.SERVING.name
     server.stop(None)
     session.exit()
@@ -71,7 +71,7 @@ def test_create_session_by_passing_grpc_channel(
     monkeypatch.setenv("PYFLUENT_LAUNCHED_FROM_FLUENT", "1")
     server.start()
     channel = grpc.insecure_channel(f"{ip}:{port}")
-    session = Session(channel=channel, cleanup_on_exit=False)
+    session = BaseSession(channel=channel, cleanup_on_exit=False)
     assert session.check_health() == HealthCheckService.Status.SERVING.name
     server.stop(None)
     session.exit()
@@ -87,8 +87,8 @@ def test_create_session_from_server_info_file(tmp_path: Path) -> None:
     server.start()
     server_info_file = tmp_path / "server_info.txt"
     server_info_file.write_text(f"{ip}:{port}\n12345")
-    session = Session.create_from_server_info_file(
-        server_info_filepath=server_info_file, cleanup_on_exit=False
+    session = BaseSession.create_from_server_info_file(
+        server_info_filepath=str(server_info_file), cleanup_on_exit=False
     )
     assert session.check_health() == HealthCheckService.Status.SERVING.name
     server.stop(None)
@@ -107,8 +107,8 @@ def test_create_session_from_server_info_file_with_wrong_password(
     server.start()
     server_info_file = tmp_path / "server_info.txt"
     server_info_file.write_text(f"{ip}:{port}\n1234")
-    session = Session.create_from_server_info_file(
-        server_info_filepath=server_info_file, cleanup_on_exit=False
+    session = BaseSession.create_from_server_info_file(
+        server_info_filepath=str(server_info_file), cleanup_on_exit=False
     )
     assert session.check_health() == HealthCheckService.Status.NOT_SERVING.name
     server.stop(None)
