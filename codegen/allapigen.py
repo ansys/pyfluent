fcf1652c407ce733d89235f9bd0bd8255d358013
ANSYS\spearson <sean.pearson@ansys.com>
2022-08-15 11:20:32 +0100

    reversion

--- codegen/allapigen.py
+++ codegen/allapigen.py
@@ -5,8 +5,6 @@ import tuigen
 
 if __name__ == "__main__":
     print_fluent_version.generate()
-    for mod in (tuigen, datamodelgen, settingsgen):
-        try:
-            mod.generate()
-        except BaseException as ex:
-            print("skipping generation of", str(mod), str(ex))
+    tuigen.generate()
+    datamodelgen.generate()
+    settingsgen.generate()
