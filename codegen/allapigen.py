ea78a02ab3d7c0f641c6f6f541d6cecdb1d8cd31
Patrick Lagace <patrick.lagace@ansys.com>
2022-08-24 16:42:03 -0400

    command line argument for codegen, to permit version switching without env var

--- codegen/allapigen.py
+++ codegen/allapigen.py
@@ -1,9 +1,33 @@
+import argparse
+
 import datamodelgen
 import print_fluent_version
 import settingsgen
 import tuigen
 
+from ansys.fluent.core.launcher.launcher import FLUENT_VERSION, set_ansys_version
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate python code from Fluent APIs"
+    )
+    parser.add_argument(
+        "--ansys-version",
+        dest="ansys_version",
+        help=f"Specify the ansys package version to use, default is {FLUENT_VERSION[0]}",
+    )
+    parser.add_argument(
+        "--fluent-path",
+        dest="fluent_path",
+        help="Specify the fluent/ folder to use, with full path.",
+    )
+    args = parser.parse_args()
+
+    if args.ansys_version:
+        set_ansys_version(args.ansys_version)
+    if args.fluent_path:
+        os.environ["PYFLUENT_FLUENT_ROOT"] = args.fluent_path
+
     print_fluent_version.generate()
     tuigen.generate()
     datamodelgen.generate()
