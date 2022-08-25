d8e24f1fe118cb50e2867903f805bd00b945d833
Dan Williams <dan.williams@ansys.com>
2022-08-25 16:07:35 -0400

    f me

--- src/ansys/fluent/core/launcher/load_machines.py
+++ src/ansys/fluent/core/launcher/load_machines.py
@@ -12,18 +12,18 @@ import subprocess
 from ansys.fluent.core.launcher.machine_list import Machine, MachineList
 
 
-def load_machines(machineDict=None, hostInfo=None, ncores=None):
+def load_machines(machine_info=None, host_info=None, ncores=None):
     """Provide a function to construct a machine list from allocated machines.
 
     Parameters
     ----------
-    machineDict : dict[str, int]
+    machine_info : list[dict[str, int]]
         Optional list of machines provided by the caller.
         This is a list of dictionaries of the form:
             [{'machine-name' : <m-name-1>, 'core-count' : <int>},
              {'machine-name' : <m-name-2>, 'core-count' : <int>},
              ... ]
-    hostInfo : str
+    host_info : str
         Optional host file name or list of machines and cores as a string
         separated by commas and colons.
         Example 1:  'M0:3,M1:2'
@@ -53,27 +53,27 @@ def load_machines(machineDict=None, hostInfo=None, ncores=None):
     host, if not, get 'actual' machine names using scontrol.
     """
 
-    machineList = []
+    machine_list = []
 
-    if machineDict:
-        machineList = _construct_machine_list_manual(machineDict)
-    elif hostInfo:
-        machineList = _parse_host_info(hostInfo)
+    if machine_info:
+        machine_list = _construct_machine_list_manual(machine_info)
+    elif host_info:
+        machine_list = _parse_host_info(host_info)
     elif "PE_HOSTFILE" in os.environ:
         hostFileName = os.environ.get("PE_HOSTFILE")
-        machineList = _construct_machine_list_uge(hostFileName)
+        machine_list = _construct_machine_list_uge(hostFileName)
     elif "LSB_MCPU_HOSTS" in os.environ:
         hostList = os.environ.get("LSB_MCPU_HOSTS")
-        machineList = _construct_machine_list_lsf(hostList)
+        machine_list = _construct_machine_list_lsf(hostList)
     elif "PBS_NODEFILE" in os.environ:
         hostFileName = os.environ.get("PBS_NODEFILE")
-        machineList = _construct_machine_list_pbs(hostFileName)
+        machine_list = _construct_machine_list_pbs(hostFileName)
     elif "SLURM_JOB_NODELIST" in os.environ:
         hostList = os.environ.get("SLURM_JOB_NODELIST")
-        machineList = _construct_machine_list_slurm(hostList)
+        machine_list = _construct_machine_list_slurm(hostList)
         sshTest = (
             "ssh "
-            + str(machineList.machines[0].hostName)
+            + str(machine_list.machines[0].host_name)
             + " /bin/true > /dev/null 2>&1; echo $?"
         )
         p = subprocess.Popen(sshTest, shell=True, stdout=subprocess.PIPE)
@@ -88,22 +88,22 @@ def load_machines(machineDict=None, hostInfo=None, ncores=None):
             hostList = procOutput[0].decode("utf-8").replace("\n", ",")
             length = len(hostList)
             hostList = hostList[0 : length - 1]
-            machineList = _construct_machine_list_slurm(hostList)
+            machine_list = _construct_machine_list_slurm(hostList)
     elif "CCP_NODES" in os.environ:
         hostList = os.environ.get("CCP_NODES")
-        machineList = _construct_machine_list_ccs(hostList)
+        machine_list = _construct_machine_list_ccs(hostList)
     elif ncores:
-        machineList = _get_local_machine(ncores)
+        machine_list = _get_local_machine(ncores)
 
-    if machineList and ncores:
+    if machine_list and ncores:
         # If both machine list and number of cores are provided, edit the
         # machine list to use exactly the number of cores indicated.
-        machineList = _restrict_machines_to_core_count(machineList, ncores)
+        machine_list = _restrict_machines_to_core_count(machine_list, ncores)
 
-    return machineList
+    return machine_list
 
 
-def _parse_host_info(hostInfo):
+def _parse_host_info(host_info):
     """Parse the host machine information from command-line arguments.
 
     Returns
@@ -114,30 +114,30 @@ def _parse_host_info(hostInfo):
     """
 
     if (
-        (":" in hostInfo or "," in hostInfo)
-        and not "\\" in hostInfo
-        and not "/" in hostInfo
+        (":" in host_info or "," in host_info)
+        and not "\\" in host_info
+        and not "/" in host_info
     ):
         # Filenames generally shouldn't have ':',
         # so assume it's a string list and parse accordingly
-        sMod = 1 if hostInfo[0] == "[" else 0
+        sMod = 1 if host_info[0] == "[" else 0
         sBeg = sMod
-        sEnd = len(hostInfo) - sMod
-        machineData = hostInfo[sBeg:sEnd].split(",")
+        sEnd = len(host_info) - sMod
+        machine_data = host_info[sBeg:sEnd].split(",")
     else:
         # Read from the file
-        with open(hostInfo, "r") as f:
-            machineData = f.read().splitlines()
+        with open(host_info, "r") as f:
+            machine_data = f.read().splitlines()
 
-    return _parse_machine_data(machineData)
+    return _parse_machine_data(machine_data)
 
 
-def _parse_machine_data(machineData):
+def _parse_machine_data(machine_data):
     """Parse the host machine data provided as a list of strings.
 
     Parameters
     ----------
-    machineData : list[str]
+    machine_data : list[str]
         The data can be a list of machines such as:
             ["M0","M1","M1"]
         or it can include cores as well:
@@ -153,7 +153,7 @@ def _parse_machine_data(machineData):
 
     machineList = MachineList()
 
-    for datum in machineData:
+    for datum in machine_data:
         # Parse machine name and core count
         if ":" in datum:
             # Machine and core given
@@ -170,8 +170,8 @@ def _parse_machine_data(machineData):
 
         # Add to existing machine if already in the list
         for machine in machineList.machines:
-            if machine.hostName == machineName:
-                machine.numberOfCores += ncores
+            if machine.host_name == machineName:
+                machine.number_of_cores += ncores
                 break
         else:  # machine name not already in machineList
             machineList.add(Machine(machineName, ncores))
@@ -192,13 +192,13 @@ def _get_local_machine(ncores):
     return machineList
 
 
-def _restrict_machines_to_core_count(oldMachineList, ncores):
+def _restrict_machines_to_core_count(old_machine_list, ncores):
     """Provide private module function to adjust the number of cores used per
     machine based on a user-supplied core count.
 
     Parameters
     ----------
-    oldMachineList : MachineList
+    old_machine_list : MachineList
         List of machines to be modified.
     ncores : int
         Updated core count.
@@ -218,30 +218,30 @@ def _restrict_machines_to_core_count(oldMachineList, ncores):
     returned machine list will be identical to the input.
     """
 
-    if ncores >= oldMachineList.numberOfCores:
-        return oldMachineList
+    if ncores >= old_machine_list.number_of_cores:
+        return old_machine_list
 
     # Get indices ordering the machines from largest to smallest core count
     machineListOrder = [
         i[0]
         for i in sorted(
-            enumerate(oldMachineList.machines),
-            key=lambda x: x[1].numberOfCores,
+            enumerate(old_machine_list.machines),
+            key=lambda x: x[1].number_of_cores,
             reverse=True,
         )
     ]
 
     newMachineList = MachineList()
-    for m in oldMachineList.machines:
-        newMachineList.add(Machine(m.hostName, 0, m.queueName, m.coreList))
+    for m in old_machine_list.machines:
+        newMachineList.add(Machine(m.host_name, 0, m.queue_name, m.core_list))
 
     ncoresRemain = ncores
     while ncoresRemain != 0:
         for i in machineListOrder:
-            oldMachine = oldMachineList.machines[i]
-            if oldMachine.numberOfCores != 0 and ncoresRemain != 0:
-                newMachineList.machines[i].numberOfCores += 1
-                oldMachine.numberOfCores -= 1
+            oldMachine = old_machine_list.machines[i]
+            if oldMachine.number_of_cores != 0 and ncoresRemain != 0:
+                newMachineList.machines[i].number_of_cores += 1
+                oldMachine.number_of_cores -= 1
                 ncoresRemain -= 1
                 if ncoresRemain == 0:
                     break
@@ -249,11 +249,11 @@ def _restrict_machines_to_core_count(oldMachineList, ncores):
     return newMachineList
 
 
-def _construct_machine_list_uge(hostFileName):
+def _construct_machine_list_uge(host_filename):
     """Provide private module function to parse the UGE host file."""
     csv.register_dialect("pemachines", delimiter=" ", skipinitialspace=True)
     machineList = MachineList()
-    with open(hostFileName, "r") as peFile:
+    with open(host_filename, "r") as peFile:
         peReader = csv.reader(peFile, dialect="pemachines")
         for row in peReader:
             if len(row) == 0:
@@ -263,10 +263,10 @@ def _construct_machine_list_uge(hostFileName):
     return machineList
 
 
-def _construct_machine_list_lsf(hostList):
+def _construct_machine_list_lsf(host_list):
     """Provide private module function to parse the LSF host list."""
     machineList = MachineList()
-    splitHostList = hostList.split()
+    splitHostList = host_list.split()
     im = 0
     while im < len(splitHostList):
         machineList.add(Machine(splitHostList[im], int(splitHostList[im + 1])))
@@ -274,17 +274,17 @@ def _construct_machine_list_lsf(hostList):
     return machineList
 
 
-def _construct_machine_list_pbs(hostFileName):
+def _construct_machine_list_pbs(host_filename):
     """Provide private module function to parse the PBS host file."""
     # PBS_NODE file has one machine name per line per core allocated on the machine.
     # It's identical to a Fluent host file format.  This code accumulates the total
     # core count on each machine.
     machineDict = {}
-    with open(hostFileName, "r") as pbsFile:
+    with open(host_filename, "r") as pbsFile:
         for hostname in pbsFile:
             hostname = hostname.rstrip("\r\n")
             if hostname in machineDict:
-                machineDict[hostname].numberOfCores += 1
+                machineDict[hostname].number_of_cores += 1
             else:
                 machineDict[hostname] = Machine(hostname, 1)
 
@@ -295,7 +295,7 @@ def _construct_machine_list_pbs(hostFileName):
     return machineList
 
 
-def _construct_machine_list_slurm(hostList):
+def _construct_machine_list_slurm(host_list):
     """Provide a private module function to parse the SLURM host and task
     lists.
 
@@ -320,7 +320,7 @@ def _construct_machine_list_slurm(hostList):
     import re
 
     machineList = MachineList()
-    splitHostList = hostList.split(",")
+    splitHostList = host_list.split(",")
     coresPerMachine = 1
     ntasksPerNodeSet = False
     if "SLURM_NTASKS_PER_NODE" in os.environ:
@@ -433,24 +433,24 @@ def _construct_machine_list_slurm(hostList):
                 coresPerMachine.append(int(numcores))
         icores = 0
         for machine in machineList.machines:
-            machine.numberOfCores = coresPerMachine[icores]
+            machine.number_of_cores = coresPerMachine[icores]
             icores += 1
 
     return machineList
 
 
-def _construct_machine_list_ccs(hostList):
+def _construct_machine_list_ccs(host_list):
     """Provide private module function to parse the Windows HPC/CCS host list.
 
     Parameters
     ----------
-    hostList : str
+    host_list : str
         A single string with the following format:
 
         "#hosts host1 #cores1 host2 #cores2 host3 #cores3 ... hostN #coresN"
     """
     machineList = MachineList()
-    splitHostList = hostList.split()
+    splitHostList = host_list.split()
     numMachines = int(splitHostList[0])
     im = 1
     for _ in range(numMachines):
@@ -459,10 +459,10 @@ def _construct_machine_list_ccs(hostList):
     return machineList
 
 
-def _construct_machine_list_manual(machineDict):
-    """Provide a private module function to convert a machine list dictionary
+def _construct_machine_list_manual(machine_info):
+    """Provide a private module function to convert a machine information list
     into a list of machine objects."""
     machineList = MachineList()
-    for m in machineDict:
+    for m in machine_info:
         machineList.add(Machine(m["machine-name"], m["core-count"]))
     return machineList
