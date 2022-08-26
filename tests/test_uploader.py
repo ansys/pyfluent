374dc7ff9436eef6e739a4c20818c5dd0c9dd535
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-08-26 14:28:44 +0530

    Used pytest.fixtures in test

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -1,11 +1,10 @@
 import pytest
 
 import ansys.fluent.core as pyfluent
-from ansys.fluent.core import launch_fluent
 
 
-def test_base_session():
-    base_session = launch_fluent(cleanup_on_exit=False, mode="meshing")
+def test_base_session(new_mesh_session):
+    base_session = new_mesh_session
     with pytest.raises(Exception) as e_info:
         base_session._upload(
             pyfluent.EXAMPLES_PATH + "/mixing_elbow.py", "test_upload_download.py"
@@ -14,8 +13,8 @@ def test_base_session():
     base_session.exit()
 
 
-def test_session():
-    session = launch_fluent(cleanup_on_exit=False, meshing_mode=True)
+def test_session(new_mesh_session):
+    session = new_mesh_session
     with pytest.raises(Exception) as e_info:
         session._upload(
             pyfluent.EXAMPLES_PATH + "/mixing_elbow.py", "test_upload_download.py"
