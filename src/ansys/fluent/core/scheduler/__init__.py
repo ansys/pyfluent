db6eb5ba74167a0a7a9d0e0e69e891acf38745a2
Dan Williams <dan.williams@ansys.com>
2022-09-06 17:47:10 -0400

    Add function to build Fluent parallel options

--- src/ansys/fluent/core/scheduler/__init__.py
+++ src/ansys/fluent/core/scheduler/__init__.py
@@ -1,3 +1,24 @@
 """A package providing job scheduler support."""
 
-from .load_machines import load_machines  # noqa: F401
+from ansys.fluent.core.scheduler.load_machines import load_machines  # noqa: F401
+from ansys.fluent.core.scheduler.machine_list import MachineList
+
+_fluentOpt = "-t%n% -cnf=%machineList%"
+_procSep = ":"
+_machineSep = ","
+
+
+def build_parallel_options(machine_list: MachineList) -> str:
+    parOpt = _fluentOpt.replace("%n%", str(machine_list.number_of_cores))
+    cnfList = (
+        machine_list[0].host_name + _procSep + str(machine_list[0].number_of_cores)
+    )
+    for m in range(1, len(machine_list)):
+        cnfList += (
+            _machineSep
+            + machine_list[m].host_name
+            + _procSep
+            + str(machine_list[m].number_of_cores)
+        )
+    parOpt = parOpt.replace("%machineList%", cnfList)
+    return parOpt
