3239b81016ac07998dd56ecc0cb8fd5516734a8d
Dan Williams <dan.williams@ansys.com>
2022-09-12 17:09:50 -0400

    Specialize the options builder for serial and local parallel

--- src/ansys/fluent/core/scheduler/__init__.py
+++ src/ansys/fluent/core/scheduler/__init__.py
@@ -1,9 +1,12 @@
 """A package providing job scheduler support."""
 
+import socket
+
 from ansys.fluent.core.scheduler.load_machines import load_machines  # noqa: F401
 from ansys.fluent.core.scheduler.machine_list import MachineList
 
-_fluentOpt = "-t%n% -cnf=%machineList%"
+_ncoresOpt = "-t%n%"
+_machinesOpt = " -cnf=%machineList%"
 _procSep = ":"
 _machineSep = ","
 
@@ -15,17 +18,33 @@ def build_parallel_options(machine_list: MachineList) -> str:
     ----------
     machine_list : MachineList
         List of machines obtained by calling `load_machines`.
+
+    Notes
+    -----
+    When running serial no options are passed back to the caller as Fluent can
+    be started without additional arguments in that case.
+
+    If the parallel options are being built on the same machine as Fluent is run
+    on, and it's local parallel, then the -cnf argument is not constructed.
     """
-    parOpt = _fluentOpt.replace("%n%", str(machine_list.number_of_cores))
-    cnfList = (
-        machine_list[0].host_name + _procSep + str(machine_list[0].number_of_cores)
-    )
-    for m in range(1, len(machine_list)):
-        cnfList += (
-            _machineSep
-            + machine_list[m].host_name
-            + _procSep
-            + str(machine_list[m].number_of_cores)
+    parOpt = ""
+    if (
+        machine_list.num_machines == 1
+        and socket.gethostname() == machine_list[0].host_name
+    ):
+        if machine_list.number_of_cores > 1:
+            parOpt = _ncoresOpt.replace("%n%", str(machine_list.number_of_cores))
+    else:
+        parOpt = _ncoresOpt.replace("%n%", str(machine_list.number_of_cores))
+        cnfList = (
+            machine_list[0].host_name + _procSep + str(machine_list[0].number_of_cores)
         )
-    parOpt = parOpt.replace("%machineList%", cnfList)
+        for m in range(1, len(machine_list)):
+            cnfList += (
+                _machineSep
+                + machine_list[m].host_name
+                + _procSep
+                + str(machine_list[m].number_of_cores)
+            )
+        parOpt += _machinesOpt.replace("%machineList%", cnfList)
     return parOpt
