de705978e0cd635933750f21aa8c9bb38501ea96
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-09-12 18:31:00 +0530

    Updated preferences test

--- tests/test_preferences.py
+++ tests/test_preferences.py
@@ -8,6 +8,17 @@ def test_solver_preferences(new_solver_session):
     solver.preferences.MeshingWorkflow.Verbosity = 10.5
     assert solver.preferences.MeshingWorkflow.Verbosity() == "10.5"
 
+    solver.preferences.MeshingWorkflow.CheckpointingOption = "Write into memory"
+    assert (
+        solver.preferences.MeshingWorkflow.CheckpointingOption() == "Write into memory"
+    )
+
+    solver.preferences.MeshingWorkflow.DrawSettings.FacetLimit = 6000000
+    assert solver.preferences.MeshingWorkflow.DrawSettings.FacetLimit() == 6000000
+
+    solver.preferences.MeshingWorkflow.DrawSettings.FaceZoneLimit = 15000
+    solver.preferences.MeshingWorkflow.DrawSettings.FaceZoneLimit() == 15000
+
     solver.preferences.Appearance.AnsysLogo.Color = "white"
     assert solver.preferences.Appearance.AnsysLogo.Color() == "white"
 
@@ -16,6 +27,9 @@ def test_solver_preferences(new_solver_session):
 
     solver.preferences.Appearance.AnsysLogo.Visible = True
     assert solver.preferences.Appearance.AnsysLogo.Visible() is True
+
+    solver.preferences.Graphics.AnimationOption = "wireframe"
+    assert solver.preferences.Graphics.AnimationOption() == "wireframe"
 
     solver.exit()
 
@@ -26,6 +40,17 @@ def test_meshing_test_solver_preferences(new_mesh_session):
     meshing.preferences.MeshingWorkflow.Verbosity = 15.5
     assert meshing.preferences.MeshingWorkflow.Verbosity() == "15.5"
 
+    meshing.preferences.MeshingWorkflow.CheckpointingOption = "Write into memory"
+    assert (
+        meshing.preferences.MeshingWorkflow.CheckpointingOption() == "Write into memory"
+    )
+
+    meshing.preferences.MeshingWorkflow.DrawSettings.FacetLimit = 6000000
+    assert meshing.preferences.MeshingWorkflow.DrawSettings.FacetLimit() == 6000000
+
+    meshing.preferences.MeshingWorkflow.DrawSettings.FaceZoneLimit = 15000
+    meshing.preferences.MeshingWorkflow.DrawSettings.FaceZoneLimit() == 15000
+
     meshing.preferences.Appearance.AnsysLogo.Color = "white"
     assert meshing.preferences.Appearance.AnsysLogo.Color() == "white"
 
@@ -34,5 +59,8 @@ def test_meshing_test_solver_preferences(new_mesh_session):
 
     meshing.preferences.Appearance.AnsysLogo.Visible = True
     assert meshing.preferences.Appearance.AnsysLogo.Visible() is True
+
+    meshing.preferences.Graphics.AnimationOption = "wireframe"
+    assert meshing.preferences.Graphics.AnimationOption() == "wireframe"
 
     meshing.exit()
