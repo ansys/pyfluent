c45e1cdf2a5e724da6f937409cfcedd83631f9bf
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-09-12 15:57:23 +0530

    Fixtures are used.

--- tests/test_preferences.py
+++ tests/test_preferences.py
@@ -1,14 +1,18 @@
+from util.meshing_workflow import new_mesh_session  # noqa: F401
+from util.solver_workflow import new_solver_session  # noqa: F401
+
 import ansys.fluent.core as pyfluent
 
 
-def test_solver_verbosity():
-    solver = pyfluent.launch_fluent(mode="solver")
+def test_solver_verbosity(new_solver_session):
+    solver = new_solver_session
     solver.preferences.MeshingWorkflow.Verbosity = 10.5
     assert solver.preferences.MeshingWorkflow.Verbosity() == "10.5"
     solver.exit()
 
 
-def test_meshing_verbosity():
+def test_meshing_verbosity(new_mesh_session):
+    meshing = new_mesh_session
     meshing = pyfluent.launch_fluent(mode="meshing")
     meshing.preferences.MeshingWorkflow.Verbosity = 15.5
     assert meshing.preferences.MeshingWorkflow.Verbosity() == "15.5"
