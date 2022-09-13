b48d6c4357a2f7b9d1d4022db482367d25638d1b
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-09-13 22:03:13 +0530

    Setup for fluent

--- src/ansys/fluent/core/__main__.py
+++ src/ansys/fluent/core/__main__.py
@@ -1,28 +1,29 @@
-import sys
 import os
+import sys
 
-from ansys.fluent.core.session_solver import Solver
 from ansys.fluent.core.launcher.launcher import launch_fluent
+from ansys.fluent.core.session_solver import Solver
+
 
 def setup_for_fluent(version: str, mode: str):
-    """Uses global PyConsole objects"""
+    """Uses global PyConsole objects."""
     session = launch_fluent(version=version, mode=mode)
-    if mode == 'meshing':
-        globals()['meshing'] = session
-        globals()['workflow'] = session.workflow
-        globals()['solver'] = Solver(fluent_connection=session.fluent_connection)
-    elif mode == 'solver':
-        globals()['solver'] = session
+    if mode == "meshing":
+        globals()["meshing"] = session
+        globals()["workflow"] = session.workflow
+        globals()["solver"] = Solver(fluent_connection=session.fluent_connection)
+    elif mode == "solver":
+        globals()["solver"] = session
 
-# File name parsing from command line arguments
+
+# File name parsing and line by line execution
 if len(sys.argv) > 1:
-    file_path = ''
+    file_path = ""
     for arg in sys.argv[1:]:
-        if type(arg) == str and arg.endswith('.py') and os.path.exists(arg):
+        if type(arg) == str and arg.endswith(".py") and os.path.exists(arg):
             file_path = arg
             break
-    if file_path != '':
-        with open(file_path, 'r') as filename:
+    if file_path != "":
+        with open(file_path, "r") as filename:
             for line in filename.readlines():
                 exec(line)
-
