4f4749fc98ad95a3a892dbf381683df355876582
ANSYS\spearson <sean.pearson@ansys.com>
2022-09-13 18:00:23 +0100

    mesh transfer

--- src/ansys/fluent/core/session_pure_meshing.py
+++ src/ansys/fluent/core/session_pure_meshing.py
@@ -4,12 +4,12 @@
 """
 
 import os
-import time
 
 from ansys.fluent.core.fluent_connection import _FluentConnection
 from ansys.fluent.core.session import _BaseSession
 from ansys.fluent.core.session_base_meshing import _BaseMeshing
 from ansys.fluent.core.utils.async_execution import asynchronous
+from ansys.fluent.core.utils.logging import LOG
 
 
 class PureMeshing(_BaseSession):
@@ -61,7 +61,7 @@ class PureMeshing(_BaseSession):
         file_name_stem=None,
         num_files_to_try=1,
         clean_up_mesh_file=True,
-        sleep_time=0,
+        overwrite_previous=True,
     ):
         _transfer_mesh_from_meshing_to_solvers(
             self,
@@ -69,45 +69,47 @@ class PureMeshing(_BaseSession):
             file_name_stem,
             num_files_to_try,
             clean_up_mesh_file,
-            sleep_time,
+            overwrite_previous,
         )
 
 
 @asynchronous
-def _read_mesh_into_solver(file_name, solver, sleep_time):
-    print("read in solver")
+def _read_mesh_into_solver(file_name, solver):
+    LOG.info(f"Trying to read mesh in solver: {file_name}")
     solver.upload(file_name)
-    time.sleep(sleep_time)
-    solver.tui.file.read_case(file_name)  # make this asynchronous
-    print("have read in solver")
+    solver.tui.file.read_case(file_name)
+    LOG.info(f"Have read mesh in solver: {file_name}")
 
 
-def _read_mesh_into_solvers(file_name, solvers, sleep_time):
+def _read_mesh_into_solvers(file_name, solvers):
     reads = []
     for solver in solvers:
-        reads.append(_read_mesh_into_solver(file_name, solver, sleep_time))
+        reads.append(_read_mesh_into_solver(file_name, solver))
     for r in reads:
         r.result()
 
 
 def _transfer_mesh_from_meshing_to_solvers(
-    meshing, solvers, file_name_stem, num_files_to_try, clean_up_mesh_file, sleep_time
+    meshing,
+    solvers,
+    file_name_stem,
+    num_files_to_try,
+    clean_up_mesh_file,
+    overwrite_previous,
 ):
     file_ext = ".msh.cas.h5"
     for idx in range(num_files_to_try):
         file_name = (file_name_stem or "fluent_mesh_") + "_" + str(idx) + file_ext
         folder = os.getenv("TMP", os.getenv("TMPDIR", "."))
         file_name = os.path.join(folder, file_name)
-        print(file_name)
-        if not os.path.isfile(file_name):
-            print("save mesh from meshing")
-            meshing.tui.file.write_case(file_name)
-            time.sleep(sleep_time)
+        LOG.info(f"Trying to save mesh from meshing session: {file_name}")
+        if overwrite_previous or not os.path.isfile(file_name):
+            LOG.info(f"Saving mesh from meshing session: {file_name}")
+            meshing.tui.file.write_case(file_name, "y")
             meshing.download(file_name, ".")
-            time.sleep(sleep_time)
-            print("saved mesh from meshing")
-            _read_mesh_into_solvers(file_name, solvers, sleep_time)
+            LOG.info(f"Saved mesh from meshing session: {file_name}")
+            _read_mesh_into_solvers(file_name, solvers)
             if clean_up_mesh_file:
                 os.remove(file_name)
             return
-    raise RuntimeError("Could not write mesh to transfer")
+    raise RuntimeError("Could not write mesh from meshing session.")
