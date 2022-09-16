22a13216252dc37219186e4f9f61549d16629485
Anurag Kulshrestha <103188356+akulshre-qa@users.noreply.github.com>
2022-09-16 15:00:33 +0530

    Added reading and writing journal scenario and pure meshing mode (#887)

---
+++ tests/test_meshingmode/test_meshing_launch.py
@@ -0,0 +1,73 @@
+import os
+
+import pytest
+from util.fixture_fluent import download_input_file
+
+import ansys.fluent.core as pyfluent
+
+
+@pytest.mark.meshing
+@pytest.mark.fluent_231
+def test_launch_pure_meshing(load_mixing_elbow_pure_meshing):
+    pure_meshing_session = load_mixing_elbow_pure_meshing
+    assert pure_meshing_session.check_health() == "SERVING"
+    file_path = os.path.join(pyfluent.EXAMPLES_PATH, "launch_pure_meshing_journal.py")
+    pure_meshing_session.setup_python_console_in_tui()
+    pure_meshing_session.start_journal(file_path)
+    session_dir = dir(pure_meshing_session)
+    for attr in ("field_data", "field_info", "meshing", "workflow"):
+        assert attr in session_dir
+    workflow = pure_meshing_session.workflow
+    workflow.TaskObject["Import Geometry"].Execute()
+    workflow.TaskObject["Add Local Sizing"].AddChildToTask()
+    workflow.TaskObject["Add Local Sizing"].Execute()
+    workflow.TaskObject["Generate the Surface Mesh"].Arguments = {
+        "CFDSurfaceMeshControls": {"MaxSize": 0.3}
+    }
+    workflow.TaskObject["Generate the Surface Mesh"].Execute()
+    workflow.TaskObject["Describe Geometry"].UpdateChildTasks(SetupTypeChanged=False)
+    workflow.TaskObject["Describe Geometry"].Arguments = dict(
+        SetupType="The geometry consists of only fluid regions with no voids"
+    )
+    workflow.TaskObject["Describe Geometry"].UpdateChildTasks(SetupTypeChanged=True)
+    workflow.TaskObject["Describe Geometry"].Execute()
+    workflow.TaskObject["Update Boundaries"].Arguments = {
+        "BoundaryLabelList": ["wall-inlet"],
+        "BoundaryLabelTypeList": ["wall"],
+        "OldBoundaryLabelList": ["wall-inlet"],
+        "OldBoundaryLabelTypeList": ["velocity-inlet"],
+    }
+    workflow.TaskObject["Update Boundaries"].Execute()
+    workflow.TaskObject["Update Regions"].Execute()
+    workflow.TaskObject["Add Boundary Layers"].AddChildToTask()
+    workflow.TaskObject["Add Boundary Layers"].InsertCompoundChildTask()
+    workflow.TaskObject["smooth-transition_1"].Arguments = {
+        "BLControlName": "smooth-transition_1",
+    }
+    workflow.TaskObject["Add Boundary Layers"].Arguments = {}
+    workflow.TaskObject["smooth-transition_1"].Execute()
+    workflow.TaskObject["Generate the Volume Mesh"].Arguments = {
+        "VolumeFill": "poly-hexcore",
+        "VolumeFillControls": {
+            "HexMaxCellLength": 0.3,
+        },
+    }
+    workflow.TaskObject["Generate the Volume Mesh"].Execute()
+    pure_meshing_session.stop_journal()
+    with open(file_path) as fp:
+        for count, line in enumerate(fp):
+            pass
+    assert count == 17
+    fp.close()
+    with pytest.raises(AttributeError):
+        pure_meshing_session.switch_to_solver()
+    pure_meshing_session.workflow.InitializeWorkflow(WorkflowType="Watertight Geometry")
+    input_type, input_name = download_input_file(
+        "pyfluent/mixing_elbow", "mixing_elbow.pmdb"
+    )
+    pure_meshing_session.workflow.TaskObject["Import Geometry"].Arguments = dict(
+        FileName=input_name, LengthUnit="in"
+    )
+    pure_meshing_session.execute_tui(r"""/file/read-journal %s""" % file_path)
+    if os.path.exists(file_path):
+        os.remove(file_path)
--- tests/test_solvermode/test_general.py
+++ tests/test_solvermode/test_general.py
@@ -1,4 +1,8 @@
+import os
+
 import pytest
+
+import ansys.fluent.core as pyfluent
 
 
 @pytest.mark.quick
@@ -6,8 +10,11 @@ import pytest
 @pytest.mark.fluent_231
 def test_solver_import_mixingelbow(load_mixing_elbow_mesh):
     solver_session = load_mixing_elbow_mesh
-    assert solver_session._root.get_attr("active?")
+    assert solver_session._root.is_active()
     assert solver_session.check_health() == "SERVING"
+    file_path = os.path.join(pyfluent.EXAMPLES_PATH, "jou_test_general.py")
+    solver_session.setup_python_console_in_tui()
+    solver_session.start_journal(file_path)
     ###
     assert not solver_session.setup.models.energy.enabled()
     assert solver_session.scheme_eval.scheme_eval("(case-valid?)")
@@ -18,7 +25,13 @@ def test_solver_import_mixingelbow(load_mixing_elbow_mesh):
         solver_session.scheme_eval.scheme_eval('(units/quantity-info "length")')[-1]
         == "in"
     )
-    solver_session.setup.general.solver.time.get_attr("allowed-values")
+    solver_session.setup.general.solver.time.allowed_values()
+    assert solver_session.setup.general.solver.time.allowed_values() == [
+        "steady",
+        "unsteady-1st-order",
+        "unsteady-2nd-order",
+        "unsteady-2nd-order-bounded",
+    ]
     solver_session.setup.general.solver.time = "unsteady-2nd-order"
     solver_session.setup.general.solver.time = "unsteady-1st-order"
     solver_session.setup.general.solver.time = "unsteady-2nd-order-bounded"
@@ -26,7 +39,19 @@ def test_solver_import_mixingelbow(load_mixing_elbow_mesh):
 
     # solver.setup.general.gravity = {"gravity": True, "y_component": -9.81}
     # solver.mesh.scale(x_scale=0.001, y_scale=0.001, z_scale=0.001)
-    solver_session.setup.general.solver.type.get_attr("allowed-values")
+    assert solver_session.setup.general.solver.type.get_attr("allowed-values") == [
+        "pressure-based",
+        "density-based-implicit",
+        "density-based-explicit",
+    ]
+    assert solver_session.setup.general.solver.type.allowed_values() == [
+        "pressure-based",
+        "density-based-implicit",
+        "density-based-explicit",
+    ]
+    assert solver_session.setup.general.solver.type.default_value() == "pressure-based"
+    assert solver_session.setup.general.solver.type.is_active()
+    assert not solver_session.setup.general.solver.type.is_read_only()
     solver_session.setup.general.solver.type = "density-based-implicit"
     assert solver_session.setup.general.solver.type() == "density-based-implicit"
     solver_session.setup.general.solver.type = "density-based-explicit"
@@ -35,12 +60,26 @@ def test_solver_import_mixingelbow(load_mixing_elbow_mesh):
     assert solver_session.setup.general.solver.type() == "pressure-based"
 
     solver_session.file.auto_save.data_frequency = 10
+    assert solver_session.file.auto_save.data_frequency.default_value() == 0
     assert solver_session.file.auto_save.data_frequency() == 10
     solver_session.file.auto_save.case_frequency = "each-time"
     assert solver_session.file.auto_save.case_frequency() == "each-time"
     solver_session.file.auto_save.root_name = "file_auto_save"
     assert solver_session.file.auto_save.root_name() == "file_auto_save"
     solver_session.setup.reference_values.compute(from_zone_name="outlet")
+    solver_session.stop_journal()
+    with open(file_path) as fp:
+        for count, line in enumerate(fp):
+            pass
+    assert count == 12
+    fp.close()
+    solver_session.execute_tui(r"""/file/read-journal %s""" % file_path)
+    assert solver_session.file.auto_save.root_name() == "file_auto_save"
+    assert solver_session.setup.general.solver.type() == "pressure-based"
+    assert solver_session.file.auto_save.data_frequency() == 10
+    assert solver_session.setup.general.solver.time() == "steady"
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
 
 @pytest.mark.quick
