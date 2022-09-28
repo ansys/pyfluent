5e49412672e8e14b378005fb418f5e9e63db4597
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-09-28 15:24:37 +0530

    Spell correction in attr

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -865,7 +865,7 @@ def get_cls(name, info, parent=None):
                     dct["__doc__"] = f"'{pname.strip('_')}' child."
 
         include_child_named_objects = info.get("include_child_named_objects", False)
-        user_creatable = info.get("user_creatable", True)
+        user_creatable = info.get("user-creatable?", True)
 
         bases = (base,)
         if include_child_named_objects:
