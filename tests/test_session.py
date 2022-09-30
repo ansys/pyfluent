5311aebb6d1a085648d27f7dd6f92c3ed972fa26
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-09-30 11:54:56 +0530

    Added test case to test the added feature

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,6 +1,7 @@
 from concurrent import futures
 import os
 from pathlib import Path
+import time
 
 import grpc
 import pytest
@@ -202,3 +203,28 @@ def test_get_fluent_mode(new_mesh_session):
     assert session.fluent_connection.get_current_fluent_mode() == "meshing"
     session = session.switch_to_solver()
     assert session.fluent_connection.get_current_fluent_mode() == "solver"
+
+
+@pytest.mark.dev
+@pytest.mark.fluent_231
+def test_start_transcript_file_write(new_mesh_session, tmp_path=pyfluent.EXAMPLES_PATH):
+    session = new_mesh_session
+    file_path = os.path.join(tmp_path, "sample_transcript.txt")
+
+    session.start_transcript(file_path)
+
+    session = session.switch_to_solver()
+
+    session.stop_transcript()
+
+    with open(file_path) as f:
+        returned = f.readlines()
+
+    assert returned[0].startswith("Transcript Start Time:")
+
+    session.exit()
+    while session.check_health() != "NOT_SERVING":
+        time.sleep(1)
+    time.sleep(1)
+    if os.path.exists(file_path):
+        os.remove(file_path)
