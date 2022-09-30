8dc7f4e99879f254b6056a1941d5552705c157f5
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-09-30 17:27:15 +0530

    Added test case for field data

---
+++ tests/test_field_data.py
@@ -0,0 +1,75 @@
+import pytest
+from util.solver_workflow import new_solver_session  # noqa: F401
+
+from ansys.fluent.core import examples
+
+
+@pytest.mark.fluent_231
+def test_field_data(new_solver_session) -> None:
+    solver = new_solver_session
+    import_filename = examples.download_file(
+        "mixing_elbow.msh.h5", "pyfluent/mixing_elbow"
+    )
+    solver.file.read(file_type="case", file_name=import_filename)
+    solver.tui.mesh.check()
+
+    solver.setup.models.energy.enabled = True
+    solver.setup.materials.database.copy_by_name(type="fluid", name="water-liquid")
+    solver.setup.cell_zone_conditions.fluid["elbow-fluid"].material = "water-liquid"
+
+    # Set up boundary conditions for CFD analysis
+    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = 0.4
+    solver.setup.boundary_conditions.velocity_inlet[
+        "cold-inlet"
+    ].ke_spec = "Intensity and Hydraulic Diameter"
+    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].turb_intensity = 0.05
+    solver.setup.boundary_conditions.velocity_inlet[
+        "cold-inlet"
+    ].turb_hydraulic_diam = "4 [in]"
+
+    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = 293.15
+
+    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = 1.2
+    solver.setup.boundary_conditions.velocity_inlet[
+        "hot-inlet"
+    ].ke_spec = "Intensity and Hydraulic Diameter"
+    solver.setup.boundary_conditions.velocity_inlet[
+        "hot-inlet"
+    ].turb_hydraulic_diam = "1 [in]"
+
+    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = 313.15
+
+    solver.setup.boundary_conditions.pressure_outlet["outlet"].turb_viscosity_ratio = 4
+
+    solver.tui.solve.monitors.residual.plot("no")
+
+    # Initialize flow field
+    solver.solution.initialization.hybrid_initialize()
+
+    solver.solution.run_calculation.iterate.get_attr("arguments")
+    solver.solution.run_calculation.iterate(iter_count=10)
+
+    # Get field data object
+    field_data = solver.field_data
+
+    transaction = field_data.new_transaction()
+
+    transaction.add_surfaces_request(
+        surface_ids=[1],
+        provide_vertices=True,
+        provide_faces=False,
+        provide_faces_centroid=True,
+    )
+    transaction.add_scalar_fields_request(
+        surface_ids=[1, 2],
+        field_name="temperature",
+        node_value=True,
+        boundary_value=True,
+    )
+
+    data = transaction.get_fields()
+
+    assert len(data) == 2
+    assert list(data[0][1].keys()) == ["vertices", "centroid"]
+    assert list(data[12][1].keys()) == ["temperature"]
+    assert len(data[12][1]["temperature"]) > 0
