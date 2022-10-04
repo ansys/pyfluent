973141e2cc762afbed3acc401790b9bbc5f0d67a
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-10-04 20:03:24 +0530

    Applied the lock effectively

--- src/ansys/fluent/core/transcript.py
+++ src/ansys/fluent/core/transcript.py
@@ -4,8 +4,6 @@ from typing import Callable, Optional
 
 from ansys.fluent.core.services.transcript import TranscriptService
 
-lock = threading.Lock()
-
 
 class Transcript:
     """Encapsulates a Fluent Transcript streaming service."""
@@ -16,8 +14,8 @@ class Transcript:
         self.transcript_service = TranscriptService(self._channel, self._metadata)
         self._transcript_thread: Optional[threading.Thread] = None
         self._transcript_callbacks = {}
-        self._id_count = itertools.count()
-        self._transcript_callback_id = next(self._id_count)
+        self._transcript_callback_id = itertools.count()
+        self.lock = threading.Lock()
 
     def add_transcript_callback(self, callback_fn: Callable, keep_new_lines=False):
         """Initiates a fluent transcript streaming depending on the
@@ -26,25 +24,27 @@ class Transcript:
         For eg.: add_transcript_callback(print) prints the transcript on
         the interpreter screen.
         """
-        with lock:
-            self._transcript_callbacks[self._transcript_callback_id] = (
+        with self.lock:
+            callback_id = next(self._transcript_callback_id)
+            self._transcript_callbacks[callback_id] = (
                 callback_fn,
                 keep_new_lines,
             )
-            returned_callback_id = self._transcript_callback_id
-            self._transcript_callback_id = next(self._id_count)
-        if len(self._transcript_callbacks) == 1:
+            start_thread = len(self._transcript_callbacks) == 1
+        if start_thread:
             self._transcript_thread = threading.Thread(
                 target=self._process_transcript,
                 args=(self.transcript_service,),
             )
             self._transcript_thread.start()
-        return returned_callback_id
+        return callback_id
 
     def remove_transcript_callback(self, callback_id):
         """Stops each transcript streaming based on the callback_id."""
-        del self._transcript_callbacks[callback_id]
-        if len(self._transcript_callbacks) == 0:
+        with self.lock:
+            del self._transcript_callbacks[callback_id]
+            stop_thread = len(self._transcript_callbacks) == 0
+        if stop_thread:
             self.transcript_service.end_streaming()
             self._transcript_thread.join()
 
@@ -58,14 +58,15 @@ class Transcript:
                 response = next(responses)
                 transcript += response.transcript
                 if transcript[-1] == "\n":
-                    for (
-                        callback_function,
-                        keep_new_lines,
-                    ) in self._transcript_callbacks.values():
-                        if keep_new_lines:
-                            callback_function(transcript)
-                        else:
-                            callback_function(transcript[0:-1])
+                    with self.lock:
+                        for (
+                            callback_function,
+                            keep_new_lines,
+                        ) in self._transcript_callbacks.values():
+                            if keep_new_lines:
+                                callback_function(transcript)
+                            else:
+                                callback_function(transcript[0:-1])
                     transcript = ""
             except StopIteration:
                 break
