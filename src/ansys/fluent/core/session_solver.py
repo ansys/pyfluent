ac416ed6ecfb43f72048206a5924f9782e50c7fc
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-10-04 10:25:48 +0530

    MeshingWorkflow -> WorkflowWrapper

--- src/ansys/fluent/core/session_solver.py
+++ src/ansys/fluent/core/session_solver.py
@@ -2,6 +2,7 @@
 
 import importlib
 
+from ansys.fluent.core.services.datamodel_se import PyMenuGeneric
 from ansys.fluent.core.services.datamodel_tui import TUIMenuGeneric
 from ansys.fluent.core.session import (
     _CODEGEN_MSG_TUI,
@@ -9,9 +10,11 @@ from ansys.fluent.core.session import (
     _get_preferences,
     _get_solverworkflow,
 )
+from ansys.fluent.core.session_shared import _CODEGEN_MSG_DATAMODEL
 from ansys.fluent.core.solver.flobject import get_root as settings_get_root
 from ansys.fluent.core.utils.fluent_version import get_version_for_filepath
 from ansys.fluent.core.utils.logging import LOG
+from ansys.fluent.core.workflow import WorkflowWrapper
 
 
 class Solver(_BaseSession):
@@ -28,6 +31,7 @@ class Solver(_BaseSession):
         self._se_service = fluent_connection.datamodel_service_se
         self._settings_service = self.fluent_connection.settings_service
         self._tui = None
+        self._workflow = None
         self._settings_root = None
         self._version = None
         self._solverworkflow = None
@@ -52,6 +56,25 @@ class Solver(_BaseSession):
                 LOG.warning(_CODEGEN_MSG_TUI)
                 self._tui = TUIMenuGeneric([], self._tui_service)
         return self._tui
+
+    @property
+    def _workflow_se(self):
+        """workflow datamodel root."""
+        try:
+            workflow_module = importlib.import_module(
+                f"ansys.fluent.core.datamodel_{self.version}.workflow"
+            )
+            workflow_se = workflow_module.Root(self._se_service, "workflow", [])
+        except (ImportError, ModuleNotFoundError):
+            LOG.warning(_CODEGEN_MSG_DATAMODEL)
+            workflow_se = PyMenuGeneric(self._se_service, "workflow")
+        return workflow_se
+
+    @property
+    def workflow(self):
+        if not self._workflow:
+            self._workflow = WorkflowWrapper(self._workflow_se, Solver)
+        return self._workflow
 
     @property
     def _root(self):
---
+++ src/ansys/fluent/core/workflow.py
@@ -0,0 +1,100 @@
+from ansys.fluent.core.services.datamodel_se import MakeReadOnly, PyCallableStateObject
+
+
+def _new_command_for_task(task, session):
+    class NewCommandError(Exception):
+        def __init__(self, task_name):
+            super().__init__(f"Could not create command for meshing task {task_name}")
+
+    task_cmd_name = task.CommandName()
+    cmd_creator = getattr(session, task_cmd_name)
+    if cmd_creator:
+        new_cmd = cmd_creator.new()
+        if new_cmd:
+            return new_cmd
+    raise NewCommandError(task._name_())
+
+
+class WorkflowWrapper:
+    class TaskContainer(PyCallableStateObject):
+        def __init__(self, session):
+            self._session_container = session
+            self._task_container = session._workflow.TaskObject
+
+        def __getitem__(self, name):
+            return WorkflowWrapper.Task(self._session_container, name)
+
+        def __getattr__(self, attr):
+            return getattr(self._task_container, attr)
+
+        def __dir__(self):
+            return sorted(
+                set(
+                    list(self.__dict__.keys())
+                    + dir(type(self))
+                    + dir(self._task_container)
+                )
+            )
+
+    class Task(PyCallableStateObject):
+        def __init__(self, session, name):
+            self.__dict__.update(
+                dict(
+                    _workflow=session._workflow,
+                    _meshing=session._meshing,
+                    _task=session._workflow.TaskObject[name],
+                    _cmd=None,
+                )
+            )
+
+        @property
+        def CommandArguments(self):
+            return self._refreshed_command()
+
+        def _refreshed_command(self):
+            task_arg_state = self.Arguments.get_state()
+            cmd = self._command()
+            if task_arg_state:
+                cmd.set_state(task_arg_state)
+            return MakeReadOnly(cmd)
+
+        def _command(self):
+            if not self._cmd:
+                self._cmd = _new_command_for_task(self._task, self._session)
+            return self._cmd
+
+        def __getattr__(self, attr):
+            return getattr(self._task, attr)
+
+        def __setattr__(self, attr, value):
+            if attr in self.__dict__:
+                self.__dict__[attr] = value
+            else:
+                setattr(self._task, attr, value)
+
+        def __dir__(self):
+            return sorted(
+                set(list(self.__dict__.keys()) + dir(type(self)) + dir(self._task))
+            )
+
+    def __init__(self, workflow, session):
+        self._workflow = workflow
+        self._session = session
+
+    def task(self, name):
+        return WorkflowWrapper.Task(self, name)
+
+    @property
+    def TaskObject(self):
+        return WorkflowWrapper.TaskContainer(self)
+
+    def __getattr__(self, attr):
+        return getattr(self._workflow, attr)
+
+    def __dir__(self):
+        return sorted(
+            set(list(self.__dict__.keys()) + dir(type(self)) + dir(self._workflow))
+        )
+
+    def __call__(self):
+        return self._workflow()
