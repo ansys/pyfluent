e1f93f51b3e2c62d958710412dbba7ea651c984a
Abhishek Chitwar <99339845+abhishekchitwar@users.noreply.github.com>
2022-10-10 13:59:06 +0530

    Update src/ansys/fluent/core/launcher/launcher.py
    Updated the suggestion.
    
    Co-authored-by: Mainak Kundu <94432368+mkundu1@users.noreply.github.com>

--- src/ansys/fluent/core/launcher/launcher.py
+++ src/ansys/fluent/core/launcher/launcher.py
@@ -541,7 +541,8 @@ def launch_fluent(
             if mode != LaunchModes.SOLVER_ICING:
                 env["APP_LAUNCHED_FROM_CLIENT"] = "1"  # disables flserver datamodel
             kwargs = _get_subprocess_kwargs_for_fluent(env)
-            kwargs.update(cwd=cwd)
+            if cwd:
+                kwargs.update(cwd=cwd)
             subprocess.Popen(launch_string, **kwargs)
 
             _await_fluent_launch(server_info_filepath, start_timeout, sifile_last_mtime)
