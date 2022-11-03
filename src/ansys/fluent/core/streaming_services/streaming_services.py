ae8e04a11360e8e6dbd392ce46de3b6acdf23f3f
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-11-03 20:02:00 +0530

    Updated docstring

--- src/ansys/fluent/core/streaming_services/streaming_services.py
+++ src/ansys/fluent/core/streaming_services/streaming_services.py
@@ -22,12 +22,31 @@ class StreamingService:
             return self._streaming
 
     def register_callback(self, call_back: Callable, *args, **kwargs) -> str:
+        """Register the callback.
+
+        Parameters
+        ----------
+        call_back : Callable
+            Callback to register.
+
+        Returns
+        -------
+        str
+            Registered callback ID.
+        """
         with self._lock:
             callback_id = f"{next(self._service_callback_id)}"
             self._service_callbacks[callback_id] = [call_back, args, kwargs]
             return callback_id
 
     def unregister_callback(self, callback_id: str):
+        """Unregister the callback.
+
+        Parameters
+        ----------
+        callback_id : str
+            ID of the registered callback.
+        """
         with self._lock:
             if callback_id in self._service_callbacks:
                 del self._service_callbacks[callback_id]
