d4a7556eb288368fd5892a2a0bd78f0adc738b55
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2022-11-14 17:00:09 +0530

    Propagate TUI error from Fluent (#1090)

--- src/ansys/fluent/core/launcher/launcher.py
+++ src/ansys/fluent/core/launcher/launcher.py
@@ -142,6 +142,7 @@ def _get_subprocess_kwargs_for_fluent(env: Dict[str, Any]) -> Dict[str, Any]:
         kwargs.update(shell=True, start_new_session=True)
     fluent_env = os.environ.copy()
     fluent_env.update({k: str(v) for k, v in env.items()})
+    fluent_env["REMOTING_THROW_LAST_TUI_ERROR"] = "1"
     kwargs.update(env=fluent_env)
     return kwargs
 
