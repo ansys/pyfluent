b5be1ee4e9e5eca670f41e054d111641e1a3360d
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2022-11-28 20:24:32 +0530

    Added watch_health module in health check() (#1108)
    * Added watch_health module in health check()
    
    * Corrected working logic - for pyfluent
    
    * Re-structured streaming services
    
    * Minor style corrections
    
    * Updated error handling logic.
    
    * Exception check with grpc status code
    
    * Corrected Test
    
    * Updated watch health to handle all errors internally.
    
    * Debugging
    
    * Add StopIteration Exception
    
    * Revert apigen change
    
    * Updated monitors streaming issue.
    
    * Revert change in fluent connection

--- src/ansys/fluent/core/services/transcript.py
+++ src/ansys/fluent/core/services/transcript.py
@@ -1,46 +1,20 @@
 """Wrapper over the transcript grpc service of Fluent."""
 
-from typing import Generator, List, Tuple
+from typing import List, Tuple
 
 import grpc
 
 from ansys.api.fluent.v0 import transcript_pb2 as TranscriptModule
 from ansys.api.fluent.v0 import transcript_pb2_grpc as TranscriptGrpcModule
+from ansys.fluent.core.services.streaming import StreamingService
 
 
-class TranscriptService:
-    """Class wrapping the transcript gRPC service of Fluent.
-
-    Methods
-    -------
-    begin_streaming
-        Begin transcript streaming from Fluent.
-    """
+class TranscriptService(StreamingService):
+    """Class wrapping the transcript gRPC service of Fluent."""
 
     def __init__(self, channel: grpc.Channel, metadata: List[Tuple[str, str]]):
-        self.__stub = TranscriptGrpcModule.TranscriptStub(channel)
-        self.__metadata = metadata
-        self.__streams = None
-
-    def begin_streaming(
-        self, started_evt
-    ) -> Generator[TranscriptModule.TranscriptResponse, None, None]:
-        """Begin transcript streaming from Fluent.
-
-        Yields
-        ------
-        str
-            A transcript line
-        """
-        request = TranscriptModule.TranscriptRequest()
-        self.__streams = self.__stub.BeginStreaming(request, metadata=self.__metadata)
-        started_evt.set()
-        while True:
-            try:
-                yield next(self.__streams)
-            except Exception:
-                break
-
-    def end_streaming(self) -> None:
-        if self.__streams and not self.__streams.cancelled():
-            self.__streams.cancel()
+        super().__init__(
+            stub=TranscriptGrpcModule.TranscriptStub(channel),
+            request=TranscriptModule.TranscriptRequest(),
+            metadata=metadata,
+        )
