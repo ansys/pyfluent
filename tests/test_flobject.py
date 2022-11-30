8a22fa8e2153e2daf7d983f6a0c14393ef9c92ee
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-11-30 19:45:58 +0530

    Added user_creatable() accessor method to NamedObject

--- tests/test_flobject.py
+++ tests/test_flobject.py
@@ -666,6 +666,12 @@ def test_accessor_methods_on_settings_object(load_static_mixer_case):
     modified = solver.file.read.file_type.is_read_only()
     assert existing == modified
 
+    existing = solver.setup.boundary_conditions.velocity_inlet.get_attr(
+        "user-creatable?"
+    )
+    modified = solver.setup.boundary_conditions.velocity_inlet.user_creatable()
+    assert existing == modified
+
     assert (
         solver.setup.boundary_conditions.velocity_inlet[
             "inlet1"
