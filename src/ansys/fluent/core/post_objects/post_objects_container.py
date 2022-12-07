c75852dff368d934dd6fb9f5211e36f2e510888b
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-12-07 17:23:34 +0530

    Re-structured

--- src/ansys/fluent/core/post_objects/post_objects_container.py
+++ src/ansys/fluent/core/post_objects/post_objects_container.py
@@ -4,7 +4,45 @@ import inspect
 from ansys.fluent.core.meta import PyLocalContainer
 
 
-class Plots:
+class Container:
+    def __init__(self, session, child, module, local_surfaces_provider=None):
+        """Instantiate Plots, container of plot objects.
+
+        Parameters
+        ----------
+        session :
+            Session object.
+        local_surfaces_provider : object, optional
+            Object providing local surfaces.
+        """
+        session_state = child._sessions_state.get(session.id if session else 1)
+        if not session_state:
+            session_state = self.__dict__
+            child._sessions_state[session.id if session else 1] = session_state
+            self.session = session
+            self._init_module(self, module)
+        else:
+            self.__dict__ = session_state
+        self._local_surfaces_provider = lambda: local_surfaces_provider or getattr(
+            self, "Surfaces", []
+        )
+
+    def _init_module(self, obj, mod):
+        from ansys.fluent.core.post_objects.post_helper import PostAPIHelper
+
+        for name, cls in mod.__dict__.items():
+
+            if cls.__class__.__name__ in (
+                "PyLocalNamedObjectMetaAbstract",
+            ) and not inspect.isabstract(cls):
+                setattr(
+                    obj,
+                    cls.PLURAL,
+                    PyLocalContainer(self, cls, PostAPIHelper),
+                )
+
+
+class Plots(Container):
     """Provides the Matplotlib ``Plots`` objects manager.
 
     This class provides access to ``Plots`` object containers for a given
@@ -29,43 +67,10 @@ class Plots:
     _sessions_state = {}
 
     def __init__(self, session, module, local_surfaces_provider=None):
-        """Instantiate Plots, container of plot objects.
-
-        Parameters
-        ----------
-        session :
-            Session object.
-        local_surfaces_provider : object, optional
-            Object providing local surfaces.
-        """
-        session_state = Plots._sessions_state.get(session.id if session else 1)
-        if not session_state:
-            session_state = self.__dict__
-            Plots._sessions_state[session.id if session else 1] = session_state
-            self.session = session
-            self._init_module(self, module)
-        else:
-            self.__dict__ = session_state
-        self._local_surfaces_provider = lambda: local_surfaces_provider or getattr(
-            self, "Surfaces", []
-        )
-
-    def _init_module(self, obj, mod):
-        from ansys.fluent.core.post_objects.post_helper import PostAPIHelper
-
-        for name, cls in mod.__dict__.items():
-
-            if cls.__class__.__name__ in (
-                "PyLocalNamedObjectMetaAbstract",
-            ) and not inspect.isabstract(cls):
-                setattr(
-                    obj,
-                    cls.PLURAL,
-                    PyLocalContainer(self, cls, PostAPIHelper),
-                )
+        super().__init__(session, self.__class__, module, local_surfaces_provider)
 
 
-class Graphics:
+class Graphics(Container):
     """Provides the PyVista ``Graphics`` objects manager.
 
     This class provides access to ``Graphics`` object containers for a given
@@ -94,41 +99,7 @@ class Graphics:
     _sessions_state = {}
 
     def __init__(self, session, module, local_surfaces_provider=None):
-        """Instantiate the ``Graphics`` object container.
-
-        Parameters
-        ----------
-        session : obj
-            Session object.
-        local_surfaces_provider : object, optional
-            Object providing local surfaces so that you can access surfaces
-            created in other modules, such as PyVista. The default is ``None``.
-        """
-        session_state = Graphics._sessions_state.get(session.id if session else 1)
-        if not session_state:
-            session_state = self.__dict__
-            Graphics._sessions_state[session.id if session else 1] = session_state
-            self.session = session
-            self._init_module(self, module)
-        else:
-            self.__dict__ = session_state
-        self._local_surfaces_provider = lambda: local_surfaces_provider or getattr(
-            self, "Surfaces", []
-        )
-
-    def _init_module(self, obj, mod):
-        from ansys.fluent.core.post_objects.post_helper import PostAPIHelper
-
-        for name, cls in mod.__dict__.items():
-
-            if cls.__class__.__name__ in (
-                "PyLocalNamedObjectMetaAbstract",
-            ) and not inspect.isabstract(cls):
-                setattr(
-                    obj,
-                    cls.PLURAL,
-                    PyLocalContainer(self, cls, PostAPIHelper),
-                )
+        super().__init__(session, self.__class__, module, local_surfaces_provider)
 
     def add_outline_mesh(self):
         """Add a mesh outline.
