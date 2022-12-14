c530029c67d80ac49ed1c9d82af486487a2f25c4
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-12-14 13:16:04 +0530

    Added test for topy

---
+++ tests/test_topy.py
@@ -0,0 +1,99 @@
+import os
+
+import ansys.fluent.core as pyfluent
+
+
+def test_single_jou():
+    file_path = os.path.join(pyfluent.EXAMPLES_PATH, "jou1.jou")
+
+    with open(file_path, "w") as journal:
+        journal.write('(display "from jou1.jou")')
+
+    solver = pyfluent.launch_fluent(mode="solver", topy=file_path)
+    solver.exit()
+
+    with open(file_path) as file:
+        returned = file.readlines()
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    assert returned
+
+
+def test_single_scm():
+    file_path = os.path.join(pyfluent.EXAMPLES_PATH, "jou1.scm")
+
+    with open(file_path, "w") as journal:
+        journal.write('(display "from jou1.scm")')
+
+    solver = pyfluent.launch_fluent(mode="solver", topy=file_path)
+    solver.exit()
+
+    with open(file_path) as file:
+        returned = file.readlines()
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    assert returned
+
+
+def test_2_jou():
+    file_path_1 = os.path.join(pyfluent.EXAMPLES_PATH, "jou1.jou")
+    file_path_2 = os.path.join(pyfluent.EXAMPLES_PATH, "jou2.jou")
+
+    with open(file_path_1, "w") as journal:
+        journal.write('(display "from jou1.jou")')
+
+    with open(file_path_2, "w") as journal:
+        journal.write('(display "from jou2.jou")')
+
+    solver = pyfluent.launch_fluent(mode="solver", topy=[file_path_1, file_path_2])
+    solver.exit()
+
+    with open(file_path_1) as file:
+        returned1 = file.readlines()
+
+    if os.path.exists(file_path_1):
+        os.remove(file_path_1)
+
+    assert returned1
+
+    with open(file_path_2) as file:
+        returned2 = file.readlines()
+
+    if os.path.exists(file_path_2):
+        os.remove(file_path_2)
+
+    assert returned2
+
+
+def test_2_scm():
+    file_path_1 = os.path.join(pyfluent.EXAMPLES_PATH, "jou1.scm")
+    file_path_2 = os.path.join(pyfluent.EXAMPLES_PATH, "jou2.scm")
+
+    with open(file_path_1, "w") as journal:
+        journal.write('(display "from jou1.scm")')
+
+    with open(file_path_2, "w") as journal:
+        journal.write('(display "from jou2.scm")')
+
+    solver = pyfluent.launch_fluent(mode="solver", topy=[file_path_1, file_path_2])
+    solver.exit()
+
+    with open(file_path_1) as file:
+        returned1 = file.readlines()
+
+    if os.path.exists(file_path_1):
+        os.remove(file_path_1)
+
+    assert returned1
+
+    with open(file_path_2) as file:
+        returned2 = file.readlines()
+
+    if os.path.exists(file_path_2):
+        os.remove(file_path_2)
+
+    assert returned2
