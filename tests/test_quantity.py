ed559c0ea49d18950c1f3910b79095e002e22f65
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-12-22 19:16:55 +0530

    Added test for report definitions

--- tests/test_quantity.py
+++ tests/test_quantity.py
@@ -880,6 +880,124 @@ def test_units_quantity_attribute_73():
     assert test.unit == "K"
 
 
+def test_units_quantity_attribute_74():
+    import ansys.fluent.core as pyfluent
+    from ansys.fluent.core import examples
+
+    import_filename = examples.download_file("mixing_elbow.msh.h5", "pyfluent/mixing_elbow")
+
+    solver = pyfluent.launch_fluent(precision="double", processor_count=2, mode="solver")
+
+    solver.file.read(file_type="case", file_name=import_filename)
+    solver.tui.mesh.check()
+
+    solver.tui.define.units("length", "in")
+
+    solver.setup.models.energy.enabled = True
+
+    if solver.get_fluent_version() == "22.2.0":
+        solver.setup.materials.copy_database_material_by_name(
+            type="fluid", name="water-liquid"
+        )
+    else:
+        solver.setup.materials.database.copy_by_name(type="fluid", name="water-liquid")
+
+    solver.setup.cell_zone_conditions.fluid["elbow-fluid"].material = "water-liquid"
+
+    if solver.get_fluent_version() == "22.2.0":
+        solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = {
+            "option": "constant or expression",
+            "constant": 0.4,
+        }
+    else:
+        solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = 0.4
+    solver.setup.boundary_conditions.velocity_inlet[
+        "cold-inlet"
+    ].ke_spec = "Intensity and Hydraulic Diameter"
+    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].turb_intensity = 0.05
+    solver.setup.boundary_conditions.velocity_inlet[
+        "cold-inlet"
+    ].turb_hydraulic_diam = "4 [in]"
+    if solver.get_fluent_version() == "22.2.0":
+        solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = {
+            "option": "constant or expression",
+            "constant": 293.15,
+        }
+    else:
+        solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = 293.15
+
+    if solver.get_fluent_version() == "22.2.0":
+        solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = {
+            "option": "constant or expression",
+            "constant": 1.2,
+        }
+    else:
+        solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = 1.2
+    solver.setup.boundary_conditions.velocity_inlet[
+        "hot-inlet"
+    ].ke_spec = "Intensity and Hydraulic Diameter"
+    solver.setup.boundary_conditions.velocity_inlet[
+        "hot-inlet"
+    ].turb_hydraulic_diam = "1 [in]"
+    if solver.get_fluent_version() == "22.2.0":
+        solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = {
+            "option": "constant or expression",
+            "constant": 313.15,
+        }
+    else:
+        solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = 313.15
+
+    solver.setup.boundary_conditions.pressure_outlet["outlet"].turb_viscosity_ratio = 4
+
+    solver.tui.solve.monitors.residual.plot("no")
+
+    solver.solution.initialization.hybrid_initialize()
+
+    solver.solution.run_calculation.iterate.get_attr("arguments")
+    if solver.get_fluent_version() >= "23.1.0":
+        solver.solution.run_calculation.iterate(iter_count=50)
+    else:
+        solver.solution.run_calculation.iterate(number_of_iterations=50)
+
+    solver.results.graphics.vector["velocity_vector_symmetry"] = {}
+    solver.results.graphics.vector["velocity_vector_symmetry"].print_state()
+    solver.results.graphics.vector["velocity_vector_symmetry"].field = "temperature"
+    solver.results.graphics.vector["velocity_vector_symmetry"].surfaces_list = [
+        "symmetry-xyplane",
+    ]
+    solver.results.graphics.vector["velocity_vector_symmetry"].scale.scale_f = 4
+    solver.results.graphics.vector["velocity_vector_symmetry"].style = "arrow"
+
+    solver.solution.report_definitions.flux["mass_flow"] = {}
+    solver.solution.report_definitions.flux["mass_flow"].zone_names.get_attr(
+        "allowed-values"
+    )
+    solver.solution.report_definitions.flux["mass_flow"].zone_names = [
+        "cold-inlet",
+        "hot-inlet",
+        "outlet",
+    ]
+    solver.solution.report_definitions.flux["mass_flow"].print_state()
+    solver.solution.report_definitions.compute(report_defs=["mass_flow"])
+
+    value_quantity_map = solver.solution.report_definitions.compute._get_value_quantity_map(
+        report_defs=["mass_flow"])
+
+    real_value = solver.solution.report_definitions.compute.value(report_defs=["mass_flow"])
+
+    quantity_map = solver.solution.report_definitions.compute.quantity_map(report_defs=["mass_flow"])
+
+    assert value_quantity_map == [real_value, quantity_map]
+    assert real_value == value_quantity_map[0]
+    assert quantity_map == value_quantity_map[1]
+
+    test = q.Quantity(real_value, quantity_map=quantity_map)
+    assert test.value == real_value
+    assert test.unit == "kg s^-1"
+
+    solver.exit()
+
+
 def testing_dimensions():
     print(f"{'*' * 25} {testing_dimensions.__name__} {'*' * 25}")
 
