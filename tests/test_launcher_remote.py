eab0699b5a240cceb0c72d16c305501d4d140e02
Mainak Kundu <mainak.kundu@ansys.com>
2023-01-17 19:56:30 -0500

    Fix CI

--- tests/test_launcher_remote.py
+++ tests/test_launcher_remote.py
@@ -6,7 +6,7 @@ import grpc
 from util.solver_workflow import new_solver_session  # noqa: F401
 
 from ansys.fluent.core.launcher import launcher
-from ansys.fluent.core.utils.fluent_version import get_version_for_filepath
+import ansys.fluent.core.utils.fluent_version as docker_image_version
 import ansys.platform.instancemanagement as pypim
 
 
@@ -39,6 +39,9 @@ def test_launch_remote_instance(monkeypatch, new_solver_session):
     monkeypatch.setattr(pypim, "connect", mock_connect)
     monkeypatch.setattr(pypim, "is_configured", mock_is_configured)
 
+    if os.getenv("PYFLUENT_LAUNCH_CONTAINER") == "1":
+        monkeypatch.setattr(launcher, "get_ansys_version", lambda: docker_image_version.get_version())
+
     # Start fluent with launch_fluent
     # Note: This is mocking to start Fluent, but actually reusing the common one
     # Thus cleanup_on_exit is set to false
@@ -48,11 +51,11 @@ def test_launch_remote_instance(monkeypatch, new_solver_session):
     assert mock_is_configured.called
     assert mock_connect.called
     if os.getenv("PYFLUENT_LAUNCH_CONTAINER") == "1":
-        product_version = get_version_for_filepath()
+        product_version = docker_image_version.get_version_for_filepath()
     else:
-        product_version = launcher.get_ansys_version().split(".")
+        product_version = "".join(launcher.get_ansys_version().split("."))[:-1]
     mock_client.create_instance.assert_called_with(
-        "fluent-3ddp", product_version="".join(product_version)[:-1]
+        "fluent-3ddp", product_version=product_version
     )
     assert mock_instance.wait_for_ready.called
     mock_instance.build_grpc_channel.assert_called_with()
