02eb6607fc0a50f5b7e7d56098b2148c937ce30d
Harshal Pohekar <harshal.pohekar@ansys.com>
2023-01-30 17:51:36 +0530

    Updated settings_rstgen.py

--- doc/settings_rstgen.py
+++ doc/settings_rstgen.py
@@ -125,9 +125,7 @@ def _populate_rst_from_settings(rst_dir, cls, version):
         r.write(f".. _{file_name}:\n\n")
         r.write(f"{cls_name}\n")
         r.write(f'{"="*(len(cls_name))}\n\n')
-        r.write(
-            f".. currentmodule:: ansys.fluent.core.solver.settings_{version}.{file_name}\n\n"
-        )
+        r.write(f".. currentmodule:: ansys.fluent.core.solver.settings_{version}.{file_name}\n\n")
         r.write(".. autosummary::\n")
         r.write("   :toctree: _autosummary\n")
         r.write("   :recursive:\n\n")
