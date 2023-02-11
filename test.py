117fe103b4811804ac66e8a661de6473ec632ebe
Mainak Kundu <mainak.kundu@ansys.com>
2023-02-10 19:19:28 -0500

    Lightweight mode

--- test.py
+++
@@ -1,6 +0,0 @@
-import ansys.fluent.core as pyfluent
-
-pyfluent.USE_LIGHT_IO = True
-solver = pyfluent.launch_fluent(show_gui=True)
-solver.read_case(file_name="elbow.cas.h5")
-solver.setup.models.energy.enabled = False
