a566cf84d3b5212a1ad56c86071073d251c39671
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-02-23 07:40:45 -0500

    Infer remoting ip address (#1386)
    * Determine remoting ip address
    
    * Infer remoting ip address
    
    * Infer remoting ip address
    
    * Infer remoting ip address

--- src/ansys/fluent/core/launcher/launcher.py
+++ src/ansys/fluent/core/launcher/launcher.py
@@ -23,6 +23,7 @@ from ansys.fluent.core.session_pure_meshing import PureMeshing
 from ansys.fluent.core.session_solver import Solver
 from ansys.fluent.core.session_solver_icing import SolverIcing
 from ansys.fluent.core.utils.logging import LOG
+from ansys.fluent.core.utils.networking import find_remoting_ip
 import ansys.platform.instancemanagement as pypim
 
 _THIS_DIR = os.path.dirname(__file__)
@@ -134,6 +135,12 @@ def _get_subprocess_kwargs_for_fluent(env: Dict[str, Any]) -> Dict[str, Any]:
     fluent_env = os.environ.copy()
     fluent_env.update({k: str(v) for k, v in env.items()})
     fluent_env["REMOTING_THROW_LAST_TUI_ERROR"] = "1"
+    from ansys.fluent.core import INFER_REMOTING_IP
+
+    if INFER_REMOTING_IP and not "REMOTING_SERVER_ADDRESS" in fluent_env:
+        remoting_ip = find_remoting_ip()
+        if remoting_ip:
+            fluent_env["REMOTING_SERVER_ADDRESS"] = remoting_ip
     kwargs.update(env=fluent_env)
     return kwargs
 
