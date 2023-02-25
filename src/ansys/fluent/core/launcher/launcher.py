bcda394814c9eaa84ded0d9a006749b85ec023bd
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-02-25 09:38:07 -0500

    Add typing stub files (#1397)
    * Add typing stub files
    
    * Disable failing test
    
    * Elide default args

--- src/ansys/fluent/core/launcher/launcher.py
+++ src/ansys/fluent/core/launcher/launcher.py
@@ -17,7 +17,7 @@ import warnings
 from ansys.fluent.core.fluent_connection import _FluentConnection
 from ansys.fluent.core.launcher.fluent_container import start_fluent_container
 from ansys.fluent.core.scheduler import build_parallel_options, load_machines
-from ansys.fluent.core.session import _BaseSession, parse_server_info_file
+from ansys.fluent.core.session import parse_server_info_file
 from ansys.fluent.core.session_meshing import Meshing
 from ansys.fluent.core.session_pure_meshing import PureMeshing
 from ansys.fluent.core.session_solver import Solver
@@ -441,7 +441,7 @@ def launch_fluent(
     cwd: str = None,
     topy: Union[str, list] = None,
     **kwargs,
-) -> _BaseSession:
+) -> Union[Solver, Meshing, PureMeshing]:
     """Launch Fluent locally in server mode or connect to a running Fluent
     server instance.
 
