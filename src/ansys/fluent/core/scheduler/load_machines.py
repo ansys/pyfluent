6163e679e64850f7c3f763d6660f6e44ccc33d9f
Adam-Boutin <124106559+Adam-Boutin@users.noreply.github.com>
2023-03-18 11:30:04 -0600

    os.path.exists(host_info) implemented

--- src/ansys/fluent/core/scheduler/load_machines.py
+++ src/ansys/fluent/core/scheduler/load_machines.py
@@ -118,11 +118,8 @@ def _parse_host_info(host_info):
         A list of dictionaries formatted as:
         {'machine-name' : ###, 'core-count' : ###}
     """
-    print(host_info)
-    if "\\" in host_info or "/" in host_info or "." in host_info:
-        # Filenames generally have '\\' or '/' or '.'
-        # so assume it's a file and parse accordingly
-        # Read from the file
+    if os.path.exists(host_info):
+        # Only opens a file if it exists
         with open(host_info, "r") as f:
             host_info = f.read()
 
