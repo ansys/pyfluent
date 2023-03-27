73ae24abc672be4d05844a5c8ca1222fbd69bde6
Aseem Jain <aseem.jain@ansys.com>
2023-03-27 15:16:58 +0530

    Field data streaming

--- src/ansys/fluent/core/streaming_services/fielddata_streaming.py
+++ src/ansys/fluent/core/streaming_services/fielddata_streaming.py
@@ -1,27 +1,22 @@
 """Module for monitors management."""
 
 import threading
-from typing import Dict, List, Tuple, Union
-
-import numpy as np
-import pandas as pd
-
 from ansys.fluent.core.streaming_services.streaming import StreamingService
 from ansys.api.fluent.v0 import field_data_pb2 as FieldDataProtoModule
 from ansys.fluent.core.services.field_data import ChunkParser
                                
 class FieldDataStreaming(StreamingService):
-    """Manages monitors (Fluent residuals and report definitions monitors).
+     """Class wrapping the Field gRPC streaming service of Fluent.
 
     Parameters
     ----------
     session_id : str
         Session ID.
-    service : MonitorsService
-        Monitors streaming service.
+    service : FieldDataService
+        FieldData streaming service.
     """
 
-    def __init__(self, session_id: str, service):
+    def __init__(self, session_id: str, service: FieldDataService):
         super().__init__(
             target=FieldDataStreaming._process_streaming,
             streaming_service=service,
@@ -31,8 +26,7 @@ class FieldDataStreaming(StreamingService):
 
 
     def _process_streaming(self, started_evt, *args, **kwargs):
-        """Performs processes on transcript depending on the callback
-        functions."""
+        """Processes on field data streaming."""
         request=FieldDataProtoModule.BeginFieldsStreamingRequest(*args, **kwargs)
         ChunkParser(self).extract_fields(self._streaming_service.begin_streaming(request, started_evt))
         
