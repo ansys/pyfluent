e6f0742eca93a1e508cc07d1ad493deee0543b3b
ANSYS\spearson <sean.pearson@ansys.com>
2023-03-31 20:38:21 +0100

    add missing file

---
+++ src/ansys/fluent/core/meshing/faulttolerant.py
@@ -0,0 +1,12 @@
+from ansys.fluent.core.launcher.launcher import LaunchMode, launch_fluent
+
+from .meshing_workflow import MeshingWorkflow
+
+
+def fault_tolerant_workflow(**launch_args) -> MeshingWorkflow:
+    args = dict(mode=LaunchMode.PURE_MESHING_MODE)
+    args.update(launch_args)
+    session = launch_fluent(**args)
+    meshing_workflow = session.workflow
+    meshing_workflow.fault_tolerant()
+    return meshing_workflow
