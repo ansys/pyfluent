2d9c9a24763a25d348ff106dcaa6f04cc90c2702
Aseem Jain <aseem.jain@ansys.com>
2023-03-31 14:53:26 +0530

    Fields streaming

--- src/ansys/fluent/core/session_pure_meshing.py
+++ src/ansys/fluent/core/session_pure_meshing.py
@@ -6,10 +6,8 @@
 
 import functools
 
-from ansys.api.fluent.v0 import datamodel_se_pb2
 from ansys.fluent.core.data_model_cache import DataModelCache
 from ansys.fluent.core.fluent_connection import FluentConnection
-from ansys.fluent.core.services.streaming import StreamingService
 from ansys.fluent.core.session import BaseSession
 from ansys.fluent.core.session_base_meshing import BaseMeshing
 from ansys.fluent.core.streaming_services.datamodel_streaming import DatamodelStream
@@ -35,22 +33,12 @@ class PureMeshing(BaseSession):
         self.datamodel_streams = {}
         if self.use_cache:
             for rules in self.__class__.rules:
-                request = datamodel_se_pb2.DataModelRequest()
-                request.rules = rules
-                request.diffstate = (
-                    datamodel_se_pb2.DIFFSTATE_NOCOMMANDS
-                )  # DIFFSTATE_FULL?
-                streaming = StreamingService(
-                    stub=datamodel_service_se._stub,
-                    request=request,
-                    metadata=datamodel_service_se._metadata,
-                )
-                stream = DatamodelStream(streaming)
+                stream = DatamodelStream(datamodel_service_se)
                 stream.register_callback(
                     functools.partial(DataModelCache.update_cache, rules=rules)
                 )
                 self.datamodel_streams[rules] = stream
-                stream.start()
+                stream.start(rules=rules, no_commands_diff_state=True)
 
     @property
     def tui(self):
