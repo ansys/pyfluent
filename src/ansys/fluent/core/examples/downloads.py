b649210612dbba51423d3105ba506d5dcf4765f7
Raphael Luciano <raphael.luciano@ansys.com>
2023-05-08 10:22:05 -0400

    Log and return abspath (only log before)

--- src/ansys/fluent/core/examples/downloads.py
+++ src/ansys/fluent/core/examples/downloads.py
@@ -47,12 +47,13 @@ def _retrieve_file(url: str, filename: str, save_path: Optional[str] = None) -> 
             pyfluent.EXAMPLES_PATH, os.path.basename(filename)
         )
     else:
+        save_path = os.path.abspath(save_path)
         local_path = os.path.join(save_path, os.path.basename(filename))
     local_path_no_zip = re.sub(".zip$", "", local_path)
     # First check if file has already been downloaded
     if os.path.isfile(local_path_no_zip) or os.path.isdir(local_path_no_zip):
         logging.info("File already exists.")
-        logging.info(f"File path: {os.path.abspath(local_path_no_zip)}")
+        logging.info(f"File path: {local_path_no_zip}")
         return local_path_no_zip
 
     logging.info("Downloading specified file...")
@@ -67,7 +68,7 @@ def _retrieve_file(url: str, filename: str, save_path: Optional[str] = None) -> 
         _decompress(local_path)
         local_path = local_path_no_zip
     logging.info("Download successful.")
-    logging.info(f"File path: {os.path.abspath(local_path)}")
+    logging.info(f"File path: {local_path}")
     return local_path
 
 
