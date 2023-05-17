ba3900bba562491021afc5ace0944ab88c74c443
Mainak Kundu <mainak.kundu@ansys.com>
2023-05-17 17:15:34 -0400

    Fix doc build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ extensions = [
 
 # Intersphinx mapping
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/dev", None),
+    "python": ("https://docs.python.org/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy", None),
     "numpy": ("https://numpy.org/devdocs", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
