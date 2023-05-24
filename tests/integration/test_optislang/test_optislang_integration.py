0927fce5977b31f362ba6cbcb31c51a8b3f03473
Mainak Kundu <mainak.kundu@ansys.com>
2023-05-24 14:29:41 -0400

    Fix nightly CI tests

--- tests/integration/test_optislang/test_optislang_integration.py
+++ tests/integration/test_optislang/test_optislang_integration.py
@@ -26,8 +26,9 @@ def test_simple_solve(load_mixing_elbow_param_case_dat):
     - Output parameters
     """
     # Step 1: Setup logging
-    pyfluent.set_log_level("ERROR")
-    pyfluent.enable_logging_to_stdout()
+    import logging
+
+    logging.getLogger().setLevel("ERROR")
 
     # Step 2: Launch fluent session and read case file with and without data file
     solver_session = load_mixing_elbow_param_case_dat
@@ -113,8 +114,9 @@ def test_generate_read_mesh(with_launching_container, mixing_elbow_geometry):
     - Session health
     """
     # Step 1: Setup logging
-    pyfluent.set_log_level("ERROR")
-    pyfluent.enable_logging_to_stdout()
+    import logging
+
+    logging.getLogger().setLevel("ERROR")
 
     # Step 2: Launch fluent session in meshing mode
     meshing = pyfluent.launch_fluent(
