4b4ac6c924705ce6952a05f61ab150ba61a62a14
Harshal Pohekar <harshal.pohekar@ansys.com>
2023-06-05 15:50:13 +0530

    Added mixin in settingsgen

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -1121,12 +1121,7 @@ def get_cls(name, info, parent=None, version=None):
             bases = bases + (_CreatableNamedObjectMixin,)
         elif obj_type == "named-object":
             bases = bases + (_NonCreatableNamedObjectMixin,)
-        elif info.get("has-allowed-values", False) and version in [
-            "222",
-            "231",
-            "232",
-            "241",
-        ]:
+        elif info.get("has-allowed-values") or version == "222":
             bases += (_HasAllowedValuesMixin,)
 
         cls = type(pname, bases, dct)
