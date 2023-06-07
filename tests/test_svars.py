b0c9dc5a928cb7d8b8312468fdd39f2e8aa2bc72
Your Name <you@example.com>
2023-06-07 16:17:03 +0530

    Updated test code.

--- tests/test_svars.py
+++ tests/test_svars.py
@@ -2,89 +2,82 @@ import os
 
 os.environ["PYFLUENT_FLUENT_ROOT"] = r"C:\ANSYSDev\ANSYSDev\vNNN\fluent"
 
-import ansys.fluent.core as pyfluent
+import numpy as np
+from util.solver_workflow import new_solver_session  # noqa: F401
+
 from ansys.fluent.core import examples
 
-import_filename = examples.download_file("mixing_elbow.msh.h5", "pyfluent/mixing_elbow")
-solver = pyfluent.launch_fluent(mode="solver")
-solver.file.read(file_type="case", file_name=import_filename)
 
-svar_info = solver.svar_info
-svar_data = solver.svar_data
+def test_svars(new_solver_session):
+    solver = new_solver_session
+    import_filename = examples.download_file(
+        "mixing_elbow.msh.h5", "pyfluent/mixing_elbow"
+    )
+    solver.file.read(file_type="case", file_name=import_filename)
 
-zones_info = svar_info.get_zones_info()
+    svar_info = solver.svar_info
+    svar_data = solver.svar_data
 
-assert zones_info.domains == ["mixture"]
+    zones_info = svar_info.get_zones_info()
 
-assert zones_info.zones == [
-    "symmetry-xyplane",
-    "hot-inlet",
-    "cold-inlet",
-    "outlet",
-    "wall-inlet",
-    "wall-elbow",
-    "elbow-fluid",
-    "interior--elbow-fluid",
-]
+    assert zones_info.domains == ["mixture"]
 
-zone_info = zones_info["wall-inlet"]
+    assert zones_info.zones == [
+        "symmetry-xyplane",
+        "hot-inlet",
+        "cold-inlet",
+        "outlet",
+        "wall-inlet",
+        "wall-elbow",
+        "elbow-fluid",
+        "interior--elbow-fluid",
+    ]
 
-assert zone_info.name == "wall-inlet"
+    zone_info = zones_info["wall-inlet"]
 
-assert zone_info.count == 268
+    assert zone_info.name == "wall-inlet"
 
-assert zone_info.zone_id == 33
+    assert zone_info.count == 268
 
-assert zone_info.zone_type == "wall"
+    assert zone_info.zone_id == 33
 
-svars_info_wall_fluid = svar_info.get_svars_info(
-    zone_names=["wall", "fluid"], domain_name="mixture"
-)
+    assert zone_info.zone_type == "wall"
 
-assert svars_info_wall_fluid.svars == [
-    "SV_CENTROID",
-    "SV_D",
-    "SV_H",
-    "SV_K",
-    "SV_P",
-    "SV_T",
-    "SV_U",
-    "SV_V",
-    "SV_W",
-]
+    svars_info_wall_fluid = svar_info.get_svars_info(
+        zone_names=["wall-elbow", "elbow-fluid"], domain_name="mixture"
+    )
 
-svar_info_centroid = svars_info_wall_fluid["SV_CENTROID"]
+    assert svars_info_wall_fluid.svars == ["SV_CENTROID"]
 
-# svar_info_centroid
-# name: SV_CENTROID
-# dimension: 3
-# field_type: <
+    svar_info_centroid = svars_info_wall_fluid["SV_CENTROID"]
 
-assert svar_info_centroid.name == "SV_CENTROID"
+    assert svar_info_centroid.name == "SV_CENTROID"
 
-assert svar_info_centroid.dimension == 3
+    assert svar_info_centroid.dimension == 3
 
-# assert svar_info_centroid.field_type == 'numpy.float64'
+    assert svar_info_centroid.field_type == np.float64
 
-sv_t_wall_fluid = svar_data.get_svar_data(
-    svar_name="SV_T", zone_names=["fluid", "wall"], domain_name="mixture"
-)
-assert sv_t_wall_fluid.domain == "mixture"
+    sv_t_wall_fluid = svar_data.get_svar_data(
+        svar_name="SV_CENTROID",
+        zone_names=["elbow-fluid", "wall-elbow"],
+        domain_name="mixture",
+    )
+    assert sv_t_wall_fluid.domain == "mixture"
 
-assert sv_t_wall_fluid.zones == ["fluid", "wall"]
+    assert sv_t_wall_fluid.zones == ["wall-elbow", "elbow-fluid"]
 
-fluid_temp = sv_t_wall_fluid["fluid"]
-assert fluid_temp.size == 13852
-assert fluid_temp.dtype == "float64"
-# assert fluid_temp == array([600., 600., 600., ..., 600., 600., 600.])
+    fluid_temp = sv_t_wall_fluid["fluid"]
+    assert fluid_temp.size == 13852
+    assert fluid_temp.dtype == "float64"
+    # assert fluid_temp == array([600., 600., 600., ..., 600., 600., 600.])
 
-wall_temp_array = svar_data.get_array("SV_T", "wall", "mixture")
-fluid_temp_array = svar_data.get_array("SV_T", "fluid", "mixture")
-wall_temp_array[:] = 500
-fluid_temp_array[:] = 600
-zone_names_to_svar_data = {"wall": wall_temp_array, "fluid": fluid_temp_array}
-svar_data.set_svar_data(
-    svar_name="SV_T",
-    zone_names_to_svar_data=zone_names_to_svar_data,
-    domain_name="mixture",
-)
+    wall_temp_array = svar_data.get_array("SV_T", "wall", "mixture")
+    fluid_temp_array = svar_data.get_array("SV_T", "fluid", "mixture")
+    wall_temp_array[:] = 500
+    fluid_temp_array[:] = 600
+    zone_names_to_svar_data = {"wall": wall_temp_array, "fluid": fluid_temp_array}
+    svar_data.set_svar_data(
+        svar_name="SV_T",
+        zone_names_to_svar_data=zone_names_to_svar_data,
+        domain_name="mixture",
+    )
