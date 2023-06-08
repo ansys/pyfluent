7242d4cb308681b99f1fdde7e2bb73d6402ec159
Your Name <you@example.com>
2023-06-08 11:35:39 +0530

    Added single precision fluent session.

--- tests/util/solver_workflow.py
+++ tests/util/solver_workflow.py
@@ -15,6 +15,13 @@ def new_solver_session(with_launching_container):
 
 
 @pytest.fixture
+def new_solver_session_single_precision(with_launching_container):
+    solver = create_solver_session(precision="single")
+    yield solver
+    solver.exit()
+
+
+@pytest.fixture
 def new_solver_session_no_transcript(with_launching_container):
     solver = create_solver_session(start_transcript=False, mode="solver")
     yield solver
