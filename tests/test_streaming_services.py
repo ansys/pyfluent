f7e19559c1509c7aefaabe427445925ceb49531c
Mainak Kundu <mainak.kundu@ansys.com>
2023-06-20 10:31:23 -0400

    Try fixing test_transcript

--- tests/test_streaming_services.py
+++ tests/test_streaming_services.py
@@ -1,9 +1,9 @@
 import time
 
+import pytest
 from util.solver_workflow import new_solver_session  # noqa: F401
 
-from ansys.fluent.core.fluent_connection import FluentConnection
-from ansys.fluent.core.session_solver import Solver
+import ansys.fluent.core as pyfluent
 
 
 def transcript(data):
@@ -11,49 +11,46 @@ def transcript(data):
 
 
 def run_transcript(i, ip, port, password):
-    solver_session = Solver(
-        FluentConnection(ip=ip, port=port, password=password, cleanup_on_exit=False)
+    transcript("")
+    solver_session = pyfluent.launch_fluent(
+        start_instance=False, ip=ip, port=port, password=password, cleanup_on_exit=False
     )
     solver_session.transcript.register_callback(transcript)
 
-    transcript_checked = transcript_passed = 0
+    transcript_checked = False
+    transcript_passed = False
 
     if i % 5 == 0:
         solver_session.scheme_eval.scheme_eval("(pp 'test)")
-        check_transcript = True
+        transcript_checked = True
         time.sleep(5)
-        transcript_checked = 1
-    else:
-        check_transcript = False
 
-    if solver_session:
-        solver_session.exit()
-        if check_transcript:
-            if not transcript.data:
-                assert transcript.data == ""
-            else:
-                assert transcript.data == "test"
-                transcript_passed = 1
-        transcript("")
+    if transcript_checked:
+        if not transcript.data:
+            assert transcript.data == ""
+        else:
+            assert transcript.data == "test"
+            transcript_passed = True
 
     return transcript_checked, transcript_passed
 
 
-def test_transcript(new_solver_session):
+def test_transcript(new_solver_session, monkeypatch: pytest.MonkeyPatch):
     solver = new_solver_session
-    ip = solver.fluent_connection._channel_str.split(":")[0]
-    port = int(solver.fluent_connection._channel_str.split(":")[1])
-    password = solver.fluent_connection._metadata[0][-1]
+    monkeypatch.setenv("PYFLUENT_LAUNCH_CONTAINER", "0")
+    ip = solver.connection_properties.ip
+    port = solver.connection_properties.port
+    password = solver.connection_properties.password
 
-    total_checked_transcript = 0
-    passed_transcript = 0
+    total_checked_transcripts = 0
+    total_passed_transcripts = 0
 
     for i in range(100):
         transcript_checked, transcript_passed = run_transcript(i, ip, port, password)
-        total_checked_transcript += transcript_checked
-        passed_transcript += transcript_passed
+        total_checked_transcripts += int(transcript_checked)
+        total_passed_transcripts += int(transcript_passed)
 
     if solver.get_fluent_version() >= "23.2.0":
-        assert total_checked_transcript == passed_transcript
+        assert total_checked_transcripts == total_passed_transcripts
     else:
-        assert total_checked_transcript >= passed_transcript
+        assert total_checked_transcripts >= total_passed_transcripts
