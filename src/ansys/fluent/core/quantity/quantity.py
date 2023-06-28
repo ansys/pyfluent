4d54c7ecb818020ae0efef0704a98676ff30e165
Alexander Carvalho <132941110+acarvalh-work@users.noreply.github.com>
2023-06-28 12:48:54 -0400

    Maint/quantity unit systems (#1735)
    * feat: add unit system class
    
    * feat: custom unit system errors
    
    * feat: unit system error handling
    
    * feat: error handling for all files
    
    * test: unit system tests
    
    * fix: quantity map units syntax
    
    * fix: error messages and global dimension length

--- src/ansys/fluent/core/quantity/quantity.py
+++ src/ansys/fluent/core/quantity/quantity.py
@@ -21,8 +21,6 @@ class Quantity(float):
     -------
     to()
         Converts to given unit string.
-    convert()
-        Converts to given unit system.
 
     Returns
     -------
@@ -35,9 +33,7 @@ class Quantity(float):
             or (units and dimensions)
             or (quantity_map and dimensions)
         ):
-            raise ValueError(
-                "Quantity only accepts 1 of the following: units, quantity_map, dimensions"
-            )
+            raise QuantityError.EXCESSIVE_PARAMETERS()
 
         _units_table = q.UnitsTable()
         _value = float(value)
@@ -64,9 +60,7 @@ class Quantity(float):
             or (units and dimensions)
             or (quantity_map and dimensions)
         ):
-            raise ValueError(
-                "Quantity only accepts 1 of the following: units, quantity_map, dimensions"
-            )
+            raise QuantityError.EXCESSIVE_PARAMETERS()
 
         self._units_table = q.UnitsTable()
         self._value = float(value)
@@ -105,9 +99,9 @@ class Quantity(float):
         str
             SI unit string of new quantity.
         """
-        # Cannot perform operations between quantities with opposing dimensions
+        # Cannot perform operations between quantities with incompatible dimensions
         if isinstance(__value, Quantity) and (self.dimensions != __value.dimensions):
-            raise QuantityError(from_unit=self.units, to_unit=__value.units)
+            raise QuantityError.INCOMPATIBLE_DIMENSIONS(self.units, __value.units)
         # Cannot perform operations on a non-dimensionless quantity
         if (
             caller not in ["__mul__", "__truediv__"]
@@ -115,9 +109,7 @@ class Quantity(float):
             and (not isinstance(__value, Quantity))
             and isinstance(__value, (float, int))
         ):
-            raise TypeError(
-                f"Error: '{__value}' is incompatible with the current quantity object."
-            )
+            raise QuantityError.INCOMPATIBLE_VALUE(__value)
 
         return (
             "delta_K"
@@ -179,7 +171,7 @@ class Quantity(float):
         """
 
         if not isinstance(to_units, str):
-            raise TypeError("'to_units' should be of 'str' type.")
+            raise TypeError("`to_units` should be a `str` type.")
 
         # Retrieve all SI required SI data and perform conversion
         _, si_multiplier, si_offset = self._units_table.si_data(to_units)
@@ -191,31 +183,6 @@ class Quantity(float):
         self._arithmetic_precheck(new_obj)
 
         return new_obj
-
-    def convert(self, to_sys: str) -> "Quantity":
-        """Perform unit system conversions.
-
-        Parameters
-        ----------
-        to_sys : str
-            Desired unit system to convert to.
-
-        Returns
-        -------
-        Quantity
-            Quantity object containing desired unit system conversion.
-        """
-
-        # Verify specified system is supported
-        if to_sys not in self._units_table.unit_systems:
-            raise ValueError(
-                f"'{to_sys}' is not a supported unit system. Only {', '.join(self._units_table.unit_systems.keys())} are supported."
-            )
-
-        # Create new dimensions with desired unit system
-        new_dim = q.Dimensions(dimensions=self.dimensions, unit_sys=to_sys)
-
-        return Quantity(value=self.value, units=new_dim.units)
 
     def __str__(self):
         return f'({self.value}, "{self.units}")'
@@ -230,7 +197,7 @@ class Quantity(float):
         return Quantity(value=new_si_value, units=new_dimensions.units)
 
     def __mul__(self, __value):
-        new_units = self._arithmetic_precheck(__value, "__mul__")
+        new_units = self._arithmetic_precheck(__value, caller="__mul__")
         if isinstance(__value, Quantity):
             temp_dimensions = [
                 dim + __value.dimensions[idx] for idx, dim in enumerate(self.dimensions)
@@ -247,7 +214,7 @@ class Quantity(float):
         return self.__mul__(__value)
 
     def __truediv__(self, __value):
-        new_units = self._arithmetic_precheck(__value, "__truediv__")
+        new_units = self._arithmetic_precheck(__value, caller="__truediv__")
         if isinstance(__value, Quantity):
             temp_dimensions = [
                 dim - __value.dimensions[idx] for idx, dim in enumerate(self.dimensions)
@@ -310,19 +277,19 @@ class Quantity(float):
 class QuantityError(ValueError):
     """Custom quantity errors."""
 
-    def __init__(self, from_unit, to_unit):
-        """
-        Quantity errors with custom messages.
+    def __init__(self, err):
+        super().__init__(err)
 
-        Parameters
-        ----------
-        from_unit: str
-            Unit of quantity
-        to_unit: str
-            Desired conversion unit
-        """
-        self.from_unit = from_unit
-        self.to_unit = to_unit
+    @classmethod
+    def EXCESSIVE_PARAMETERS(cls):
+        return cls(
+            "Quantity only accepts 1 of the following parameters: (units) or (quantity_map) or (dimensions)."
+        )
 
-    def __str__(self):
-        return f"'{self.from_unit}' and '{self.to_unit}' have incompatible dimensions."
+    @classmethod
+    def INCOMPATIBLE_DIMENSIONS(cls, from_unit, to_unit):
+        return cls(f"`{from_unit}` and `{to_unit}` have incompatible dimensions.")
+
+    @classmethod
+    def INCOMPATIBLE_VALUE(cls, value):
+        return cls(f"`{value}` is incompatible with the current quantity object.")
---
+++ src/ansys/fluent/core/quantity/unit_system.py
@@ -0,0 +1,125 @@
+import ansys.fluent.core.quantity as q
+
+
+class UnitSystem:
+    """Initializes a unit system based on user-defined units or a pre-definned unit system.
+
+    Parameters
+    ----------
+    name: str
+        Custom name associated with a user-defined unit system.
+    base_units: list
+        Custom units associated with a user-defined unit system.
+    unit_sys: str
+        Pre-defined unit system.
+
+    Methods
+    -------
+    convert()
+        Converts from one unit system to to given unit system.
+
+    Returns
+    -------
+    Quantity instance.
+    """
+
+    def __init__(self, name: str = None, base_units: list = None, unit_sys: str = None):
+        self._units_table = q.UnitsTable()
+
+        if name and unit_sys or base_units and unit_sys:
+            raise UnitSystemError.EXCESSIVE_PARAMETERS()
+
+        if base_units:
+            if len(base_units) != q.Dimensions.max_dim_len():
+                raise UnitSystemError.BASE_UNITS_LENGTH(len(base_units))
+
+            for idx, unit in enumerate(base_units):
+                if unit not in self._units_table.fundamental_units:
+                    raise UnitSystemError.UNIT_UNDEFINED(unit)
+
+                if (idx + 1) != self._units_table.dimension_order[
+                    self._units_table.fundamental_units[unit]["type"]
+                ]:
+                    raise UnitSystemError.UNIT_ORDER(
+                        t1=list(self._units_table.dimension_order.keys())[idx],
+                        o1=idx + 1,
+                        t2=self._units_table.fundamental_units[unit]["type"],
+                        o2=self._units_table.dimension_order[
+                            self._units_table.fundamental_units[unit]["type"]
+                        ],
+                    )
+
+            self._name = name
+            self._base_units = base_units
+
+        if unit_sys is not None:
+            if unit_sys not in self._units_table.unit_systems:
+                raise UnitSystemError.INVALID_UNIT_SYS(unit_sys)
+
+            self._name = unit_sys
+            self._base_units = self._units_table.unit_systems[unit_sys]
+
+    def convert(self, quantity: q.Quantity) -> q.Quantity:
+        """Perform unit system conversions.
+
+        Parameters
+        ----------
+        quantity : Quantity
+            Desired quantity object to convert.
+
+        Returns
+        -------
+        Quantity
+            Quantity object containing desired unit system conversion.
+        """
+
+        new_dim = q.Dimensions(
+            dimensions=quantity.dimensions, unit_sys=self._base_units
+        )
+
+        return q.Quantity(value=quantity.value, units=new_dim.units)
+
+    @property
+    def name(self):
+        """Name associated with unit system."""
+        return self._name
+
+    @property
+    def base_units(self):
+        """Units associated with unit system."""
+        return self._base_units
+
+
+class UnitSystemError(ValueError):
+    """Custom unit system errors."""
+
+    def __init__(self, err):
+        super().__init__(err)
+
+    @classmethod
+    def EXCESSIVE_PARAMETERS(cls):
+        return cls(
+            "UnitSystem only accepts 1 of the following parameters: (name, base_units) or (unit_sys)."
+        )
+
+    @classmethod
+    def BASE_UNITS_LENGTH(cls, len):
+        return cls(
+            f"The `base_units` argument must contain 9 units, currently there are {len}."
+        )
+
+    @classmethod
+    def UNIT_UNDEFINED(cls, unit):
+        return cls(
+            f"`{unit}` is an undefined unit. To use `{unit}` add it to the `fundamental_units` table within quantity_config.yaml."
+        )
+
+    @classmethod
+    def UNIT_ORDER(cls, t1, o1, t2, o2):
+        return cls(
+            f"Expected unit of type: `{t1}` (order: {o1}), received unit of type: `{t2}` (order: {o2})."
+        )
+
+    @classmethod
+    def INVALID_UNIT_SYS(cls, sys):
+        return cls(f"`{sys}` is not a supported unit system.")
