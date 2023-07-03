997c998601254f437ab38e151a1d611f6a537392
ANSYS\spearson <sean.pearson@ansys.com>
2023-07-03 17:06:57 +0100

    tidy up

--- src/ansys/fluent/core/services/error_handler.py
+++ src/ansys/fluent/core/services/error_handler.py
@@ -15,17 +15,3 @@ def catch_grpc_error(f: Callable) -> Callable:
             raise RuntimeError(ex.details()) from None
 
     return func
-
-
-def catch_grpc_error_quiet(f: Callable) -> Callable:
-    """Decorator to catch gRPC errors."""
-
-    @functools.wraps(f)
-    # is the Callbale return type hint correct here?
-    def func(*args, **kwargs) -> Callable:
-        try:
-            return f(*args, **kwargs)
-        except BaseException:
-            pass
-
-    return func
