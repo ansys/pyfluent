94ff7f96f9fc3eb0f1611b499449e98dc3c8f762
Mainak Kundu <mainak.kundu@ansys.com>
2023-07-14 22:32:51 -0400

    Fluent version marker

--- tests/test_fluent_version_marker.py
+++ tests/test_fluent_version_marker.py
@@ -1,25 +1,54 @@
 import pytest
 
 
-def test_fluent_any():
+def test_dev_fluent_any():
     pass
 
 
 @pytest.mark.fluent_version(">=24.1")
-def test_fluent_gt_241():
+def test_dev_fluent_gt_241():
     pass
 
 
 @pytest.mark.fluent_version(">=23.2")
-def test_fluent_gt_232():
+def test_dev_fluent_gt_232():
     pass
 
 
 @pytest.mark.fluent_version(">=23.1")
-def test_fluent_gt_231():
+def test_dev_fluent_gt_231():
     pass
 
 
 @pytest.mark.fluent_version(">=22.2")
-def test_fluent_gt_222():
+def test_dev_fluent_gt_222():
+    pass
+
+
+@pytest.mark.nightly
+def test_nightly_fluent_any():
+    pass
+
+
+@pytest.mark.nightly
+@pytest.mark.fluent_version(">=24.1")
+def test_nightly_fluent_gt_241():
+    pass
+
+
+@pytest.mark.nightly
+@pytest.mark.fluent_version(">=23.2")
+def test_nightly_fluent_gt_232():
+    pass
+
+
+@pytest.mark.nightly
+@pytest.mark.fluent_version(">=23.1")
+def test_nightly_fluent_gt_231():
+    pass
+
+
+@pytest.mark.nightly
+@pytest.mark.fluent_version(">=22.2")
+def test_nightly_fluent_gt_222():
     pass
