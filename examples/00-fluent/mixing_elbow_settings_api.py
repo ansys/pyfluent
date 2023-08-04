7e599b6a977baee9cd23d1a076583a5c14352158
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-08-04 16:43:14 -0400

    Run dev-docs CI with 23.2 and 24.1 Fluent only (#1874)
    * Run dev-docs CI with 23.2 and 24.1 Fluent only
    
    * Remove version checks
    
    * Fix fielddata doc
    
    * Fix fielddata doc
    
    * Update doc/source/api/solver/fielddata.rst
    
    * Update fielddata.rst
    
    formatting
    
    ---------
    
    Co-authored-by: Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>

--- examples/00-fluent/mixing_elbow_settings_api.py
+++ examples/00-fluent/mixing_elbow_settings_api.py
@@ -74,12 +74,7 @@ solver.setup.models.energy.enabled = True
 # ~~~~~~~~~~~~~~~
 # Create a material named ``"water-liquid"``.
 
-if solver.get_fluent_version() == "22.2.0":
-    solver.setup.materials.copy_database_material_by_name(
-        type="fluid", name="water-liquid"
-    )
-else:
-    solver.setup.materials.database.copy_by_name(type="fluid", name="water-liquid")
+solver.setup.materials.database.copy_by_name(type="fluid", name="water-liquid")
 
 ###############################################################################
 # Set up cell zone conditions
@@ -103,13 +98,7 @@ solver.setup.cell_zone_conditions.fluid["elbow-fluid"].material = "water-liquid"
 # Hydraulic Diameter: 4 [inch]
 # Temperature: 293.15 [K]
 
-if solver.get_fluent_version() == "22.2.0":
-    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = {
-        "option": "constant or expression",
-        "constant": 0.4,
-    }
-else:
-    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = 0.4
+solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].vmag = 0.4
 solver.setup.boundary_conditions.velocity_inlet[
     "cold-inlet"
 ].ke_spec = "Intensity and Hydraulic Diameter"
@@ -117,13 +106,7 @@ solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].turb_intensity = 0
 solver.setup.boundary_conditions.velocity_inlet[
     "cold-inlet"
 ].turb_hydraulic_diam = "4 [in]"
-if solver.get_fluent_version() == "22.2.0":
-    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = {
-        "option": "constant or expression",
-        "constant": 293.15,
-    }
-else:
-    solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = 293.15
+solver.setup.boundary_conditions.velocity_inlet["cold-inlet"].t = 293.15
 
 # hot inlet (hot-inlet), Setting: Value:
 # Velocity Specification Method: Magnitude, Normal to Boundary
@@ -133,26 +116,14 @@ else:
 # Hydraulic Diameter: 1 [inch]
 # Temperature: 313.15 [K]
 
-if solver.get_fluent_version() == "22.2.0":
-    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = {
-        "option": "constant or expression",
-        "constant": 1.2,
-    }
-else:
-    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = 1.2
+solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].vmag = 1.2
 solver.setup.boundary_conditions.velocity_inlet[
     "hot-inlet"
 ].ke_spec = "Intensity and Hydraulic Diameter"
 solver.setup.boundary_conditions.velocity_inlet[
     "hot-inlet"
 ].turb_hydraulic_diam = "1 [in]"
-if solver.get_fluent_version() == "22.2.0":
-    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = {
-        "option": "constant or expression",
-        "constant": 313.15,
-    }
-else:
-    solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = 313.15
+solver.setup.boundary_conditions.velocity_inlet["hot-inlet"].t = 313.15
 
 # pressure outlet (outlet), Setting: Value:
 # Backflow Turbulent Intensity: 5 [%]
@@ -180,10 +151,7 @@ solver.solution.initialization.hybrid_initialize()
 # Solve for 150 iterations.
 
 solver.solution.run_calculation.iterate.get_attr("arguments")
-if solver.get_fluent_version() >= "23.1.0":
-    solver.solution.run_calculation.iterate(iter_count=150)
-else:
-    solver.solution.run_calculation.iterate(number_of_iterations=150)
+solver.solution.run_calculation.iterate(iter_count=150)
 
 ###############################################################################
 # Create velocity vectors
