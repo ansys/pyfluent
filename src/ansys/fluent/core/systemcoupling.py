b4e68686c5a48cc99c269ff2fc997a55b8393c65
Oleg Chernukhin <oleg.chernukhin@ansys.com>
2023-08-21 13:42:51 -0400

    update for consistency with pysystemcoupling changes

--- src/ansys/fluent/core/systemcoupling.py
+++ src/ansys/fluent/core/systemcoupling.py
@@ -3,8 +3,24 @@
 from dataclasses import dataclass
 import os
 from typing import List
-import xml.etree.ElementTree
+import xml.etree.ElementTree as XmlET
 
+@dataclass
+class Variable:
+    name: str
+    display_name: str
+    tensor_type: str
+    is_extensive: bool
+    location: str
+    quantity_type: str
+
+@dataclass
+class Region:
+    name: str
+    display_name: str
+    topology: str
+    input_variables: List[str]
+    output_variables: List[str]
 
 class SystemCoupling:
     """Wrap a System Coupling object, adding methods to discover more about the
@@ -20,88 +36,127 @@ class SystemCoupling:
     solve
     """
 
-    @dataclass
-    class Variable:
-        name: str
-        display_name: str
-        tensor_type: str
-        is_extensive: bool
-        location: str
-        quantity_type: str
-
-    @dataclass
-    class Region(object):
-        name: str
-        display_name: str
-        topology: str
-        input_variables: List[str]
-        output_variables: List[str]
-
     def __init__(self, solver):
         self._solver = solver
-        self.participant_type = "FLUENT"
 
-    def get_variables(self):
+    @property
+    def participant_type(self) -> str:
+        return "FLUENT"
+
+    def get_variables(self) -> List[Variable]:
         return self.__get_syc_setup()["variables"]
 
-    def get_regions(self):
+    def get_regions(self) -> List[Region]:
         return self.__get_syc_setup()["regions"]
 
-    def get_analysis_type(self):
+    def get_analysis_type(self) -> str:
         return self.__get_syc_setup()["analysis-type"]
 
-    def connect(self, host: str, port: int, name: str):
+    def connect(self, host: str, port: int, name: str) -> None:
+        print(f"Host = {host}, port = {port}, name = {name}", flush=True)
         self._solver.setup.models.system_coupling.connect_parallel(
             host=host, port=port, name=name
         )
 
-    def solve(self):
+    def solve(self) -> None:
         self._solver.setup.models.system_coupling.init_and_solve()
 
-    def __get_syc_setup(self):
+    def __get_syc_setup(self) -> dict:
+
+        def get_scp_string() -> str:
+            """ Get SCP file contents in the form of the XML string. """
+            scp_file_name = "fluent.scp"
+            self._solver.setup.models.system_coupling.write_scp_file(file_name=scp_file_name)
+            assert os.path.exists(
+                scp_file_name
+            ), "ERROR: could not create System Coupling .scp file"
+
+            with open(scp_file_name, "r") as f:
+                xml_string = f.read()
+
+            os.remove(scp_file_name)
+            return xml_string
+
+        def get_name(xml_element) -> str:
+            return xml_element.find("Name").text
+
+        def get_display_name(xml_element) -> str:
+            display_name_elem = xml_element.find("DisplayName")
+            if display_name_elem is not None:
+                return display_name_elem.text
+            else:
+                return get_name(xml_element)
+
+        def get_tensor_type(variable) -> str:
+            tensor_type_elem = variable.find("TensorType")
+            if tensor_type_elem is not None:
+                return tensor_type_elem.text
+
+            if get_name(variable) in {"force", "lorentz-force"}:
+                return "Vector"
+            else:
+                return "Scalar"
+
+        def get_is_extensive(variable) -> bool:
+            is_extensive_elem = variable.find("IsExtensive")
+            if is_extensive_elem is not None:
+                is_ext_map = {"True" : True, "False" : False}
+                return is_ext_map[is_extensive_elem.text]
+
+            ext_vars = {"force", "lortentz-force", "heatrate", "heatflow", "mass-flow-rate"}
+            if get_name(variable) in ext_vars:
+                return True
+            else:
+                return False
+
+        def get_location(variable) -> str:
+            # SCP file contents for location are not always correct,
+            # so do not rely on that.
+            # This has to do with old vs. new APIs. Here, assume
+            # only new APIs are used, so all variables are on elements,
+            # except displacements.
+            if get_name(variable) == "displacement":
+                return "Node"
+            else:
+                return "Element"
+
+        def get_quantity_type(variable) -> str:
+            quantity_type_elem = variable.find("QuantityType")
+            if quantity_type_elem is not None:
+                return quantity_type_elem.text
+            else:
+                return "Unspecified"
+
         setup_info = dict()
 
-        scp_file_name = "fluent.scp"
+        xml_root = XmlET.ElementTree(XmlET.fromstring(get_scp_string()))
+        cosim_control = xml_root.find("./CosimulationControl")
 
-        self._solver.setup.models.system_coupling.write_scp_file(
-            file_name=scp_file_name
-        )
+        setup_info["analysis-type"] = cosim_control.find("AnalysisType").text
 
-        assert os.path.exists(
-            scp_file_name
-        ), "ERROR: could not create System Coupling .scp file"
-        xmlRoot = xml.etree.ElementTree.parse(scp_file_name)
-        coSimControl = xmlRoot.find("./CosimulationControl")
-        setup_info["analysis-type"] = coSimControl.find("AnalysisType").text
         setup_info["variables"] = list()
-        for variable in coSimControl.find("Variables").findall("Variable"):
-            name = variable.find("Name").text
+        for variable in cosim_control.find("Variables").findall("Variable"):
             setup_info["variables"].append(
-                SystemCoupling.Variable(
-                    name=name,
-                    display_name=variable.find("DisplayName").text,
-                    tensor_type="Vector"
-                    if name in {"force", "lorentz-force"}
-                    else "Scalar",
-                    is_extensive=name
-                    in {"force", "lortentz-force", "heatrate", "heatflow"},
-                    location="Node" if name in {"displacement"} else "Element",
-                    quantity_type=variable.find("QuantityType").text,
+                Variable(
+                    name=get_name(variable),
+                    display_name = get_display_name(variable),
+                    tensor_type = get_tensor_type(variable),
+                    is_extensive= get_is_extensive(variable),
+                    location= get_location(variable),
+                    quantity_type = get_quantity_type(variable),
                 )
             )
-        regions = coSimControl.find("Regions").findall("Region")
+
+        regions = cosim_control.find("Regions").findall("Region")
         setup_info["regions"] = [
-            SystemCoupling.Region(
-                name=region.find("Name").text,
-                display_name=region.find("DisplayName").text,
-                topology=region.find("Topology").text,
-                input_variables=[var.text for var in region.find("InputVariables")],
-                output_variables=[var.text for var in region.find("OutputVariables")],
+            Region(
+                name = get_name(region),
+                display_name = get_display_name(region),
+                topology = region.find("Topology").text,
+                input_variables = [var.text for var in region.find("InputVariables")],
+                output_variables = [var.text for var in region.find("OutputVariables")],
             )
             for region in regions
         ]
-
-        # remove the generated scp file
-        os.remove(scp_file_name)
 
         return setup_info
