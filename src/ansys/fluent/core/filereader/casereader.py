3195bd7ca73f9cde19f4b091ea75e3feeabdf109
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2023-09-08 16:54:43 +0530

    Removed extra import statements.

--- src/ansys/fluent/core/filereader/casereader.py
+++ src/ansys/fluent/core/filereader/casereader.py
@@ -1,4 +1,2 @@
 # Compatibility aliases
 from .case_file import CaseFile as CaseReader  # noqa: F401
-from .case_file import InputParameter, OutputParameter  # noqa: F401
-from .case_file import _get_processed_string  # noqa: F401
