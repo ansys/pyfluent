93fc92dc6f13d64ce5205bdb1e08a9a4f89a667b
ANSYS\spearson <sean.pearson@ansys.com>
2023-09-22 13:37:46 +0100

    update docstrings

--- src/ansys/fluent/core/meshing/meshing_workflow.py
+++ src/ansys/fluent/core/meshing/meshing_workflow.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 from typing import Optional
 
+from ansys.fluent.core.services.datamodel_se import PyMenuGeneric
 from ansys.fluent.core.workflow import WorkflowWrapper
 
 
