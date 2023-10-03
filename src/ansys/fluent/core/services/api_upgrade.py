e94151d09b320c23b3842db77e8ea15d99f21c76
Mainak Kundu <mainak.kundu@ansys.com>
2023-10-03 11:58:11 -0400

    Remove usage of removeprefix

--- src/ansys/fluent/core/services/api_upgrade.py
+++ src/ansys/fluent/core/services/api_upgrade.py
@@ -38,4 +38,4 @@ class ApiUpgradeAdvisor:
                 print(
                     "The following solver settings object method could also be used to execute the above command:"
                 )
-                print(f"<solver_session>.{journal_str.removeprefix('solver.')}")
+                print(f"<solver_session>.{journal_str[7:]}")
