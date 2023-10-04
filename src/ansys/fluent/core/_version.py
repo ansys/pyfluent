90377dce58a5458dbbe550321176d04fb3cddf11
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2023-10-05 00:28:35 +0530

    Bump version to v0.18.dev5 (#2078)

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 18, "dev4"
+version_info = 0, 18, "dev5"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
