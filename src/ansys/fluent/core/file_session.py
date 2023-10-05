c360f913ec068bb8d4b44ac8db2b0fbe9eef0b1f
Mainak Kundu <mainak.kundu@ansys.com>
2023-10-05 14:48:14 -0400

    Add doc for exceptions

--- src/ansys/fluent/core/file_session.py
+++ src/ansys/fluent/core/file_session.py
@@ -105,6 +105,13 @@ class Transaction:
         Returns
         -------
         None
+
+        Raises
+        ------
+        RuntimeError
+            If surface names or surface ids are not provided.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
         """
         if surface_ids is None:
             surface_ids = []
@@ -152,6 +159,13 @@ class Transaction:
         Returns
         -------
         None
+
+        Raises
+        ------
+        RuntimeError
+            If surface names or surface ids are not provided.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
         """
         if surface_ids is None:
             surface_ids = []
@@ -199,6 +213,11 @@ class Transaction:
         Returns
         -------
         None
+
+        Raises
+        ------
+        RuntimeError
+            Always, as pathlines are not supported.
         """
         raise RuntimeError("Path-lines not supported.")
 
@@ -210,6 +229,11 @@ class Transaction:
         Dict[int, Dict[int, Dict[str, np.array]]]
             Data is returned as dictionary of dictionaries in the following structure:
             tag Union[int, Tuple]-> surface_id [int] -> field_name [str] -> field_data[np.array]
+
+        Raises
+        ------
+        RuntimeError
+            If any field other than ``"velocity"`` is provided.
         """
         mesh = self._file_session._case_file.get_mesh()
         field_data = {}
@@ -301,6 +325,11 @@ class FileFieldData:
              If a surface name is provided as input, face vertices, connectivity data, and normal or centroid data are returned.
              If surface IDs are provided as input, a dictionary containing a map of surface IDs to face
              vertices, connectivity data, and normal or centroid data is returned.
+
+        Raises
+        ------
+        RuntimeError
+            If surface names or surface ids are not provided.
         """
         if surface_ids and surface_name:
             raise RuntimeError("Please provide either surface name or surface ids.")
@@ -380,6 +409,15 @@ class FileFieldData:
             If a surface name is provided as input, scalar field data is returned. If surface
             IDs are provided as input, a dictionary containing a map of surface IDs to scalar
             field data.
+
+        Raises
+        ------
+        RuntimeError
+            If surface names or surface ids are not provided.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
         """
         if surface_ids and surface_name:
             raise RuntimeError("Please provide either surface name or surface ids.")
@@ -459,6 +497,17 @@ class FileFieldData:
             If a surface name is provided as input, vector field data is returned.
             If surface IDs are provided as input, a dictionary containing a map of
             surface IDs to vector field data is returned.
+
+        Raises
+        ------
+        RuntimeError
+            If surface names or surface ids are not provided.
+        RuntimeError
+            If any field other than ``"velocity"`` is provided.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
+        RuntimeError
+            If field name does not have prefix ``phase-`` for multi-phase cases.
         """
         if surface_ids and surface_name:
             raise RuntimeError("Please provide either surface name or surface ids.")
@@ -537,6 +586,11 @@ class FileFieldData:
         Dict
             Dictionary containing a map of surface IDs to the pathline data.
             For example, pathlines connectivity, vertices, and field.
+
+        Raises
+        ------
+        RuntimeError
+            Always, as pathlines are not supported.
         """
         raise RuntimeError("Path-lines not supported.")
 
