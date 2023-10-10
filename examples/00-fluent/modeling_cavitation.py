593be98b8ee0ed5106e59546678bad7ca01c31ba
Adam Boutin <adam.boutin@ansys.com>
2023-10-10 13:17:19 -0400

    Residuals changed to tui.

--- examples/00-fluent/modeling_cavitation.py
+++ examples/00-fluent/modeling_cavitation.py
@@ -2,15 +2,15 @@
 
 Modeling Cavitation
 -------------------
-This example examines the pressure-driven cavitating flow of water
-through a sharp-edged orifice. This is a typical configuration in
-fuel injectors, and brings a challenge to the physics and numerics
-of cavitation models because of the high pressure differentials
-involved and the high ratio of liquid to vapor density.
+This example examines the pressure-driven cavitating flow of water through a
+sharp-edged orifice. This is a typical configuration in fuel injectors, and
+brings a challenge to the physics and numerics of cavitation models because of
+the high pressure differentials involved and the high ratio of liquid to vapor
+density.
 
-This example uses the multiphase modeling capability of Ansys
-Fluent, and will be able to predict the strong cavitation near the
-orifice after flow separation at a sharp edge.
+This example uses the multiphase modeling capability of Ansys Fluent, and will
+be able to predict the strong cavitation near the orifice after flow separation
+at a sharp edge.
 
 **Workflow tasks**
 
@@ -22,18 +22,19 @@ This example demonstrates how to do the following:
 
 **Problem description**
 
-The problem considers the cavitation caused by the flow separation
-after a sharp-edged orifice. The flow is pressure driven, with an
-inlet pressure of 5 x 10^5 Pa and an outlet pressure of 9.5 x 10^4 Pa.
-The orifice diameter is 4 x 10^-3 m, and the geometrical parameters
-of the orifice are D/d = 2.88 and L/d = 4, where D, d, and L are the
-inlet diameter, orifice diameter, and orifice length respectively.
+The problem considers the cavitation caused by the flow separation after a
+sharp-edged orifice. The flow is pressure driven, with an inlet pressure of 5 x
+10^5 Pa and an outlet pressure of 9.5 x 10^4 Pa. The orifice diameter is 4 x
+10^-3 m, and the geometrical parameters of the orifice are D/d = 2.88 and L/d =
+4, where D, d, and L are the inlet diameter, orifice diameter, and orifice
+length respectively.
 """
 
 ###############################################################################
 # Example Setup
 # -------------
-# Before you can begin, you must set up the example and initialize this workflow.
+# Before you can begin, you must set up the example and initialize this
+# workflow.
 #
 # Perform required imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~
@@ -56,7 +57,7 @@ cav_file = examples.download_file(
 
 solver = pyfluent.launch_fluent(
     precision="double",
-    processor_count=1,
+    processor_count=4,
     mode="solver",
     version="2d",
     cwd=pyfluent.EXAMPLES_PATH,
@@ -68,12 +69,6 @@ solver = pyfluent.launch_fluent(
 solver.read_case("cav.msh")
 
 solver.mesh.check()
-
-###############################################################################
-# Set units for length
-# ~~~~~~~~~~~~~~~~~~~~
-
-solver.tui.define.units("length", "m")
 
 ###############################################################################
 # Specify an axisymmetric model
@@ -92,8 +87,7 @@ solver.tui.define.models.multiphase.mixture_parameters("no", "implicit")
 # Select turbulence model
 # ~~~~~~~~~~~~~~~~~~~~~~~
 
-# model : k-omega
-# k-omega model : sst
+# model : k-omega k-omega model : sst
 
 solver.setup.models.viscous = {"model": "k-omega", "k_omega_model": "sst"}
 
@@ -102,12 +96,8 @@ solver.setup.models.viscous = {"model": "k-omega", "k_omega_model": "sst"}
 # ~~~~~~~~~~~~~~~~
 # Create the material ``water``.
 
-# name: water
-# density : constant
-# reference density : 1000 [kg/m^3]
-# viscosity : constant
-# viscosity method : constant
-# reference viscosity : 0.001 [kg/(m s)]
+# name: water density : constant reference density : 1000 [kg/m^3] viscosity :
+# constant viscosity method : constant reference viscosity : 0.001 [kg/(m s)]
 
 water = {
     "density": {"option": "constant", "value": 1000},
@@ -116,7 +106,8 @@ water = {
 
 solver.setup.materials.fluid["water"] = water
 
-# Copy water vapor from the materials database and modify the properties of your local copy.
+# Copy water vapor from the materials database and modify the properties of your
+# local copy.
 
 solver.setup.materials.database.copy_by_name(type="fluid", name="water-vapor")
 
@@ -153,7 +144,6 @@ solver.tui.define.phases.set_domain_properties.phase_domains.vapor.material(
 solver.tui.define.phases.set_domain_properties.interaction_domain.heat_mass_reactions.mass_transfer(
     1, "liquid", "vapor", "cavitation", "1", "no", "no", "no"
 )
-"""Set the vaporization pressure to 3540 Pa and bubble number density to 1e+11."""
 
 ###############################################################################
 # Boundary Conditions
@@ -162,12 +152,10 @@ solver.tui.define.phases.set_domain_properties.interaction_domain.heat_mass_reac
 
 inlet_1 = solver.setup.boundary_conditions.pressure_inlet["inlet_1"].phase
 
-# Set direction specification method to ``Normal to Boundary``.
-# Set gauge total pressure as 500000 Pa.
-# Set supersonic or initial gauge pressure as 449000 Pa.
-# Set turbulent intensity as 0.05.
-# Set turbulent specification to ``Intensity and Viscosity Ratio``.
-# Set turbulent viscosity ratio as 10.
+# Set direction specification method to ``Normal to Boundary``. Set gauge total
+# pressure as 500000 Pa. Set supersonic or initial gauge pressure as 449000 Pa.
+# Set turbulent intensity as 0.05. Set turbulent specification to ``Intensity
+# and Viscosity Ratio``. Set turbulent viscosity ratio as 10.
 
 in_mixture = {
     "direction_spec": "Normal to Boundary",
@@ -191,10 +179,9 @@ solver.setup.boundary_conditions.copy(from_="inlet_1", to="inlet_2")
 
 outlet = solver.setup.boundary_conditions.pressure_outlet["outlet"].phase
 
-# Set the gauge pressure as 95000.
-# Set turbulent intensity as 0.05.
-# Set turbulent specification to ``Intensity and Viscosity Ratio``.
-# Set turbulent viscosity ratio as 10.
+# Set the gauge pressure as 95000. Set turbulent intensity as 0.05. Set
+# turbulent specification to ``Intensity and Viscosity Ratio``. Set turbulent
+# viscosity ratio as 10.
 
 out_mixture = {
     "gauge_pressure": {"value": 95000},
@@ -224,9 +211,9 @@ solver.setup.general.operating_conditions.operating_pressure = 0
 
 methods = solver.solution.methods
 
-# Set Coupled from pressure-velocity coupling, presto! for spatial discretization,
-# quick for momentum and volume fraction, first order upwind for turbulent kinetic
-# energy and turbulent dissipation rate.
+# Set Coupled from pressure-velocity coupling, presto! for spatial
+# discretization, quick for momentum and volume fraction, first order upwind for
+# turbulent kinetic energy and turbulent dissipation rate.
 
 discretization_scheme = {
     "k": "first-order-upwind",
@@ -258,21 +245,15 @@ solver.solution.controls.pseudo_time_explicit_relaxation_factor.global_dt_pseudo
 
 # Enable the plotting of residuals during the calculation.
 
-residual = solver.solution.monitor.residual
 
-residual.options.plot = True
+solver.tui.solve.monitors.residual.plot("yes")
 
-# Set the absolute criteria of continuity, x-velocity, y-velocity, k, omega, and vf-vapor to 1e-05
+# Set the absolute criteria of continuity, x-velocity, y-velocity, k, omega, and
+# vf-vapor to 1e-05
 
-equations = {
-    "continuity": {"convergence_criteria": 1e-05},
-    "x-velocity": {"convergence_criteria": 1e-05},
-    "y-velocity": {"convergence_criteria": 1e-05},
-    "k": {"convergence_criteria": 1e-05},
-    "omega": {"convergence_criteria": 1e-05},
-    "vf-vapor": {"convergence_criteria": 1e-05},
-}
-residual.equations = equations
+solver.tui.solve.monitors.residual.convergence_criteria(
+    1e-05, 1e-05, 1e-05, 1e-05, 1e-05, 1e-05
+)
 
 # Enable use specified initial pressure on inlets.
 
@@ -287,7 +268,7 @@ solver.solution.initialization.hybrid_initialize()
 ###############################################################################
 # Save and Run
 # ~~~~~~~~~~~~
-# Solve the case file (``cav.cas.h5``).
+# Save the case file (``cav.cas.h5``).
 
 solver.file.write(file_name="cav", file_type="case")
 
@@ -319,14 +300,12 @@ contour_static_pressure = {
 
 solver.results.graphics.contour["contour_static_pressure"] = contour_static_pressure
 
-# Mirror the display.
+# Mirror the display around the symmetry plane to show the full model.
 
 solver.tui.display.set.mirror_zones(["symm_2", "symm_1"])
 
 ###############################################################################
-# .. image:: _static\cavitation_model_012.png
-#   :width: 500pt
-#   :align: center
+# .. image:: _static\cavitation_model_012.png :width: 500pt :align: center
 
 # Create and plot a contour definition of the turbulent kinetic energy.
 
@@ -344,18 +323,14 @@ contour_tke = {
 solver.results.graphics.contour["contour_tke"] = contour_tke
 
 ###############################################################################
-# .. image:: _static\cavitation_model_011.png
-#   :width: 500pt
-#   :align: center
+# .. image:: _static\cavitation_model_011.png :width: 500pt :align: center
 
 # Create and plot a contour definition of the volume fraction of water vapor.
 
 solver.results.graphics.contour.create("contour_vf_vapor")
 
 ###############################################################################
-# .. image:: _static\cavitation_model.png
-#   :width: 500pt
-#   :align: center
+# .. image:: _static\cavitation_model.png :width: 500pt :align: center
 
 # Set filled to True, coloring to 'banded' and field to 'static pressure'.
 
