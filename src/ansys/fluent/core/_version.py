f4dc22967fe03d01b15921226f39fa55fc264342
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-10-11 14:13:17 -0400

    Bump version 0.18 (#2128)

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 18, "dev7"
+version_info = 0, 18, 0
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
