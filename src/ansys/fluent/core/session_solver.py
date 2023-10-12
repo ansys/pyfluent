09fa0e950860cfd2c235c0d63dab4a3677c33d22
Harshal Pohekar <harshal.pohekar@ansys.com>
2023-10-12 09:28:32 +0530

    Seamless File Handling

--- src/ansys/fluent/core/session_solver.py
+++ src/ansys/fluent/core/session_solver.py
@@ -1,10 +1,13 @@
 """Module containing class encapsulating Fluent connection."""
 
+
 from asyncio import Future
 import functools
 import importlib
 import logging
+import os
 import threading
+import time
 
 from ansys.fluent.core.services.datamodel_se import PyMenuGeneric
 from ansys.fluent.core.services.datamodel_tui import TUIMenu
@@ -18,6 +21,7 @@ from ansys.fluent.core.systemcoupling import SystemCoupling
 from ansys.fluent.core.utils.execution import asynchronous
 from ansys.fluent.core.utils.fluent_version import get_version_for_filepath
 from ansys.fluent.core.workflow import WorkflowWrapper
+import ansys.platform.instancemanagement as pypim
 
 tui_logger = logging.getLogger("pyfluent.tui")
 datamodel_logger = logging.getLogger("pyfluent.datamodel")
@@ -41,6 +45,8 @@ class Solver(BaseSession):
         """
         super(Solver, self).__init__(fluent_connection=fluent_connection)
         self._build_from_fluent_connection(fluent_connection)
+        self._fluent_connection = fluent_connection
+        self._server_file_manager = None
 
     def _build_from_fluent_connection(self, fluent_connection):
         self._tui_service = self.datamodel_service_tui
@@ -204,7 +210,7 @@ class Solver(BaseSession):
             except Exception:
                 fut_session.exit()
 
-    def read_case(self, file_name: str, lightweight_mode: bool = False):
+    def read_case_lightweight(self, file_name: str, lightweight_mode: bool = False):
         """Read a case file using light IO mode if ``pyfluent.USE_LIGHT_IO`` is set to
         ``True``.
 
@@ -228,3 +234,130 @@ class Solver(BaseSession):
             fut.add_done_callback(functools.partial(Solver._sync_from_future, self))
         else:
             self.file.read(file_type="case", file_name=file_name)
+
+    def read_case(
+        self,
+        file_name: str,
+    ):
+        """Reads a case file.
+
+        Parameters
+        ----------
+        file_name : str
+            Case file name
+        """
+        if not self._server_file_manager:
+            self._server_file_manager = _ServerFileManager(self._fluent_connection)
+        return self._server_file_manager.read_case(
+            file_name,
+        )
+
+    def write_case(
+        self,
+        file_name: str,
+    ):
+        """Writes a case file.
+
+        Parameters
+        ----------
+        file_name : str
+            Case file name
+        """
+        if not self._server_file_manager:
+            self._server_file_manager = _ServerFileManager(self._fluent_connection)
+        return self._server_file_manager.write_case(file_name)
+
+
+class _ServerFileManager(Solver):
+    """Supports file upload and download for every existing file read,
+    write methods respectively in the cloud particularly in Ansys lab.
+    Here we are supporting upload and download methods in existing session
+    methods. These would be no-ops if PyPIM is not configured or not authorized
+    with the appropriate service. This will be used for internal purpose only.
+
+    Methods
+    -------
+    read_case(
+        file_name
+        )
+        Read a case file.
+
+    write_case(
+        file_name
+        )
+        Write a case file.
+    """
+
+    def __init__(self, fluent_connection):
+        super(_ServerFileManager, self).__init__(fluent_connection)
+        self.pim_instance = fluent_connection._remote_instance
+        self.file_service = None
+        try:
+            upload_server = self.pim_instance.services["http-simple-upload-server"]
+        except (AttributeError, KeyError):
+            pass
+        else:
+            from simple_upload_server.client import Client
+
+            self.file_service = Client(
+                token="token", url=upload_server.uri, headers=upload_server.headers
+            )
+
+    def _wait_for_file(self, file_name, file_service):
+        start_time = time.time()
+        max_wait_time = 100
+        while (time.time() - start_time) < max_wait_time:
+            if file_service.file_exist(os.path.basename(file_name)):
+                break
+            max_wait_time -= 1
+            time.sleep(3)
+        else:
+            raise FileNotFoundError(f"{file_name} does not exist.")
+
+    def read_case(
+        self,
+        file_name: str,
+    ):
+        """Reads a case file.
+
+        Parameters
+        ----------
+        file_name : str
+            Case file name
+
+        Raises
+        ------
+        FileNotFoundError
+            If a case file does not exist.
+        """
+        if pypim.is_configured():
+            if os.path.isfile(file_name):
+                if not self.file_service.file_exist(os.path.basename(file_name)):
+                    self.upload(os.path.basename(file_name))
+                    self._wait_for_file(file_name, self.file_service)
+            elif self.file_service.file_exist(os.path.basename(file_name)):
+                pass
+            else:
+                raise FileNotFoundError(f"{file_name} does not exist.")
+            self.file.read_case(file_name=os.path.basename(file_name))
+        else:
+            self.file.read_case(file_name=file_name)
+
+    def write_case(
+        self,
+        file_name: str,
+    ):
+        """Writes a case file.
+
+        Parameters
+        ----------
+        file_name : str
+            Case file name
+        """
+        self.file.write_case(file_name=os.path.basename(file_name))
+        if pypim.is_configured():
+            self._wait_for_file(file_name, self.file_service)
+            if os.path.isfile(file_name):
+                print(f"\nFile already exists. File path:\n{file_name}\n")
+            else:
+                self.download(os.path.basename(file_name), ".")
