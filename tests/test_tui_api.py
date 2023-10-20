627072011992b36d12f950381f016b9b09ee9a0b
Mainak Kundu <mainak.kundu@ansys.com>
2023-10-20 14:31:49 -0400

    test_report_system_proc_stats_tui

--- tests/test_tui_api.py
+++ tests/test_tui_api.py
@@ -1,16 +1,11 @@
-import pytest
 from util.meshing_workflow import new_mesh_session  # noqa: F401
 from util.solver_workflow import new_solver_session  # noqa: F401
 
 from ansys.fluent.core.services.datamodel_tui import TUIMenu
 
 
-@pytest.mark.skip("randomly failing due to missing transcript capture")
 def test_report_system_proc_stats_tui(new_solver_session, capsys) -> None:
-    new_solver_session.transcript.start()
-    # Issue: Transcript missing for the first TUI command
-    new_solver_session.solver.tui.report.system.sys_stats()
-    new_solver_session.solver.tui.report.system.sys_stats()
+    new_solver_session.tui.report.system.sys_stats()
     captured = capsys.readouterr()
     assert "CPU" in captured.out
 
