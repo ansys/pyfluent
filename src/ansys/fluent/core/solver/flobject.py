39fa5416e8cbba433b81409e2ea165e83944f877
Adam Boutin <adam.boutin@ansys.com>
2023-11-02 10:53:33 -0400

    Removed unneeded print.

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -362,9 +362,7 @@ class Real(SettingsBase[RealType], Numerical):
         """Set the state of the object."""
         if isinstance(state, ansunits.Quantity):
             unit = self._quantity_map(self.get_attr("units-quantity"))
-            print(unit)
             state = state.to(ansunits.QuantityMap({unit: 1}).units)
-            print(state)
             return self.flproxy.set_var(
                 self.path, self.to_scheme_keys(state.si_value), **kwargs
             )
