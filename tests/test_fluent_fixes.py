84ca22a644b88fcddab51494ab7a2c079fcfb044
Harshal Pohekar <106588300+hpohekar@users.noreply.github.com>
2023-11-02 20:45:51 +0530

    Fix for #2188 (#2193)
    * Fix for #2188
    
    * test for #2188
    
    * test for #2188
    
    * test for 232 and 241

--- tests/test_fluent_fixes.py
+++ tests/test_fluent_fixes.py
@@ -6,7 +6,7 @@ from ansys.fluent.core import examples
 
 @pytest.mark.nightly
 @pytest.mark.fluent_version("==23.2")
-def test_1364(new_solver_session):
+def test_allowed_values_on_report_definitions_1364(new_solver_session):
     solver = new_solver_session
 
     import_filename = examples.download_file(
@@ -33,7 +33,8 @@ def test_1364(new_solver_session):
     assert report_def.expr_list.allowed_values() == None
 
 
-def test_637_974_1744(new_solver_session):
+@pytest.mark.fluent_version(">=23.2")
+def test_monitors_list_set_data_637_974_1744_2188(new_solver_session):
     solver_session = new_solver_session
 
     import_case = examples.download_file(
@@ -66,3 +67,22 @@ def test_637_974_1744(new_solver_session):
     )
 
     assert mp
+
+    sample_report_plot = solver_session.solution.monitor.report_plots.create(
+        "sample-report-plot"
+    )
+    sample_report_plot.report_defs = "mass-bal"
+
+    solver_session.solution.initialization.hybrid_initialize()
+    solver_session.tui.solve.iterate()
+
+    new_monitors_list = solver_session.monitors_manager.get_monitor_set_names()
+
+    assert new_monitors_list == [
+        "residual",
+        "mass-bal-rplot",
+        "mass-tot-rplot",
+        "mass-in-rplot",
+        "point-vel-rplot",
+        "sample-report-plot",
+    ]
