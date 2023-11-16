949b0ec6205c76ca4e8a6cddeb1daed974a83eab
Harshal Pohekar <harshal.pohekar@ansys.com>
2023-11-16 13:55:59 +0530

    use an exception in tests

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -3,6 +3,7 @@ from util.meshing_workflow import new_mesh_session  # noqa: F401
 from util.solver_workflow import new_solver_session  # noqa: F401
 
 from ansys.fluent.core import examples
+from ansys.fluent.core.utils.file_transfer_service import PyPIMConfigurationError
 
 import_file_name = examples.download_file(
     "mixing_elbow.msh.h5", "pyfluent/mixing_elbow"
@@ -11,27 +12,27 @@ import_file_name = examples.download_file(
 
 def test_meshing_session_upload(new_mesh_session):
     session = new_mesh_session
-    with pytest.raises(ConnectionError) as e_info:
+    with pytest.raises(PyPIMConfigurationError) as e_info:
         session.remote_file_handler.upload(import_file_name)
     session.exit()
 
 
 def test_meshing_session_download(new_mesh_session):
     session = new_mesh_session
-    with pytest.raises(ConnectionError) as e_info:
+    with pytest.raises(PyPIMConfigurationError) as e_info:
         session.remote_file_handler.download(import_file_name)
     session.exit()
 
 
 def test_solver_session_upload(new_solver_session):
     session = new_solver_session
-    with pytest.raises(ConnectionError) as e_info:
+    with pytest.raises(PyPIMConfigurationError) as e_info:
         session.remote_file_handler.upload(import_file_name)
     session.exit()
 
 
 def test_solver_session_download(new_solver_session):
     session = new_solver_session
-    with pytest.raises(ConnectionError) as e_info:
+    with pytest.raises(PyPIMConfigurationError) as e_info:
         session.remote_file_handler.download(import_file_name)
     session.exit()
