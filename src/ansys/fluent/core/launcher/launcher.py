948e2d76e44aca9c50592dd565ff2cdd32eba209
Harshal Pohekar <harshal.pohekar@ansys.com>
2023-11-21 17:20:04 +0530

    Revert "make get_fluent_launcher_mode private"
    This reverts commit 7c46a313100f9ef232af3bb096b9340a10d1c502.

--- src/ansys/fluent/core/launcher/launcher.py
+++ src/ansys/fluent/core/launcher/launcher.py
@@ -559,8 +559,7 @@ class LaunchFluentError(Exception):
         super().__init__(details)
 
 
-def _get_fluent_launcher_mode(start_container, container_dict):
-    """Get Fluent launch mode."""
+def get_fluent_launcher_mode(start_container, container_dict):
     if pypim.is_configured():
         fluent_launch_mode = LaunchMode.PIM
     elif start_container is True or (
@@ -1055,7 +1054,7 @@ def launch_fluent(
     """
     argvals = locals().copy()
     new_session, meshing_mode, argvals, mode = _get_session_info(argvals, mode)
-    fluent_launch_mode = _get_fluent_launcher_mode(
+    fluent_launch_mode = get_fluent_launcher_mode(
         start_container=start_container, container_dict=container_dict
     )
     argvals = locals().copy()
