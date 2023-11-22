56724bdd07f0832132b9ef5e5a29b8c694678876
Adam Boutin <adam.boutin@ansys.com>
2023-11-22 08:22:31 -0700

    monkeypatch command changed.

--- tests/test_launcher_remote.py
+++ tests/test_launcher_remote.py
@@ -17,6 +17,7 @@ from ansys.fluent.core.fluent_connection import (
 from ansys.fluent.core.launcher import launcher
 from ansys.fluent.core.session import BaseSession
 import ansys.fluent.core.utils.fluent_version as docker_image_version
+from ansys.fluent.core.utils.fluent_version import FluentVersion
 from ansys.fluent.core.utils.networking import get_free_port
 import ansys.platform.instancemanagement as pypim
 
@@ -54,7 +55,7 @@ def test_launch_remote_instance(monkeypatch, new_solver_session):
 
     if os.getenv("FLUENT_IMAGE_TAG"):
         monkeypatch.setattr(
-            launcher, "get_ansys_version", lambda: docker_image_version.get_version()
+            FluentVersion, "current", lambda: docker_image_version.get_version()
         )
 
     # Start fluent with launch_fluent
