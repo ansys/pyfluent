7ae742a2248fa3f7f49b02828a6302302548de95
aboutin <adam.boutin@ansys.com>
2023-11-29 15:46:19 -0500

    Set FLUENT_IMAGE_TAG instead of passing args.

--- src/ansys/fluent/core/launcher/container_launcher.py
+++ src/ansys/fluent/core/launcher/container_launcher.py
@@ -178,6 +178,8 @@ class DockerLauncher:
         args = _build_fluent_launch_args_string(**self.argvals).split()
         if self.meshing_mode:
             args.append(" -meshing")
+        if self.product_version:
+            os.environ["FLUENT_IMAGE_TAG"] = f"v{self.product_version}"
 
         if self.dry_run:
             if self.container_dict is None:
