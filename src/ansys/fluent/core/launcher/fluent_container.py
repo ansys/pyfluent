74ade5bab70a5857f551b220e21e4f0b14ad35a6
Aseem <aseem.jain@ansys.com>
2023-12-10 15:57:23 +0530

    test

--- src/ansys/fluent/core/launcher/fluent_container.py
+++ src/ansys/fluent/core/launcher/fluent_container.py
@@ -143,6 +143,7 @@ def configure_container_dict(
     else:
         logger.debug(f"container_dict before processing: {container_dict}")
 
+    print('host_mount_path, container_mount_path', host_mount_path, container_mount_path, container_dict)
     if not host_mount_path:
         host_mount_path = pyfluent.EXAMPLES_PATH
     elif "volumes" in container_dict:
