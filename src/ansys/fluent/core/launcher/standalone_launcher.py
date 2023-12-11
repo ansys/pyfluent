838d03776afd8c154fce850cc703583bf5f989a8
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2023-12-11 15:00:22 +0530

    Support Slurm options in launch_fluent (#2269)
    * Support scheduler options in launch_fluent
    
    * SlurmLauncher __init__
    
    * start_timeout
    
    * Fix
    
    * Update src/ansys/fluent/core/launcher/launcher_utils.py
    
    Co-authored-by: Raphael Luciano <raphael.luciano@ansys.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * doc
    
    * doc
    
    * doc
    
    * Fix cancel
    
    * test SlurmFuture
    
    * Fix docformatter issues
    
    ---------
    
    Co-authored-by: Raphael Luciano <raphael.luciano@ansys.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ansys/fluent/core/launcher/standalone_launcher.py
+++ src/ansys/fluent/core/launcher/standalone_launcher.py
@@ -59,6 +59,7 @@ class StandaloneLauncher:
         cwd: Optional[str] = None,
         topy: Optional[Union[str, list]] = None,
         start_watchdog: Optional[bool] = None,
+        scheduler_options: Optional[dict] = None,
         **kwargs,
     ):
         """Launch Fluent session in standalone mode.
@@ -176,6 +177,8 @@ class StandaloneLauncher:
         _process_invalid_args(dry_run, "standalone", argvals)
         args = _get_argvals(argvals, mode)
         argvals.update(args)
+        if argvals["start_timeout"] is None:
+            argvals["start_timeout"] = 60
         for arg_name, arg_values in argvals.items():
             setattr(self, arg_name, arg_values)
         self.argvals = argvals
@@ -204,7 +207,7 @@ class StandaloneLauncher:
         sifile_last_mtime = Path(server_info_file_name).stat().st_mtime
         if self.env is None:
             setattr(self, "env", {})
-        kwargs = _get_subprocess_kwargs_for_fluent(self.env)
+        kwargs = _get_subprocess_kwargs_for_fluent(self.env, self.argvals)
         if self.cwd:
             kwargs.update(cwd=self.cwd)
         launch_string += _build_journal_argument(self.topy, self.journal_file_names)
