8579bd6cc4a8af07b9d9db39ffa917f49b0e9e04
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-01-02 15:24:35 +0530

    datamodel command name approach removed

--- src/ansys/fluent/core/services/datamodel_se.py
+++ src/ansys/fluent/core/services/datamodel_se.py
@@ -1312,13 +1312,13 @@ class PyCommand:
         Any
             Return value.
         """
-        if "Read" in self.__class__.__name__:
-            self.service.remote_file_handler.upload(file_name=kwds["FileName"])
-        self.service.execute_command(
+        # if "Read" in self.__class__.__name__:
+        #     self.service.remote_file_handler.upload(file_name=kwds["FileName"])
+        return self.service.execute_command(
             self.rules, convert_path_to_se_path(self.path), self.command, kwds
         )
-        if "Write" in self.__class__.__name__:
-            self.service.remote_file_handler.download(file_name=kwds["FileName"])
+        # if "Write" in self.__class__.__name__:
+        #     self.service.remote_file_handler.download(file_name=kwds["FileName"])
 
     def help(self) -> None:
         """Prints help string."""
