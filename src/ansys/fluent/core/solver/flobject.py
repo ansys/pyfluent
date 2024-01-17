08e545731a3deca5d45df1883c499a3cea643309
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-01-17 16:51:28 +0530

    Update docstring.

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -33,7 +33,7 @@ settings_logger = logging.getLogger("pyfluent.settings_api")
 
 
 class InactiveObjectError(RuntimeError):
-    """Raised on attempt to access an inactive object."""
+    """Inactive object access.."""
 
     def __init__(self, python_path):
         super().__init__(f"'{python_path}' is currently inactive.")
