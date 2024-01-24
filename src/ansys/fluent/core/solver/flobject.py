eaa3c77f03eb3c7af0195fa2442ce88aa1289858
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-01-24 20:01:41 +0530

    recursion error fix

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -138,7 +138,9 @@ class Base:
 
         Supports file upload and download.
         """
-        return self._remote_file_handler or self._parent.remote_file_handler
+        if self._remote_file_handler is None:
+            return self._parent.remote_file_handler
+        return self._remote_file_handler
 
     _name = None
     fluent_name = None
