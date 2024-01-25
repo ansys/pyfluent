a02274f4af51e8f4a93fc7423c0b61bda0855d88
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2024-01-25 09:51:41 +0530

    build: Bump version to 0.19.2 (#2390)
    Bump version to 0.19.2

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 19, 1
+version_info = 0, 19, 2
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
