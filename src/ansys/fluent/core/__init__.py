d768f625e3c29724c9d94b83b88ded3ff90a2ee8
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-02-07 19:15:32 +0530

    rename version-info -> git_version

--- src/ansys/fluent/core/__init__.py
+++ src/ansys/fluent/core/__init__.py
@@ -35,7 +35,7 @@ if os.path.exists(_README_FILE):
         __doc__ = f.read()
 
 
-def version_info() -> str:
+def git_version() -> str:
     """Method returning the version of PyFluent being used.
 
     Returns
