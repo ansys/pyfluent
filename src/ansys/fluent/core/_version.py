59314254adaa4d4c94af44baefc48c9e284f42bf
Mainak Kundu <mainak.kundu@ansys.com>
2024-02-12 15:15:07 -0500

    Bump version 0.20.dev3

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 20, "dev1"
+version_info = 0, 20, "dev2"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
