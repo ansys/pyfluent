77c9de582530ee72ed2e9233081b0dd0fe6cf732
Harshal Pohekar <106588300+hpohekar@users.noreply.github.com>
2024-02-23 11:01:12 +0530

    refactor: read/write fail after switching to solver mode (#2481)
    * refactor: read/write fail after switching to solver mode
    
    * update flobject.py
    
    * update flobject.py 1
    
    * update flobject.py 2

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -152,9 +152,10 @@ class Base:
 
         Supports file upload and download.
         """
-        if self._remote_file_handler is None:
+        if self._remote_file_handler:
+            return self._remote_file_handler
+        elif self._parent:
             return self._parent.remote_file_handler
-        return self._remote_file_handler
 
     _name = None
     fluent_name = None
@@ -649,18 +650,14 @@ class FileName(Base):
 
 class _InputFile(FileName):
     def _do_before_execute(self, value):
-        try:
+        if self.remote_file_handler:
             self.remote_file_handler.upload(file_name=value)
-        except AttributeError:
-            pass
 
 
 class _OutputFile(FileName):
     def _do_after_execute(self, value):
-        try:
+        if self.remote_file_handler:
             self.remote_file_handler.download(file_name=value)
-        except AttributeError:
-            pass
 
 
 class _InOutFile(_InputFile, _OutputFile):
