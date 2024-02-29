d061893965c67b291a6d672707f70fdc0b8253a1
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-02-29 19:52:32 +0530

    update dependencies

--- src/ansys/fluent/core/__init__.py
+++ src/ansys/fluent/core/__init__.py
@@ -24,7 +24,6 @@ from ansys.fluent.core.launcher.launcher_utils import FluentMode  # noqa: F401
 from ansys.fluent.core.services.batch_ops import BatchOps  # noqa: F401
 from ansys.fluent.core.session import BaseSession as Fluent  # noqa: F401
 from ansys.fluent.core.utils import fldoc
-from ansys.fluent.core.utils.fluent_version import FluentVersion  # noqa: F401
 from ansys.fluent.core.utils.search import search  # noqa: F401
 from ansys.fluent.core.utils.setup_for_fluent import setup_for_fluent  # noqa: F401
 
