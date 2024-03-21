235796a47f5070c061fd188fceae00efb5ebf7aa
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2024-03-22 00:43:13 +0530

    feat: Include 2D Meshing workflow. (#2596)
    * feat: Include 2D Meshing workflow.
    
    * fix get_attr.
    
    * Add topology based meshing workflow.
    
    * Update src/ansys/fluent/core/session_pure_meshing.py
    
    Co-authored-by: Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>
    
    * Update src/ansys/fluent/core/session_pure_meshing.py
    
    Co-authored-by: Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>
    
    * Update src/ansys/fluent/core/meshing/meshing_workflow.py
    
    Co-authored-by: Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sean Pearson <93727996+seanpearsonuk@users.noreply.github.com>

--- src/ansys/fluent/core/session_pure_meshing.py
+++ src/ansys/fluent/core/session_pure_meshing.py
@@ -107,6 +107,24 @@ class PureMeshing(BaseSession):
         self._base_meshing.fault_tolerant_workflow.reinitialize()
         return self._base_meshing.fault_tolerant_workflow
 
+    def two_dimensional_meshing(self):
+        """Get a new 2D meshing workflow."""
+        self._base_meshing.two_dimensional_meshing_workflow.reinitialize()
+        return self._base_meshing.two_dimensional_meshing_workflow
+
+    def topology_based(self):
+        """Get a new topology-based meshing workflow.
+
+        Raises
+        ------
+        RuntimeError
+            If beta features are not enabled in Fluent.
+        """
+        if not self.scheme_eval.scheme_eval("(is-beta-feature-available?)"):
+            raise RuntimeError("Topology-based Meshing is a beta feature in Fluent.")
+        self._base_meshing.topology_based_meshing_workflow.reinitialize()
+        return self._base_meshing.topology_based_meshing_workflow
+
     @property
     def PartManagement(self):
         """Datamodel root of PartManagement."""
