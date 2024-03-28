0806a69e295cc20be27b26a45d8d4c406f158679
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-03-28 11:14:42 +0530

    Fix meshing workflow test.

--- tests/test_new_meshing_workflow.py
+++ tests/test_new_meshing_workflow.py
@@ -5,7 +5,6 @@ import pytest
 
 from ansys.fluent.core import examples
 from ansys.fluent.core.meshing.watertight import watertight_workflow
-from ansys.fluent.core.utils.fluent_version import FluentVersion
 from tests.test_datamodel_service import disable_datamodel_cache  # noqa: F401
 
 
@@ -522,7 +521,6 @@ def test_snake_case_attrs_in_new_meshing_workflow(new_mesh_session):
     watertight.import_geometry()
 
 
-@pytest.mark.skip
 @pytest.mark.codegen_required
 @pytest.mark.fluent_version(">=24.1")
 def test_workflow_and_data_model_methods_new_meshing_workflow(new_mesh_session):
@@ -551,11 +549,8 @@ def test_workflow_and_data_model_methods_new_meshing_workflow(new_mesh_session):
         "import_body_of_influence_geometry",
         "set_up_periodic_boundaries",
         "create_local_refinement_regions",
-        "load_cad_geometry",
         "run_custom_journal",
     ]
-    if meshing.get_fluent_version() < FluentVersion.v242:
-        _next_possible_tasks.remove("load_cad_geometry")
     assert (
         watertight.task("import_geom_wtm").get_next_possible_tasks()
         == _next_possible_tasks
