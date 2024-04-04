323920f8eac1fc45935fdbda5831eac48c4f2c95
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-04-04 17:24:47 +0530

    More....

--- src/ansys/fluent/core/fluent_connection.py
+++ src/ansys/fluent/core/fluent_connection.py
@@ -238,13 +238,7 @@ class _ConnectionProperties:
         branch = self.scheme_eval.scheme_eval("(inquire-src-vcs-branch)")
         return f"Build Time: {build_time}  Build Id: {build_id}  Revision: {rev}  Branch: {branch}"
 
-    def get_cortex_connection_properties(
-        self,
-        ip: str,
-        port: int,
-        password: Optional[str] = None,
-        inside_container: Optional[bool] = None,
-    ):
+    def get_cortex_connection_properties(self):
         """Get connection properties of Fluent."""
         from grpc._channel import _InactiveRpcError
 
@@ -266,16 +260,18 @@ class _ConnectionProperties:
             cortex_pid = None
             cortex_pwd = None
 
-        return cortex_host, FluentConnectionProperties(
-            ip,
-            port,
-            password,
-            cortex_pwd,
-            cortex_pid,
-            cortex_host,
-            fluent_host_pid,
-            inside_container,
-        )
+        return fluent_host_pid, cortex_host, cortex_pid, cortex_pwd
+        #
+        # return cortex_host, FluentConnectionProperties(
+        #     ip,
+        #     port,
+        #     password,
+        #     cortex_pwd,
+        #     cortex_pid,
+        #     cortex_host,
+        #     fluent_host_pid,
+        #     inside_container,
+        # )
 
 
 class FluentConnection:
@@ -381,27 +377,11 @@ class FluentConnection:
         self.scheme_eval = service_creator("scheme_eval").create(
             self._scheme_eval_service
         )
-
+        con_props = _ConnectionProperties(self.scheme_eval)
+        fluent_host_pid, cortex_host, cortex_pid, cortex_pwd = (
+            con_props.get_cortex_connection_properties()
+        )
         self._cleanup_on_exit = cleanup_on_exit
-        from grpc._channel import _InactiveRpcError
-
-        try:
-            logger.info(self.fluent_build_info)
-            logger.debug("Obtaining Cortex connection properties...")
-            fluent_host_pid = self.scheme_eval.scheme_eval("(cx-client-id)")
-            cortex_host = self.scheme_eval.scheme_eval("(cx-cortex-host)")
-            cortex_pid = self.scheme_eval.scheme_eval("(cx-cortex-id)")
-            cortex_pwd = self.scheme_eval.scheme_eval("(cortex-pwd)")
-            logger.debug("Cortex connection properties successfully obtained.")
-        except _InactiveRpcError:
-            logger.warning(
-                "Fluent Cortex properties unobtainable, force exit and other"
-                "methods are not going to work properly, proceeding..."
-            )
-            cortex_host = None
-            cortex_pid = None
-            cortex_pwd = None
-            fluent_host_pid = None
 
         if (
             (inside_container is None or inside_container is True)
@@ -454,15 +434,6 @@ class FluentConnection:
             self._exit_event,
         )
         FluentConnection._monitor_thread.cbs.append(self._finalizer)
-
-    @property
-    def fluent_build_info(self) -> str:
-        """Get Fluent build info."""
-        build_time = self.scheme_eval.scheme_eval("(inquire-build-time)")
-        build_id = self.scheme_eval.scheme_eval("(inquire-build-id)")
-        rev = self.scheme_eval.scheme_eval("(inquire-src-vcs-id)")
-        branch = self.scheme_eval.scheme_eval("(inquire-src-vcs-branch)")
-        return f"Build Time: {build_time}  Build Id: {build_id}  Revision: {rev}  Branch: {branch}"
 
     def _close_slurm(self):
         subprocess.run(["scancel", f"{self._slurm_job_id}"])
