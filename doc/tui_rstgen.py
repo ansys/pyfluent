6cefc6e295f41a931089e647593569a0df3a234e
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-04-05 16:31:13 +0530

    tuigen update 4

--- doc/tui_rstgen.py
+++ doc/tui_rstgen.py
@@ -2,7 +2,7 @@
 
 from ansys.fluent.core import FluentMode, launch_fluent
 from ansys.fluent.core.utils.fluent_version import get_version_for_file_name
-from codegen.datamodelgen import generate
+from codegen.tuigen import generate
 
 if __name__ == "__main__":
     sessions = {FluentMode.SOLVER: launch_fluent()}
