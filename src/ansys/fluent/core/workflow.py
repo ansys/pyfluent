173e40f83e26a1d80d9e93245791faa388325514
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2024-04-12 23:23:02 +0530

    Update src/ansys/fluent/core/workflow.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/fluent/core/workflow.py
+++ src/ansys/fluent/core/workflow.py
@@ -229,8 +229,8 @@ class BaseTask:
     def ordered_children(self, recompute=True) -> list:
         """Get the ordered task list held by this task.
 
-        Sort tasks in terms of the workflow order and only includes this task's top-level tasks,
-        while other tasks can be obtained by calling ordered_children() on a parent task.
+        This method sort tasks in terms of the workflow order and only includes this task's top-level tasks.
+        You can obtain other tasks by calling the ``ordered_children()`` method on a parent task.
 
         Given the workflow::
 
