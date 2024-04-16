0ef00e781e24482cad8bac2ff7bd5d61bb0929c6
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-04-16 16:37:39 +0530

    tui dir 3

--- doc/tui_rstgen.py
+++ doc/tui_rstgen.py
@@ -3,7 +3,6 @@
 import fnmatch
 import os
 from pathlib import Path
-import shutil
 from typing import Optional
 
 _THIS_DIRNAME = os.path.dirname(__file__)
@@ -46,28 +45,6 @@ def _get_tui_file_name(mode: str):
     for file in os.listdir(_get_tui_filepath(mode)):
         if fnmatch.fnmatch(file, "tui_*.py"):
             return file
-
-
-def generate():
-    """Generate TUI result (RST) files."""
-    for mode in ["meshing", "solver"]:
-        if Path(_get_tui_docdir(mode)).exists():
-            shutil.rmtree(Path(_get_tui_docdir(mode)))
-        Path(_get_tui_docdir(mode)).mkdir(exist_ok=True)
-        index_file = Path(_get_tui_docdir(mode)) / "index.rst"
-        with open(index_file, "w", encoding="utf8") as f:
-            f.write(f".. _ref_{mode}_tui:\n\n")
-            heading = mode + ".tui"
-            f.write(f"{heading}\n")
-            f.write(f"{'=' * len(heading)}\n\n")
-            f.write(
-                f".. autoclass:: ansys.fluent.core.{mode}.{_get_tui_file_name(mode)}.main_menu\n"
-            )
-            f.write("   :members:\n")
-            f.write("   :show-inheritance:\n")
-            f.write("   :undoc-members:\n")
-            f.write("   :autosummary:\n")
-            f.write("   :autosummary-members:\n")
 
 
 if __name__ == "__main__":
