06fa1c5395eeca0f02725abe55f3dd7a301ec159
Harshal Pohekar <106588300+hpohekar@users.noreply.github.com>
2024-04-18 09:47:01 +0530

    Update src/ansys/fluent/core/launcher/standalone_launcher.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/fluent/core/launcher/standalone_launcher.py
+++ src/ansys/fluent/core/launcher/standalone_launcher.py
@@ -129,10 +129,11 @@ class StandaloneLauncher:
             Fluent transcript subsequently via the method calls, ``transcript.start()``
             and ``transcript.stop()`` on the session object.
         show_gui : bool, optional
-            Whether to display the Fluent GUI. The default is ``None``, which does not
-            cause the GUI to be shown. If a value of ``False`` is
-            not explicitly provided, the GUI will also be shown if
-            the environment variable ``PYFLUENT_SHOW_SERVER_GUI`` is set to 1.
+            Whether to display the Fluent GUI. The default is ``None``,
+            in which case the GUI is not shown. If ``False`` is
+            not explicitly provided, the GUI will be shown if
+            the ``PYFLUENT_SHOW_SERVER_GUI`` environment
+            variable is set to 1.
         case_file_name : str, optional
             If provided, the case file at ``case_file_name`` is read into the Fluent session.
         case_data_file_name : str, optional
