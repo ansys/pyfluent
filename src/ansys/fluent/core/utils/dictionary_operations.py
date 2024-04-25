65ef7c1fe10bf0d00c88a1aec4af7fe137a6eabc
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-04-25 11:01:46 +0530

    Update logic.

--- src/ansys/fluent/core/utils/dictionary_operations.py
+++ src/ansys/fluent/core/utils/dictionary_operations.py
@@ -3,4 +3,4 @@
 
 def get_first_dict_key_for_value(input_dict, value):
     """Get the first dictionary key that matches a value."""
-    return next(key for key, val in input_dict.items() if val == value)
+    return next((key for key, val in input_dict.items() if val == value), None)
