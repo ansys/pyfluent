e614ea242cd7a8e5c3f20fd295a3c3184dd11f35
Harshal Pohekar <106588300+hpohekar@users.noreply.github.com>
2024-04-30 21:02:34 +0530

    fix: nightly dev doc import fix (#2747)
    * fix: nightly dev doc import fix
    
    * fix: nightly dev doc import fix 1
    
    * fix: nightly dev doc import fix 2

--- doc/datamodel_rstgen.py
+++ doc/datamodel_rstgen.py
@@ -8,6 +8,7 @@ from rstgen import _get_file_or_folder, _write_datamodel_index_doc, generate
 def generate_meshing_datamodels():
     """Generate meshing datamodel RST files."""
     meshing_datamodel_roots = []
+    available_datamodels = []
     meshing_datamodels = [
         "meshing",
         "MeshingUtilities",
@@ -17,11 +18,16 @@ def generate_meshing_datamodels():
         "workflow",
     ]
     for meshing_datamodel in meshing_datamodels:
-        datamodel = importlib.import_module(
-            f"ansys.fluent.core.{_get_file_or_folder(mode='meshing', is_datamodel=True)}.{meshing_datamodel}"
-        )
-        meshing_datamodel_roots.append(datamodel.Root)
-        _write_datamodel_index_doc(meshing_datamodels, "meshing")
+        try:
+            datamodel = importlib.import_module(
+                f"ansys.fluent.core.{_get_file_or_folder(mode='meshing', is_datamodel=True)}.{meshing_datamodel}"
+            )
+            if datamodel:
+                meshing_datamodel_roots.append(datamodel.Root)
+                available_datamodels.append(meshing_datamodel)
+        except ModuleNotFoundError:
+            pass
+    _write_datamodel_index_doc(available_datamodels, "meshing")
     for root in meshing_datamodel_roots:
         generate(main_menu=root, mode="meshing", is_datamodel=True)
 
@@ -29,13 +35,19 @@ def generate_meshing_datamodels():
 def generate_solver_datamodels():
     """Generate solver datamodel RST files."""
     solver_datamodel_roots = []
+    available_datamodels = []
     solver_datamodels = ["flicing", "preferences", "solverworkflow", "workflow"]
     for solver_datamodel in solver_datamodels:
-        datamodel = importlib.import_module(
-            f"ansys.fluent.core.{_get_file_or_folder(mode='solver', is_datamodel=True)}.{solver_datamodel}"
-        )
-        solver_datamodel_roots.append(datamodel.Root)
-        _write_datamodel_index_doc(solver_datamodels, "solver")
+        try:
+            datamodel = importlib.import_module(
+                f"ansys.fluent.core.{_get_file_or_folder(mode='solver', is_datamodel=True)}.{solver_datamodel}"
+            )
+            if datamodel:
+                solver_datamodel_roots.append(datamodel.Root)
+                available_datamodels.append(solver_datamodel)
+        except ModuleNotFoundError:
+            pass
+    _write_datamodel_index_doc(available_datamodels, "solver")
     for root in solver_datamodel_roots:
         generate(main_menu=root, mode="solver", is_datamodel=True)
 
