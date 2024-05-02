80c1be8d9c175f2f38f2bbe2dd030fbd3e669e63
Oleg Chernukhin <oleg.chernukhin@ansys.com>
2024-05-01 21:38:51 -0400

    fix a typo in variable name

--- src/ansys/fluent/core/systemcoupling.py
+++ src/ansys/fluent/core/systemcoupling.py
@@ -116,7 +116,7 @@ class SystemCoupling:
 
             ext_vars = {
                 "force",
-                "lortentz-force",
+                "lorentz-force",
                 "heatrate",
                 "heatflow",
                 "mass-flow-rate",
