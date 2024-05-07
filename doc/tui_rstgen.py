2953a77cfebcd4d6faeafbf39f2ee1633a0b70e7
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-05-07 12:47:26 +0530

    nightly dev doc generated fix

--- doc/tui_rstgen.py
+++ doc/tui_rstgen.py
@@ -7,14 +7,14 @@ from rstgen import _get_file_or_folder, generate
 if __name__ == "__main__":
     try:
         meshing_tui = importlib.import_module(
-            f"ansys.fluent.core.meshing.{_get_file_or_folder(mode='meshing', is_datamodel=False)}"
+            f"ansys.fluent.core.generated.meshing.{_get_file_or_folder(mode='meshing', is_datamodel=False)}"
         )
         generate(main_menu=meshing_tui.main_menu, mode="meshing", is_datamodel=False)
     except ModuleNotFoundError:
         pass
     try:
         solver_tui = importlib.import_module(
-            f"ansys.fluent.core.solver.{_get_file_or_folder(mode='solver', is_datamodel=False)}"
+            f"ansys.fluent.core.generated.solver.{_get_file_or_folder(mode='solver', is_datamodel=False)}"
         )
         generate(main_menu=solver_tui.main_menu, mode="solver", is_datamodel=False)
     except ModuleNotFoundError:
