1f0ea41df9dc84cb8d2e341f669fb21e0bd5fd01
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-05-07 09:44:35 +0530

    fix: Nightly dev test fix

--- tests/test_file_transfer_service.py
+++ tests/test_file_transfer_service.py
@@ -40,14 +40,16 @@ def file_downloaded_to_the_client(file_name: str) -> bool:
 def test_remote_grpc_fts_container(monkeypatch, new_solver_session, new_mesh_session):
     solver = new_solver_session
     solver.file.read_case(file_name=import_case_file_name)
-    solver.file.write_case(file_name="downloaded_solver_mixing_elbow.cas.h5")
     if solver._file_transfer_service:
+        solver.file.write_case(file_name="downloaded_solver_mixing_elbow.cas.h5")
         assert solver.file_exists_on_remote("downloaded_solver_mixing_elbow.cas.h5")
-    assert file_downloaded_to_the_client("downloaded_solver_mixing_elbow.cas.h5")
+        assert file_downloaded_to_the_client("downloaded_solver_mixing_elbow.cas.h5")
 
     meshing = new_mesh_session
     meshing.meshing.File.ReadMesh(FileName=import_mesh_file_name)
-    meshing.meshing.File.WriteMesh(FileName="downloaded_meshing_mixing_elbow.msh.h5")
     if meshing._file_transfer_service:
+        meshing.meshing.File.WriteMesh(
+            FileName="downloaded_meshing_mixing_elbow.msh.h5"
+        )
         assert meshing.file_exists_on_remote("downloaded_meshing_mixing_elbow.msh.h5")
-    assert file_downloaded_to_the_client("downloaded_meshing_mixing_elbow.msh.h5")
+        assert file_downloaded_to_the_client("downloaded_meshing_mixing_elbow.msh.h5")
