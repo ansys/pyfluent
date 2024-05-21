4c44c052a1e31b2f89b854813ac9c339c2e787cd
Raphael Luciano <raphael.luciano@ansys.com>
2024-05-21 16:28:43 -0400

    final touches

--- src/ansys/fluent/core/utils/deprecate_args.py
+++ src/ansys/fluent/core/utils/deprecate_args.py
@@ -30,18 +30,24 @@ def deprecate_argument(
             """Warns about the deprecated argument and replaces it with the new
             argument."""
             if old_arg in kwargs:
-                warnings.warn(
-                    f"'{old_arg}' is deprecated. Use '{new_arg}' instead.",
-                    deprecation_class,
-                    stacklevel=2,
-                )
                 old_value = kwargs[old_arg]
-                new_value = converter(kwargs[old_arg])
-                if kwargs.get(new_arg) is None:
+                new_value = kwargs.get(new_arg)
+                if new_value is None:
+                    warnings.warn(
+                        f"'{old_arg}' is deprecated. Use '{new_arg}' instead.",
+                        deprecation_class,
+                        stacklevel=2,
+                    )
+                    new_value = converter(kwargs[old_arg])
                     kwargs[new_arg] = new_value
                     logger.warning(
                         f"Using '{func.__name__}({new_arg} = {_str_repr(new_value)})'"
                         f" instead of '{old_arg} = {_str_repr(old_value)}'."
+                    )
+                else:
+                    logger.warning(
+                        f"Ignoring '{old_arg} = {_str_repr(old_value)}' specification for {func.__name__}(),"
+                        f" only '{new_arg} = {_str_repr(new_value)}' applies."
                     )
                 kwargs.pop(old_arg)
             return func(*args, **kwargs)
