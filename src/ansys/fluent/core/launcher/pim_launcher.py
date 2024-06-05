75275ecadeb669edee82bfbcd3b6740a52d2a2b3
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2024-06-05 14:46:37 -0400

    Apply suggestions from code review
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- src/ansys/fluent/core/launcher/pim_launcher.py
+++ src/ansys/fluent/core/launcher/pim_launcher.py
@@ -75,7 +75,7 @@ class PIMLauncher:
             ``FluentLinuxGraphicsDriver`` enum in Linux.
         product_version : FluentVersion or str or float or int, optional
             Version of Ansys Fluent to launch. To use Fluent version 2024 R2, pass
-            any of ``FluentVersion.v242``, ``"24.2.0"``, ``"24.2"``, ``24.2``or ``242``.
+           ``FluentVersion.v242``, ``"24.2.0"``, ``"24.2"``, ``24.2``, or ``242``.
             The default is ``None``, in which case the newest installed version is used.
         version : str, optional
             Geometric dimensionality of the Fluent simulation. The default is ``None``,
