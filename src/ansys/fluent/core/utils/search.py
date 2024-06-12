60967d7e836a70e31ccbcd96ff8b2417acafad72
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-06-12 14:55:37 +0530

    write_api_tree_data bool

--- src/ansys/fluent/core/utils/search.py
+++ src/ansys/fluent/core/utils/search.py
@@ -149,6 +149,7 @@ def _search(
     match_case: bool = False,
     version: Optional[str] = None,
     search_root: Optional[Any] = None,
+    write_api_tree_data: Optional[bool] = False,
 ):
     """Search for a word through the Fluent's object hierarchy.
 
@@ -167,6 +168,8 @@ def _search(
         The root object within which the search is performed.
         It can be a session object or any API object within a session.
         The default is ``None``. If ``None``, it searches everything.
+    write_api_tree_data: bool, optional
+        Whether to write the API tree data.
 
     Examples
     --------
@@ -285,9 +288,10 @@ def _search(
         with open(api_tree_file, "w") as json_file:
             json.dump(api_tree_data, json_file)
 
-    _write_api_tree_file(
-        api_tree_data=api_tree_data, api_object_names=list(api_object_names)
-    )
+    if write_api_tree_data:
+        _write_api_tree_file(
+            api_tree_data=api_tree_data, api_object_names=list(api_object_names)
+        )
     return results
 
 
