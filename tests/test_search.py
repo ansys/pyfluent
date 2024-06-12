2eb65774f5481b95516fedfe69b5a16aa090749a
Harshal Pohekar <harshal.pohekar@ansys.com>
2024-06-12 17:15:34 +0530

    skip 23.2 test

--- tests/test_search.py
+++ tests/test_search.py
@@ -495,6 +495,7 @@ def test_search_from_root(new_watertight_workflow_session):
     assert "<search_root>.IdleTimeout (Parameter)" in results
 
 
+@pytest.mark.skip("Results are varying each time.")
 @pytest.mark.codegen_required
 @pytest.mark.fluent_version("==23.2")
 def test_search_settings_from_root(capsys, load_static_mixer_settings_only):
