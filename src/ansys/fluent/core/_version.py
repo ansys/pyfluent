959dde4f2becd5235b8bf0975f1145e06dc9aa51
Harshal Pohekar <106588300+hpohekar@users.noreply.github.com>
2024-06-20 10:04:24 +0530

    build: Bump version to 0.21.0 (#2951)

--- src/ansys/fluent/core/_version.py
+++ src/ansys/fluent/core/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 21, "dev1"
+version_info = 0, 21, 0
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
