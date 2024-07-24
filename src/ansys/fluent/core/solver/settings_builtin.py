a5a2031a700c4fdb94d916d4ad720ea03a3fdcb2
Mainak Kundu <mainak.kundu@ansys.com>
2024-07-24 07:09:18 -0400

    feat: compact

--- src/ansys/fluent/core/solver/settings_builtin.py
+++ src/ansys/fluent/core/solver/settings_builtin.py
@@ -4,74 +4,36 @@ from ansys.fluent.core.session_solver import Solver
 
 
 class _SingletonSettings:
-    def __new__(cls, solver: Solver, path: str):
+    def __new__(cls, solver: Solver):
         obj = solver.settings
-        for comp in path.split("."):
+        for comp in cls.path.split("."):
             obj = getattr(obj, comp)
         return obj
 
 
 class _NamedObjectSettings:
-    def __new__(cls, solver: Solver, path: str, name: str):
-        container = _SingletonSettings(solver, path)
-        return container[name]
+    def __new__(cls, solver: Solver, name: str):
+        obj = solver.settings
+        for comp in cls.path.split("."):
+            obj = getattr(obj, comp)
+        return obj[name]
 
 
-class viscous:
-    """Viscous settings."""
-
-    def __new__(self, solver: Solver):
-        """Create a new viscous settings object.
-
-        Parameters
-        ----------
-        solver : Solver
-            Fluent solver object.
-        """
-        return _SingletonSettings(solver, "setup.models.viscous")
+def _create_singleton_settings(name: str, path: str):
+    return type(name, (_SingletonSettings,), {"path": path})
 
 
-class boundary_conditions:
-    """Boundary conditions settings."""
-
-    def __new__(self, solver: Solver):
-        """Create a new boundary conditions settings object.
-
-        Parameters
-        ----------
-        solver : Solver
-            Fluent solver object.
-        """
-        return _SingletonSettings(solver, "setup.boundary_conditions")
+def _create_named_object_settings(name: str, path: str):
+    return type(name, (_NamedObjectSettings,), {"path": path})
 
 
-class boundary_condition:
-    """Boundary condition settings."""
-
-    def __new__(self, solver: Solver, name: str):
-        """Create a new boundary condition settings object.
-
-        Parameters
-        ----------
-        solver : Solver
-            Fluent solver object.
-        name : str
-            Boundary condition name.
-        """
-        return _NamedObjectSettings(solver, "setup.boundary_conditions", name)
-
-
-class velocity_inlet:
-    """Velocity inlet settings."""
-
-    def __new__(self, solver: Solver, name: str):
-        """Create a new velocity inlet settings object.
-
-        Parameters
-        ----------
-        solver : Solver
-            Fluent solver object.
-        name : str
-            Velocity inlet name.
-        """
-        return _NamedObjectSettings(solver, "setup.boundary_conditions", name)
+viscous = _create_singleton_settings("viscous", "setup.models.viscous")
+boundary_conditions = _create_singleton_settings(
+    "boundary_conditions", "setup.boundary_conditions"
+)
+boundary_condition = _create_named_object_settings(
+    "boundary_condition", "setup.boundary_conditions"
+)
+velocity_inlet = _create_named_object_settings(
+    "velocity_inlet", "setup.boundary_conditions"
+)
