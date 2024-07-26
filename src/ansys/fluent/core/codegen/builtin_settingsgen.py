a131dfd2ff6ac9883a1dc8545f3f25e128514dcd
Mainak Kundu <mainak.kundu@ansys.com>
2024-07-26 10:21:28 -0400

    feat: generated

--- src/ansys/fluent/core/codegen/builtin_settingsgen.py
+++ src/ansys/fluent/core/codegen/builtin_settingsgen.py
@@ -13,30 +13,38 @@ _PY_FILE = CODEGEN_OUTDIR / "settings_builtin.py"
 _PYI_FILE = CODEGEN_OUTDIR / "settings_builtin.pyi"
 _VERSIONS = ("222", "231", "232", "241", "242", "251")
 
-with open(_PY_FILE, "w") as f:
-    f.write('"""Solver settings."""\n\n')
-    f.write(
-        "from ansys.fluent.core.solver.settings_builtin_bases import _SingletonSetting, _NamedObjectSetting\n\n\n"
-    )
-    f.write("__all__ = [\n")
-    for name, _, _ in _DATA:
-        f.write(f'    "{name}",\n')
-    f.write("]\n\n")
-    for name, kind, path in _DATA:
-        f.write(f"class {name}(_{kind}Setting):\n")
-        f.write(f'    """{name} setting."""\n\n')
-        f.write(f'    path = "{path}"\n\n')
 
-with open(_PYI_FILE, "w") as f:
-    for version in _VERSIONS:
+def generate():
+    """Generate builtin setting classes."""
+    CODEGEN_OUTDIR.mkdir(exist_ok=True)
+    with open(_PY_FILE, "w") as f:
+        f.write('"""Solver settings."""\n\n')
         f.write(
-            f"from ansys.fluent.core.generated.solver.settings_{version} import root as settings_root_{version}\n"
+            "from ansys.fluent.core.solver.settings_builtin_bases import _SingletonSetting, _NamedObjectSetting\n\n\n"
         )
-    f.write("\n\n")
-    for name, kind, path in _DATA:
-        f.write(f"class {name}(\n")
-        if kind == "NamedObject":
-            path = f"{path}.child_object_type"
+        f.write("__all__ = [\n")
+        for name, _, _ in _DATA:
+            f.write(f'    "{name}",\n')
+        f.write("]\n\n")
+        for name, kind, path in _DATA:
+            f.write(f"class {name}(_{kind}Setting):\n")
+            f.write(f'    """{name} setting."""\n\n')
+            f.write(f'    path = "{path}"\n\n')
+
+    with open(_PYI_FILE, "w") as f:
         for version in _VERSIONS:
-            f.write(f"    type(settings_root_{version}.{path}),\n")
-        f.write("): ...\n\n")
+            f.write(
+                f"from ansys.fluent.core.generated.solver.settings_{version} import root as settings_root_{version}\n"
+            )
+        f.write("\n\n")
+        for name, kind, path in _DATA:
+            f.write(f"class {name}(\n")
+            if kind == "NamedObject":
+                path = f"{path}.child_object_type"
+            for version in _VERSIONS:
+                f.write(f"    type(settings_root_{version}.{path}),\n")
+            f.write("): ...\n\n")
+
+
+if __name__ == "__main__":
+    generate()
