04e1f2d0b50493860e19edb594e3a3157ed6fd0a
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-09-11 15:47:14 +0530

    Added test.

--- tests/test_aero_session.py
+++ tests/test_aero_session.py
@@ -7,3 +7,68 @@ import ansys.fluent.core as pyfluent
 def test_icing_session():
     aero_session = pyfluent.launch_fluent(mode=pyfluent.FluentMode.SOLVER_AERO)
     assert "aero" in dir(aero_session)
+
+
+@pytest.mark.fluent_version(">=24.2")
+def test_sample_setup():
+    solver = pyfluent.launch_fluent(mode="solver_aero")
+    solver.new_project(project_name="sample_aero_proj")
+    solver.new_simulation(
+        case_file_name=r"C:\ANSYSDev\PyFluent_Dev_01\pf_aero\wing.msh.h5"
+    )
+
+    # Geometric Properties
+    solver.aero.AeroWorkflow.Setup.GeometricProperties.MomentCenterX = 0.2
+    solver.aero.AeroWorkflow.Setup.GeometricProperties.MomentCenterY = 0
+    solver.aero.AeroWorkflow.Setup.GeometricProperties.MomentCenterZ = 0
+    solver.aero.AeroWorkflow.Setup.GeometricProperties.RefLength = 0.5334
+    solver.aero.AeroWorkflow.Setup.GeometricProperties.RefArea = 0.05334
+
+    # Simulation Conditions
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowSpeed.Parameter = (
+        "Mach"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowSpeed.Distribution = (
+        "Constant"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowSpeed.Mach = (
+        0.3
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowDirection.Parameter = (
+        "AoA"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowDirection.DistributionAoa = (
+        "Uniform"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowDirection.AoaMin = (
+        3
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowDirection.AoaMax = (
+        5
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.FlowDirection.AoaNum = (
+        2
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.PresTempInput.Parameter = (
+        "Static"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.PresTempInput.DistributionPressure = (
+        "Constant"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.PresTempInput.DistributionTemperature = (
+        "Constant"
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.PresTempInput.Pressure = (
+        101325
+    )
+    solver.aero.AeroWorkflow.Setup.SimulationConditions.FlightConditions.PresTempInput.Temperature = (
+        300
+    )
+
+    # Solve
+    solver.aero.AeroWorkflow.Solution.Solve.Iterations = 10
+    solver.aero.AeroWorkflow.Solution.Solve.ShowAdvanced = True
+    solver.aero.AeroWorkflow.Setup.AirflowPhysics.Solver.Type = "Pressure based"
+
+    # Calculate, Disconnect, Exit
+    solver.aero.AeroWorkflow.Solution.Solve.AeroCalculate()
