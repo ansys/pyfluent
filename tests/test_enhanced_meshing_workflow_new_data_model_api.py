da80e431ae37a85d32ef2c94046b87d8df92b5d6
ANSYS\spearson <sean.pearson@ansys.com>
2024-09-11 12:00:10 +0100

    testUpdates

--- tests/test_enhanced_meshing_workflow_new_data_model_api.py
+++ tests/test_enhanced_meshing_workflow_new_data_model_api.py
@@ -1,9 +1,83 @@
+import time
+
 import pytest
 
-from tests.conftest import new_meshing_session_new_api_enabled  # noqa: F401
+from ansys.fluent.core import examples
+
+"""
+                # enum = IF(LEN(@./allowedValues) == 4, ("none", "right", "left", "in"), ("none", "right", "left", "in", "out"))
+Basic interactive test
+======================
+
+This checks the backend's mapped data model values directly
+
+import ansys.fluent.core as pf
+import time
+from ansys.fluent.core import examples
+meshing = pf.launch_fluent(mode="meshing", py=True, show_gui=True, start_transcript=False, enable_data_model_api_upgrades=True)
+wt = meshing.watertight()
+import_file_name = examples.download_file(
+    "mixing_elbow.pmdb", "pyfluent/mixing_elbow"
+)
+
+wt.import_geometry.file_name.set_state(import_file_name)
+
+assert wt.describe_geometry.setup_type.allowed_values() == ['fluid', 'fluid_solid_voids', 'solid']
+wt.describe_geometry.multizone.set_state(True)
+wt.describe_geometry()
+add_multizone_controls = None
+
+while add_multizone_controls is None:
+    try:
+        add_multizone_controls = wt.add_multizone_controls
+        print(f"got add_multizone_controls {add_multizone_controls}")
+    except AttributeError:
+        print("Caught AttributeError")
+        time.sleep(1)
+
+bias_method = add_multizone_controls.bias_method
+
+# assert bias_method.allowed_values() == ["none", "right", "left", "in"]
+bias_method.set_state("right")
+assert bias_method.get_state() == "right"
 
 
-@pytest.mark.skip("Pending server availability.")
+In UI:
+meshing.workflow.TaskObject["Describe Geometry"].Arguments.get_state()
+{'Multizone': 'Yes'}
+
+Version without latest API update:
+=================================
+
+import ansys.fluent.core as pf
+import time
+from ansys.fluent.core import examples
+meshing = pf.launch_fluent(mode="meshing", py=True, show_gui=True, start_transcript=False, enable_data_model_api_upgrades=True)
+wt = meshing.watertight()
+import_file_name = examples.download_file(
+    "mixing_elbow.pmdb", "pyfluent/mixing_elbow"
+)
+
+wt.import_geometry.file_name.set_state(import_file_name)
+
+wt.describe_geometry.multizone.set_state("Yes")
+wt.describe_geometry()
+add_multizone_controls = None
+
+while add_multizone_controls is None:
+    try:
+        add_multizone_controls = wt.add_multizone_controls
+        print(f"got add_multizone_controls {add_multizone_controls}")
+    except AttributeError:
+        print("Caught AttributeError")
+        time.sleep(1)
+
+
+
+"""
+
+
+# @pytest.mark.skip("Pending server availability.")
 @pytest.mark.codegen_required
 @pytest.mark.fluent_version(">=25.1")
 def test_enhanced_meshing_workflow_new_data_model_api(
@@ -73,11 +147,30 @@ def test_enhanced_meshing_workflow_new_data_model_api(
     import_cad.jt_lod.set_state(2)
     assert on_affected.changed is True
 
-    # for parameters, on_changed can always be called
-    # def on_param_affected(obj):
-    #    on_param_affected.changed = True
-    # on_param_affected.changed = False
+    wt = meshing.watertight()
 
-    # import_cad.jt_lod.add_on_affected(on_param_affected)
-    # import_cad.jt_lod.set_state(3)
-    # assert on_param_affected.changed is True
+    import_file_name = examples.download_file(
+        "mixing_elbow.pmdb", "pyfluent/mixing_elbow"
+    )
+
+    wt.import_geometry.file_name.set_state(import_file_name)
+
+    assert wt.describe_geometry.setup_type.allowed_values() == [
+        "fluid",
+        "fluid_solid_voids",
+        "solid",
+    ]
+
+    wt.describe_geometry.multizone.set_state(True)
+
+    wt.describe_geometry()
+
+    add_multizone_controls = None
+
+    while add_multizone_controls is None:
+        try:
+            add_multizone_controls = wt.add_multizone_controls
+            print(f"got add_multizone_controls {add_multizone_controls}")
+        except AttributeError:
+            print("Caught AttributeError")
+            time.sleep(1)
