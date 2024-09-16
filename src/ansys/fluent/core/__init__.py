a3fbbb9f6ee612a5f0f7d44edf0e80eabab535e2
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2024-09-16 13:44:28 -0400

    feat: Builtin settings objects from Setup section of Fluent's outline tree (#3127)
    feat: Initial implementation of settings objects

--- src/ansys/fluent/core/__init__.py
+++ src/ansys/fluent/core/__init__.py
@@ -47,6 +47,11 @@ from ansys.fluent.core.warnings import (  # noqa: F401
     warning,
 )
 
+try:
+    from ansys.fluent.core.generated.solver.settings_builtin import *  # noqa: F401, F403
+except (ImportError, AttributeError):
+    pass
+
 _VERSION_INFO = None
 """Global variable indicating the version of the PyFluent package - Empty by default"""
 
