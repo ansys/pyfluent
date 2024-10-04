68c67cf3ec2c7fdd94c15dd7198e1140ed26a9fc
Mainak Kundu <mainak.kundu@ansys.com>
2024-10-04 11:01:19 -0400

    feat: Add the remaining builtin settings

--- src/ansys/fluent/core/solver/settings_builtin_data.py
+++ src/ansys/fluent/core/solver/settings_builtin_data.py
@@ -331,6 +331,7 @@ DATA = {
             FluentVersion.v232: "setup.named_expressions",
         },
     ),
+    "Solution": ("Singleton", "solution"),
     "Methods": ("Singleton", "solution.methods"),
     "Controls": ("Singleton", "solution.controls"),
     "ReportDefinitions": ("Singleton", "solution.report_definitions"),
@@ -452,4 +453,497 @@ DATA = {
         },
     ),
     "RunCalculation": ("Singleton", "solution.run_calculation"),
+    "Results": ("Singleton", "results"),
+    "Surfaces": ("Singleton", "results.surfaces"),
+    "PointSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.point_surface",
+            FluentVersion.v242: "results.surfaces.point_surface",
+            FluentVersion.v241: "results.surfaces.point_surface",
+            FluentVersion.v232: "results.surfaces.point_surface",
+        },
+    ),
+    "PointSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.point_surface",
+            FluentVersion.v242: "results.surfaces.point_surface",
+            FluentVersion.v241: "results.surfaces.point_surface",
+            FluentVersion.v232: "results.surfaces.point_surface",
+        },
+    ),
+    "LineSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.line_surface",
+            FluentVersion.v242: "results.surfaces.line_surface",
+            FluentVersion.v241: "results.surfaces.line_surface",
+            FluentVersion.v232: "results.surfaces.line_surface",
+        },
+    ),
+    "LineSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.line_surface",
+            FluentVersion.v242: "results.surfaces.line_surface",
+            FluentVersion.v241: "results.surfaces.line_surface",
+            FluentVersion.v232: "results.surfaces.line_surface",
+        },
+    ),
+    "RakeSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.rake_surface",
+            FluentVersion.v242: "results.surfaces.rake_surface",
+            FluentVersion.v241: "results.surfaces.rake_surface",
+            FluentVersion.v232: "results.surfaces.rake_surface",
+        },
+    ),
+    "RakeSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.rake_surface",
+            FluentVersion.v242: "results.surfaces.rake_surface",
+            FluentVersion.v241: "results.surfaces.rake_surface",
+            FluentVersion.v232: "results.surfaces.rake_surface",
+        },
+    ),
+    "PlaneSurfaces": ("Singleton", "results.surfaces.plane_surface"),
+    "PlaneSurface": ("NamedObject", "results.surfaces.plane_surface"),
+    "IsoSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.iso_surface",
+            FluentVersion.v242: "results.surfaces.iso_surface",
+            FluentVersion.v241: "results.surfaces.iso_surface",
+            FluentVersion.v232: "results.surfaces.iso_surface",
+        },
+    ),
+    "IsoSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.iso_surface",
+            FluentVersion.v242: "results.surfaces.iso_surface",
+            FluentVersion.v241: "results.surfaces.iso_surface",
+            FluentVersion.v232: "results.surfaces.iso_surface",
+        },
+    ),
+    "IsoClips": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.iso_clip",
+            FluentVersion.v242: "results.surfaces.iso_clip",
+            FluentVersion.v241: "results.surfaces.iso_clip",
+        },
+    ),
+    "IsoClip": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.iso_clip",
+            FluentVersion.v242: "results.surfaces.iso_clip",
+            FluentVersion.v241: "results.surfaces.iso_clip",
+        },
+    ),
+    "ZoneSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.zone_surface",
+            FluentVersion.v242: "results.surfaces.zone_surface",
+            FluentVersion.v241: "results.surfaces.zone_surface",
+        },
+    ),
+    "ZoneSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.zone_surface",
+            FluentVersion.v242: "results.surfaces.zone_surface",
+            FluentVersion.v241: "results.surfaces.zone_surface",
+        },
+    ),
+    "PartitionSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.partition_surface",
+            FluentVersion.v242: "results.surfaces.partition_surface",
+            FluentVersion.v241: "results.surfaces.partition_surface",
+        },
+    ),
+    "PartitionSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.partition_surface",
+            FluentVersion.v242: "results.surfaces.partition_surface",
+            FluentVersion.v241: "results.surfaces.partition_surface",
+        },
+    ),
+    "TransformSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.transform_surface",
+            FluentVersion.v242: "results.surfaces.transform_surface",
+            FluentVersion.v241: "results.surfaces.transform_surface",
+        },
+    ),
+    "TransformSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.transform_surface",
+            FluentVersion.v242: "results.surfaces.transform_surface",
+            FluentVersion.v241: "results.surfaces.transform_surface",
+        },
+    ),
+    "ImprintSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.imprint_surface",
+            FluentVersion.v242: "results.surfaces.imprint_surface",
+            FluentVersion.v241: "results.surfaces.imprint_surface",
+        },
+    ),
+    "ImprintSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.imprint_surface",
+            FluentVersion.v242: "results.surfaces.imprint_surface",
+            FluentVersion.v241: "results.surfaces.imprint_surface",
+        },
+    ),
+    "PlaneSlices": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.plane_slice",
+            FluentVersion.v242: "results.surfaces.plane_slice",
+            FluentVersion.v241: "results.surfaces.plane_slice",
+        },
+    ),
+    "PlaneSlice": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.plane_slice",
+            FluentVersion.v242: "results.surfaces.plane_slice",
+            FluentVersion.v241: "results.surfaces.plane_slice",
+        },
+    ),
+    "SphereSlices": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.sphere_slice",
+            FluentVersion.v242: "results.surfaces.sphere_slice",
+            FluentVersion.v241: "results.surfaces.sphere_slice",
+        },
+    ),
+    "SphereSlice": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.sphere_slice",
+            FluentVersion.v242: "results.surfaces.sphere_slice",
+            FluentVersion.v241: "results.surfaces.sphere_slice",
+        },
+    ),
+    "QuadricSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.quadric_surface",
+            FluentVersion.v242: "results.surfaces.quadric_surface",
+            FluentVersion.v241: "results.surfaces.quadric_surface",
+        },
+    ),
+    "QuadricSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.quadric_surface",
+            FluentVersion.v242: "results.surfaces.quadric_surface",
+            FluentVersion.v241: "results.surfaces.quadric_surface",
+        },
+    ),
+    "SurfaceCells": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.surface_cells",
+            FluentVersion.v242: "results.surfaces.surface_cells",
+            FluentVersion.v241: "results.surfaces.surface_cells",
+        },
+    ),
+    "SurfaceCell": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.surface_cells",
+            FluentVersion.v242: "results.surfaces.surface_cells",
+            FluentVersion.v241: "results.surfaces.surface_cells",
+        },
+    ),
+    "ExpressionVolumes": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.expression_volume",
+        },
+    ),
+    "ExpressionVolume": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.expression_volume",
+        },
+    ),
+    "GroupSurfaces": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.surfaces.group_surface",
+        },
+    ),
+    "GroupSurface": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.surfaces.group_surface",
+        },
+    ),
+    "Graphics": ("Singleton", "results.graphics"),
+    "Meshes": ("Singleton", "results.graphics.mesh"),
+    "Mesh": ("NamedObject", "results.graphics.mesh"),
+    "Contours": ("Singleton", "results.graphics.contour"),
+    "Contour": ("NamedObject", "results.graphics.contour"),
+    "Vectors": ("Singleton", "results.graphics.vector"),
+    "Vector": ("NamedObject", "results.graphics.vector"),
+    "Pathlines": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.graphics.pathline",
+            FluentVersion.v242: "results.graphics.pathline",
+            FluentVersion.v241: "results.graphics.pathline",
+            FluentVersion.v232: "results.graphics.pathline",
+            FluentVersion.v231: "results.graphics.pathline",
+        },
+    ),
+    "Pathline": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.graphics.pathline",
+            FluentVersion.v242: "results.graphics.pathline",
+            FluentVersion.v241: "results.graphics.pathline",
+            FluentVersion.v232: "results.graphics.pathline",
+            FluentVersion.v231: "results.graphics.pathline",
+        },
+    ),
+    "ParticleTracks": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.graphics.particle_track",
+            FluentVersion.v242: "results.graphics.particle_track",
+            FluentVersion.v241: "results.graphics.particle_track",
+            FluentVersion.v232: "results.graphics.particle_track",
+            FluentVersion.v231: "results.graphics.particle_track",
+        },
+    ),
+    "ParticleTrack": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.graphics.particle_track",
+            FluentVersion.v242: "results.graphics.particle_track",
+            FluentVersion.v241: "results.graphics.particle_track",
+            FluentVersion.v232: "results.graphics.particle_track",
+            FluentVersion.v231: "results.graphics.particle_track",
+        },
+    ),
+    "LICs": ("Singleton", "results.graphics.lic"),
+    "LIC": ("NamedObject", "results.graphics.lic"),
+    "Plots": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot",
+            FluentVersion.v242: "results.plot",
+            FluentVersion.v241: "results.plot",
+            FluentVersion.v232: "results.plot",
+            FluentVersion.v231: "results.plot",
+        },
+    ),
+    "XYPlots": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot.xy_plot",
+            FluentVersion.v242: "results.plot.xy_plot",
+            FluentVersion.v241: "results.plot.xy_plot",
+            FluentVersion.v232: "results.plot.xy_plot",
+            FluentVersion.v231: "results.plot.xy_plot",
+        },
+    ),
+    "XYPlot": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.plot.xy_plot",
+            FluentVersion.v242: "results.plot.xy_plot",
+            FluentVersion.v241: "results.plot.xy_plot",
+            FluentVersion.v232: "results.plot.xy_plot",
+            FluentVersion.v231: "results.plot.xy_plot",
+        },
+    ),
+    "Histogram": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot.histogram",
+            FluentVersion.v242: "results.plot.histogram",
+            FluentVersion.v241: "results.plot.histogram",
+        },
+    ),
+    "CumulativePlots": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot.cumulative_plot",
+            FluentVersion.v242: "results.plot.cumulative_plot",
+            FluentVersion.v241: "results.plot.cumulative_plot",
+        },
+    ),
+    "CumulativePlot": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.plot.cumulative_plot",
+            FluentVersion.v242: "results.plot.cumulative_plot",
+            FluentVersion.v241: "results.plot.cumulative_plot",
+        },
+    ),
+    "ProfileData": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot.profile_data",
+            FluentVersion.v242: "results.plot.profile_data",
+        },
+    ),
+    "InterpolatedData": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.plot.interpolated_data",
+            FluentVersion.v242: "results.plot.interpolated_data",
+        },
+    ),
+    "Scenes": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.scene",
+            FluentVersion.v242: "results.scene",
+            FluentVersion.v241: "results.scene",
+            FluentVersion.v232: "results.scene",
+            FluentVersion.v231: "results.scene",
+        },
+    ),
+    "Scene": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.scene",
+            FluentVersion.v242: "results.scene",
+            FluentVersion.v241: "results.scene",
+            FluentVersion.v232: "results.scene",
+            FluentVersion.v231: "results.scene",
+        },
+    ),
+    "SceneAnimation": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.animations.scene_animation",
+            FluentVersion.v242: "results.animations.scene_animation",
+            FluentVersion.v241: "results.animations.scene_animation",
+        },
+    ),
+    "Report": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report",
+            FluentVersion.v242: "results.report",
+            FluentVersion.v241: "results.report",
+            FluentVersion.v232: "results.report",
+            FluentVersion.v231: "results.report",
+        },
+    ),
+    "DiscretePhaseHistogram": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report.discrete_phase.histogram",
+            FluentVersion.v242: "results.report.discrete_phase.histogram",
+            FluentVersion.v241: "results.report.discrete_phase.histogram",
+            FluentVersion.v232: "results.report.discrete_phase.histogram",
+            FluentVersion.v231: "results.report.discrete_phase.histogram",
+        },
+    ),
+    "Fluxes": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report.fluxes",
+            FluentVersion.v242: "results.report.fluxes",
+            FluentVersion.v241: "results.report.fluxes",
+            FluentVersion.v232: "results.report.fluxes",
+            FluentVersion.v231: "results.report.fluxes",
+        },
+    ),
+    "SurfaceIntegrals": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report.surface_integrals",
+            FluentVersion.v242: "results.report.surface_integrals",
+            FluentVersion.v241: "results.report.surface_integrals",
+            FluentVersion.v232: "results.report.surface_integrals",
+            FluentVersion.v231: "results.report.surface_integrals",
+        },
+    ),
+    "VolumeIntegrals": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report.volume_integrals",
+            FluentVersion.v242: "results.report.volume_integrals",
+            FluentVersion.v241: "results.report.volume_integrals",
+            FluentVersion.v232: "results.report.volume_integrals",
+            FluentVersion.v231: "results.report.volume_integrals",
+        },
+    ),
+    "InputParameters": (
+        "Singleton",
+        {
+            FluentVersion.v251: "parameters.input_parameters",
+            FluentVersion.v242: "parameters.input_parameters",
+            FluentVersion.v241: "parameters.input_parameters",
+        },
+    ),
+    "OutputParameters": (
+        "Singleton",
+        {
+            FluentVersion.v251: "parameters.output_parameters",
+            FluentVersion.v242: "parameters.output_parameters",
+            FluentVersion.v241: "parameters.output_parameters",
+        },
+    ),
+    "CustomFieldFunctions": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.custom_field_functions",
+        },
+    ),
+    "CustomFieldFunction": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.custom_field_functions",
+        },
+    ),
+    "CustomVectors": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.custom_vectors",
+            FluentVersion.v242: "results.custom_vectors",
+            FluentVersion.v241: "results.custom_vectors",
+        },
+    ),
+    "CustomVector": (
+        "NamedObject",
+        {
+            FluentVersion.v251: "results.custom_vectors",
+            FluentVersion.v242: "results.custom_vectors",
+            FluentVersion.v241: "results.custom_vectors",
+        },
+    ),
+    "SimulationReports": (
+        "Singleton",
+        {
+            FluentVersion.v251: "results.report.simulation_reports",
+            FluentVersion.v242: "results.report.simulation_reports",
+            FluentVersion.v241: "results.report.simulation_reports",
+            FluentVersion.v232: "results.report.simulation_reports",
+            FluentVersion.v231: "results.report.simulation_reports",
+        },
+    ),
 }
