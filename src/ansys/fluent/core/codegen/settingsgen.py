5e8101018f2d936fbeb81b0761c940394a48f496
Mainak Kundu <mainak.kundu@ansys.com>
2024-10-11 10:32:35 -0400

    feat: stub file

--- src/ansys/fluent/core/codegen/settingsgen.py
+++ src/ansys/fluent/core/codegen/settingsgen.py
@@ -68,10 +68,13 @@ def _get_unique_name(name):
     return name
 
 
-def _write_data(cls_name, python_name, data, f, version):
+def _write_data(cls_name, python_name, data, f, f_stub, version):
     bases = ", ".join([base.__name__ for base in data["bases"]])
     s = StringIO()
-    s.write(f"class {cls_name}({bases}):\n")
+    s_stub = StringIO()
+    cls_def = f"class {cls_name}({bases}):\n"
+    s.write(cls_def)
+    s_stub.write(cls_def)
     doc = data["doc"]
     doc = doc.replace("\n", "\n    ")
     s.write('    """\n')
@@ -80,18 +83,25 @@ def _write_data(cls_name, python_name, data, f, version):
     s.write(f"    version = {version!r}\n")
     s.write(f"    fluent_name = {data['fluent_name']!r}\n")
     s.write(f"    _python_name = {python_name!r}\n")
+    s_stub.write(f"    version: str\n")
+    s_stub.write(f"    fluent_name: str\n")
+    s_stub.write(f"    _python_name: str\n")
     child_names = data["child_names"]
     if child_names:
         s.write(f"    child_names = {child_names}\n")
+        s_stub.write(f"    child_names: List[str]\n")
     command_names = data["command_names"]
     if command_names:
         s.write(f"    command_names = {command_names}\n")
+        s_stub.write(f"    command_names: List[str]\n")
     query_names = data["query_names"]
     if query_names:
         s.write(f"    query_names = {query_names}\n")
+        s_stub.write(f"    query_names: List[str]\n")
     argument_names = data["argument_names"]
     if argument_names:
         s.write(f"    argument_names = {argument_names}\n")
+        s_stub.write(f"    argument_names: List[str]\n")
     classes_to_write = {}
     if data["child_classes"]:
         s.write("    _child_classes = dict(\n")
@@ -101,9 +111,11 @@ def _write_data(cls_name, python_name, data, f, version):
             unique_name = _NAME_BY_HASH.get(hash_)
             if unique_name:
                 s.write(f"        {k}={unique_name},\n")
+                s_stub.write(f"    {k}: {unique_name}\n")
             else:
                 unique_name = _get_unique_name(name)
                 s.write(f"        {k}={unique_name},\n")
+                s_stub.write(f"    {k}: {unique_name}\n")
                 classes_to_write[unique_name] = (name, v, hash_)
         s.write("    )\n")
     child_object_type = data["child_object_type"]
@@ -115,20 +127,24 @@ def _write_data(cls_name, python_name, data, f, version):
             child_object_type,
             _gethash(child_object_type),
         )
+        s_stub.write(f"    child_object_type: {name}\n")
     child_aliases = data["child_aliases"]
     if child_aliases:
         s.write("    _child_aliases = dict(\n")
         for k, v in child_aliases.items():
             s.write(f"        {k}={v!r},\n")
         s.write("    )\n")
+        s_stub.write("    _child_aliases: dict\n")
     return_type = data["return_type"]
     if return_type:
         s.write(f"    return_type = {return_type!r}\n")
+        s_stub.write(f"    return_type: str\n")
     s.write("\n")
     for name, (python_name, data, hash_) in classes_to_write.items():
         _NAME_BY_HASH[hash_] = name
-        _write_data(name, python_name, data, f, version)
+        _write_data(name, python_name, data, f, f_stub, version)
     f.write(s.getvalue())
+    f_stub.write(s_stub.getvalue())
 
 
 def generate(version: str, static_infos: dict) -> None:
@@ -141,25 +157,37 @@ def generate(version: str, static_infos: dict) -> None:
     output_file = (
         pyfluent.CODEGEN_OUTDIR / "solver" / f"settings_{version}.py"
     ).resolve()
+    output_stub_file = output_file.parent / f"{output_file.stem}.pyi"
     cls, _ = get_cls("", sinfo, version=version)
     data = _populate_data(cls, api_tree)
-    with open(output_file, "w") as f:
-        f.write("from ansys.fluent.core.solver.flobject import *\n\n")
-        f.write("from ansys.fluent.core.solver.flobject import (\n")
-        f.write("    _ChildNamedObjectAccessorMixin,\n")
-        f.write("    _NonCreatableNamedObjectMixin,\n")
-        f.write("    _InputFile,\n")
-        f.write("    _OutputFile,\n")
-        f.write("    _InOutFile,\n")
-        f.write(")\n\n")
+    with open(output_file, "w") as f, open(output_stub_file, "w") as f_stub:
+        header = StringIO()
+        header.write("#\n")
+        header.write("# This is an auto-generated file.  DO NOT EDIT!\n")
+        header.write("#\n")
+        header.write("\n")
+        header.write("from ansys.fluent.core.solver.flobject import *\n\n")
+        header.write("from ansys.fluent.core.solver.flobject import (\n")
+        header.write("    _ChildNamedObjectAccessorMixin,\n")
+        header.write("    _NonCreatableNamedObjectMixin,\n")
+        header.write("    _InputFile,\n")
+        header.write("    _OutputFile,\n")
+        header.write("    _InOutFile,\n")
+        header.write(")\n\n")
+        f.write(header.getvalue())
+        f_stub.write(header.getvalue())
         f.write(f'SHASH = "{_gethash(sinfo)}"\n\n')
+        f_stub.write("from typing import Union, List, Tuple\n\n")
         name = data["name"]
         _NAME_BY_HASH[_gethash(data)] = name
-        _write_data(name, name, data, f, version)
+        _write_data(name, name, data, f, f_stub, version)
     file_size = output_file.stat().st_size / 1024 / 1024
+    file_size_stub = output_stub_file.stat().st_size / 1024 / 1024
     print(
-        f"Generated {output_file.name} in {time.time() - start_time:.2f} seconds. Size: {file_size:.2f} MB."
+        f"Generated {output_file.name} and {output_stub_file.name} in {time.time() - start_time:.2f} seconds."
     )
+    print(f"{output_file.name} size: {file_size:.2f} MB")
+    print(f"{output_stub_file.name} size: {file_size_stub:.2f} MB")
     return {"<solver_session>": api_tree}
 
 
