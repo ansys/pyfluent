4905bf6793042de3db63b2f9843a1cf049bc729b
Oleg Chernukhin <oleg.chernukhin@ansys.com>
2024-11-11 12:29:13 -0500

    feat: add a lower-level test

--- tests/test_systemcoupling.py
+++ tests/test_systemcoupling.py
@@ -1,5 +1,18 @@
 import pytest
 
+@pytest.mark.fluent_version(">=25.1")
+def test_systemcoupling_mixing_elbow_settings_apis(mixing_elbow_case_data_session):
+    """Test System Coupling related settings APIs"""
+    solver = mixing_elbow_case_data_session
+    # enable the feature flag to be able to make the queries
+    solver.scheme_eval.scheme_eval("(enable-feature 'sc/participant-info)")
+    elbow_fluid = "elbow_fluid"
+    region_names = solver.settings.setup.models.system_coupling.get_all_regions()
+    assert elbow_fluid in region_names
+    # elbow fluid must be a volume
+    assert solver.settings.setup.models.system_coupling.get_topology(
+        region_name = elbow_fluid) == "Volume"
+
 
 def _test_systemcoupling_mixing_elbow_settings_common(mixing_elbow_case_data_session):
     solver = mixing_elbow_case_data_session
