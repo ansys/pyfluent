4e467470e20fb7884c908e1155c3339432c2b9a8
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2024-11-27 19:12:27 +0530

    Update __eq__ implementation in flobject.Base

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -470,6 +470,8 @@ class Base:
         return nullcontext()
 
     def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
         return self.flproxy == other.flproxy and self.path == other.path
 
 
