2e898631080345d2f9caf58f8d556bc8051d3bc8
ANSYS\spearson <sean.pearson@ansys.com>
2024-12-19 18:38:56 +0000

    update api walk fn

--- src/ansys/fluent/core/codegen/walk_api.py
+++ src/ansys/fluent/core/codegen/walk_api.py
@@ -7,19 +7,21 @@ Example
 
     >>> from ansys.fluent.core.codegen import walk_api
     >>> from ansys.fluent.core.generated.solver import settings_252
-    >>> walk_api.walk_api(settings_252.root, lambda p: print(p), current_path=[])
+    >>> walk_api.walk_api(settings_252.root, lambda p, is_method: print(p), current_path=[])
 
 """
 
 from typing import List
 
 
-def walk_api(api_root_cls, on_each_path, current_path: str | List[str] = ""):
+def walk_api(
+    api_cls, on_each_path, current_path: str | List[str] = "", is_method: bool = False
+):
     """
     Recursively traverse the API hierarchy, calling `on_each_path` for each item.
 
     Parameters:
-    - api_root_cls: The root class of the API hierarchy.
+    - api_cls: The current class of the API hierarchy.
     - on_each_path: A callback function to call for each path.
     - current_path: The current path in the hierarchy (default: empty string).
       Paths can be either dot-separated strings or string lists. The type is
@@ -27,18 +29,11 @@ def walk_api(api_root_cls, on_each_path, current_path: str | List[str] = ""):
     """
     # Skip the root path
     if current_path:
-        on_each_path(current_path)
+        on_each_path(current_path, is_method)
 
-    # Get child names and their respective classes
-    all_names = [
-        name
-        for attr in ("child_names", "argument_names", "command_names", "query_names")
-        for name in getattr(api_root_cls, attr, [])
-    ]
-    child_classes = getattr(api_root_cls, "_child_classes", {})
+    child_classes = getattr(api_cls, "_child_classes", {})
 
-    # Traverse each child
-    for child_name in all_names:
+    def _traverse(child_name, is_method):
         if child_name in child_classes:
             child_cls = child_classes[child_name]
             # Construct the new path
@@ -49,9 +44,36 @@ def walk_api(api_root_cls, on_each_path, current_path: str | List[str] = ""):
                     f"{current_path}.{child_name}" if current_path else child_name
                 )
             # Recursively walk the child
-            walk_api(child_cls, on_each_path, new_path)
+            walk_api(child_cls, on_each_path, new_path, is_method)
 
-    # Delegate directly to any child_object_type (relevant for named objects)
-    child_object_type = getattr(api_root_cls, "child_object_type", None)
-    if child_object_type:
-        walk_api(child_cls, on_each_path, current_path)
+            # Delegate directly to any child_object_type (relevant for named objects)
+            child_object_type = getattr(api_cls, "child_object_type", None)
+            if child_object_type:
+                walk_api(child_cls, on_each_path, current_path, is_method)
+
+    # Get child names and their respective classes
+    if is_method:
+        arg_names = [
+            name for attr in ("argument_names",) for name in getattr(api_cls, attr, [])
+        ]
+
+        for arg_name in arg_names:
+            _traverse(arg_name, is_method=False)
+
+    else:
+
+        child_names = [
+            name for attr in ("child_names",) for name in getattr(api_cls, attr, [])
+        ]
+
+        for child_name in child_names:
+            _traverse(child_name, is_method=False)
+
+        method_names = [
+            name
+            for attr in ("command_names", "query_names")
+            for name in getattr(api_cls, attr, [])
+        ]
+
+        for method_name in method_names:
+            _traverse(method_name, is_method=True)
