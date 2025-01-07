2ce832aba702a552887c2ffcde65b1aa5de2f4ce
Mainak Kundu <mainak.kundu@ansys.com>
2025-01-06 21:46:08 -0500

    test: fix

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -24,6 +24,7 @@ def test_fluent_fatal_error(error_code, raises, new_solver_session):
             time.sleep(0.1)
 
 
+@pytest.mark.fluent_version(">=25.2")
 def test_custom_python_error_via_grpc(datamodel_api_version_new, new_solver_session):
     solver = new_solver_session
     # This may need to be updated if the error type changes in the server
