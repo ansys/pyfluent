961d724705f58ef83574280fb0a9542af9ca77e3
ANSYS\spearson <sean.pearson@ansys.com>
2025-02-05 15:46:54 +0000

    chgs

--- tests/conftest.py
+++ tests/conftest.py
@@ -428,10 +428,8 @@ def disable_datamodel_cache(monkeypatch: pytest.MonkeyPatch):
 def datamodel_api_version_all(request, monkeypatch: pytest.MonkeyPatch) -> None:
     if request.param == "new":
         monkeypatch.setenv("REMOTING_NEW_DM_API", "1")
-        monkeypatch.setenv("REMOTING_MAPPED_NEW_DM_API", "1")
 
 
 @pytest.fixture
 def datamodel_api_version_new(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setenv("REMOTING_NEW_DM_API", "1")
-    monkeypatch.setenv("REMOTING_MAPPED_NEW_DM_API", "1")
