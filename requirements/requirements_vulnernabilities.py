8b81f919e267776e1a1db91403213c0dff04d9b5
hpohekar <harshal.pohekar@ansys.com>
2025-02-17 11:02:23 +0530

    ci: Refactoring 1

---
+++ requirements/requirements_vulnernabilities.py
@@ -0,0 +1,23 @@
+"""This module installs the required packages for the vulnerabilities check."""
+
+import subprocess  # nosec B404
+import sys
+
+try:
+    import bandit.cli.main as bandit  # noqa: F401
+    import click  # noqa: F401
+    import github  # noqa: F401
+    import safety.cli as safety  # noqa: F401
+except ModuleNotFoundError:
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "click>=7.0,<9",
+            "PyGithub>=1.59,<2",
+            "bandit>=1.7,<2",
+            "safety>=2.3,<4",
+        ]
+    )
