6a1877e3e5c252a88a58d970d18a2d7f6b64bd70
hpohekar <harshal.pohekar@ansys.com>
2025-04-02 19:55:36 +0530

    remove compose revert

--- src/ansys/fluent/core/docker/docker_compose.py
+++ src/ansys/fluent/core/docker/docker_compose.py
@@ -265,19 +265,10 @@ class DockerComposeLauncher(LauncherProtocol[DockerComposeLaunchConfig]):
             platform_name + cmd, input="y\n", capture_output=True, text=True
         )
 
-    def remove_compose_file(self) -> None:
-        """Remove the compose file."""
-        try:
-            with self._get_compose_file() as compose_file:
-                Path(str(compose_file)).unlink(missing_ok=True)
-        except Exception:
-            pass
-
     def exit(self) -> None:
         """Exit the container launcher."""
         self.stop()
         self.prune_network()
-        self.remove_compose_file()
 
     def check(self, timeout: float | None = None) -> bool:
         """Check if the services are running."""
