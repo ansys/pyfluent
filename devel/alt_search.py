6b9dd72583c4a362e5b168b7d61a8d94687acd53
Mainak Kundu <mainak.kundu@ansys.com>
2025-04-08 17:04:31 -0400

    feat: alt_search - basic substring search

--- devel/alt_search.py
+++ devel/alt_search.py
@@ -9,55 +9,6 @@ from ansys.fluent.core.generated.solver.settings_252 import root
 from ansys.fluent.core.solver.flobject import NamedObject
 
 
-class TrieNode:
-    """
-    A node in the Trie data structure.
-    """
-
-    def __init__(self):
-        self.children = {}
-        self.results = []
-
-
-class Trie:
-    """
-    A Trie (prefix tree) data structure for storing and searching search results."
-    """
-
-    def __init__(self):
-        self._root = TrieNode()
-
-    def insert(self, word, result):
-        """
-        Inserts a word into the Trie and associates it with the given results.
-        """
-        node = self._root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.results.append(result)
-
-    def search(self, prefix):
-        """
-        Searches all results in the Trie for the given word and collects results from all child nodes.
-        """
-
-        def collect_results(node):
-            results = list(node.results)
-            for child in node.children.values():
-                results.extend(collect_results(child))
-            return results
-
-        node = self._root
-        for char in prefix:
-            if char not in node.children:
-                return []
-            node = node.children[char]
-
-        return collect_results(node)
-
-
 def get_name_components(name: str):
     """
     Given a name like 'abc_def' returns ['abc', 'def']
@@ -65,33 +16,26 @@ def get_name_components(name: str):
     return name.split("_")
 
 
-def get_all_ending_substrings(name_component: str):
-    """
-    Given a name component like 'abc' returns all ending substrings of length > 1: ['abc', 'bc']
-    """
-    return [
-        name_component[i:]
-        for i in range(len(name_component))
-        if len(name_component[i:]) > 1
-    ]
+SearchCache = {}
 
 
-def build_trie(root_cls):
+def build_cache(root_cls):
     """
     Build a trie from the settings module
     """
-    print(f"Memory usage before building trie: {get_memory_usage():.2f} MB")
+    print(f"Memory usage before building cache: {get_memory_usage():.2f} MB")
 
     # A depth-first algorithm is chosen for the following reasons:
     # 1. Show the search results in a depth-first order of the settings API.
     # 2. Can support a `depth` parameter in the search function to limit the depth of the search.
-    queue = deque([("", root_cls, "<solver_session>.settings")])
+    queue_order = 0
+    queue = deque([("", root_cls, "<solver_session>.settings", queue_order)])
 
     while queue:
-        current_name, current_cls, current_path = queue.popleft()
-        for component in get_name_components(current_name):
-            for substring in get_all_ending_substrings(component):
-                SettingsTrie.insert(substring, current_path)
+        current_name, current_cls, current_path, rank = queue.popleft()
+        SearchCache.setdefault(current_name, []).append((current_path, rank))
+        for name_component in get_name_components(current_name):
+            SearchCache.setdefault(name_component, []).append((current_path, rank))
 
         if not hasattr(current_cls, "_child_classes"):
             continue
@@ -105,20 +49,28 @@ def build_trie(root_cls):
                 next_path = f'{current_path}.{k}["_name_"]'
             # with open("alt_search.log", "a") as f:
             #     f.write(f"{next_path}\n")
-            queue.append((k, next_cls, next_path))
+            queue_order += 1
+            queue.append((k, next_cls, next_path, queue_order))
 
-    print(f"Memory usage after building trie: {get_memory_usage():.2f} MB")
-
-
-SettingsTrie = Trie()
+    print(f"Memory usage after building cache: {get_memory_usage():.2f} MB")
 
 
-def search(search_term):
+def search(search_string: str, match_whole_word: bool = False):
     """
-    Basic substring search
+    Basic string-based search
     """
-    results = SettingsTrie.search(search_term)
-    return results
+    if not SearchCache:
+        build_cache(root)
+        # with open("alt_search.log", "w") as f:
+        #     pprint(SearchCache, stream=f)
+    if match_whole_word:
+        results = SearchCache.get(search_string, [])
+    else:
+        results = [
+            item for k, v in SearchCache.items() if search_string in k for item in v
+        ]
+    results.sort(key=lambda x: x[1])
+    return [x[0] for x in results]
 
 
 def get_memory_usage():
@@ -131,9 +83,10 @@ def get_memory_usage():
 
 
 if __name__ == "__main__":
-    build_trie(root)
     # Example usage
+    pprint(search("viscous", match_whole_word=True))
+    pprint(len(search("viscous", match_whole_word=True)))
+    pprint(search("read_case", match_whole_word=True))
+    pprint(len(search("read_case", match_whole_word=True)))
     pprint(search("viscous"))
-    pprint(search("isco"))
     pprint(len(search("viscous")))
-    pprint(len(search("isco")))
