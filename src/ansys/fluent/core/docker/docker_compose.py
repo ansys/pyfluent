28abad0a211cd249b6e71cf8eb5fb20cf5027e96
hpohekar <harshal.pohekar@ansys.com>
2025-04-17 18:01:06 +0530

    remove from compose file

--- src/ansys/fluent/core/docker/docker_compose.py
+++ src/ansys/fluent/core/docker/docker_compose.py
@@ -137,7 +137,7 @@ def _set_env_vars(container_dict):
             "FLUENT_NO_AUTOMATIC_TRANSCRIPT", "0"
         ),
         "REMOTE_NEW_DM_API": container_dict["environment"].get(
-            "REMOTING_NEW_DM_API", "0"
+            "REMOTING_NEW_DM_API", ""
         ),
         "COMMAND": " ".join(container_dict["command"]),
         "MOUNT_TARGET": container_dict.get("mount_target"),
