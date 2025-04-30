739abdb3f49585d5dd3a05e23a6a31f657bac7a9
ANSYS\spearson <sean.pearson@ansys.com>
2025-04-30 20:19:41 +0100

    testing

--- tests/test_physical_quantities.py
+++ tests/test_physical_quantities.py
@@ -32,19 +32,32 @@ def test_field_data_transactions_deprecated_interface(new_solver_session) -> Non
 
     solver.settings.solution.initialization.hybrid_initialize()
 
+    fields = solver.fields
+
     temperature = PhysicalQuantities.TEMPERATURE
+    locations = ["hot-inlet"]
 
-    temperature_field_data = solver.fields.field_data.get_scalar_field_data(
-        field_name=temperature, surfaces=["hot-inlet"]
+    temperature_field_data = fields.field_data.get_scalar_field_data(
+        field_name=temperature, surfaces=locations
     )
-    assert round(temperature_field_data["hot-inlet"][0]) == 305
+    assert round(temperature_field_data[locations[0]][0]) == 305
 
-    temperature_min = solver.fields.reduction.minimum(
-        expression=temperature, locations=["hot-inlet"]
+    temperature_min = fields.reduction.minimum(
+        expression=temperature, locations=locations
     )
     assert round(temperature_min) == 313
 
-    temperature_solution_data = solver.fields.solution_variable_data.get_data(
-        solution_variable_name=temperature, zone_names=["hot-inlet"]
+    temperature_solution_data = fields.solution_variable_data.get_data(
+        solution_variable_name=temperature, zone_names=locations
     )
-    assert round(temperature_solution_data["hot-inlet"][0]) == 313
+    assert round(temperature_solution_data[locations[0]][0]) == 313
+
+    report_defs = solver.settings.solution.report_definitions
+    report_defs.surface["yyy"] = {}
+    surface = report_defs.surface["yyy"]
+    surface.report_type = "surface-areaavg"
+    surface.field = "temperature"
+    surface.surface_names = locations
+
+    result = report_defs.compute(report_defs=["yyy"])
+    assert round(result[0]["yyy"][0]) == 313
