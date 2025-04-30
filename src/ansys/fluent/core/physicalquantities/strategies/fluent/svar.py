2a6454aa37ebec7dcc12f05b7df939a4cca0028e
ANSYS\spearson <sean.pearson@ansys.com>
2025-04-30 19:33:17 +0100

    add missing files

---
+++ src/ansys/fluent/core/physicalquantities/strategies/fluent/svar.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2021 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Provides a ConversionStrategy for mapping PhysicalQuantity to Fluent's SVAR names.
+"""
+
+from ansys.fluent.core.physicalquantities.base import PhysicalQuantities
+from ansys.fluent.core.physicalquantities.strategy import MappingConversionStrategy
+
+
+class FluentSVarStrategy(MappingConversionStrategy):
+    """This strategy handles conversion of selected PhysicalQuantities into Fluent's
+    server-side field variable naming conventions (e.g., "SV_P" for pressure).
+    """
+
+    _mapping = {
+        PhysicalQuantities.PRESSURE: "SV_P",
+        PhysicalQuantities.VELOCITY_X: "SV_U",
+        PhysicalQuantities.TEMPERATURE: "SV_T",
+    }
