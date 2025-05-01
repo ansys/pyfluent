3a7382b6f00f5a0cdda7ff5947da74616398e8cc
hpohekar <harshal.pohekar@ansys.com>
2025-05-01 08:43:13 +0530

    apply latest suggestion 2

--- src/ansys/fluent/core/fluent_connection.py
+++ src/ansys/fluent/core/fluent_connection.py
@@ -568,15 +568,11 @@ class FluentConnection:
                 )
                 cmd_list.append(cleanup_file_name)
                 logger.debug(f"Cleanup command list = {cmd_list}")
-                process = subprocess.Popen(
+                subprocess.Popen(
                     cmd_list,
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.DEVNULL,
                 )
-                process.communicate(timeout=120)
-                return_code = process.wait(timeout=120)
-                if return_code != 0:
-                    raise subprocess.CalledProcessError(return_code, cmd_list)
             elif self._slurm_job_id:
                 logger.debug("Fluent running inside Slurm, closing Slurm session...")
                 self._close_slurm()
