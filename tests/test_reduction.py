5d27470489c9ddc2a4363b6655830ddafdfb884b
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2025-05-23 12:59:37 +0530

    Update tests/test_reduction.py

--- tests/test_reduction.py
+++ tests/test_reduction.py
@@ -408,7 +408,7 @@ def static_mixer_case_session2(static_mixer_case_session: Any):
     return static_mixer_case_session
 
 
-@pytest.mark.fluent_version(">=23.1")
+@pytest.mark.fluent_version(">=24.1")
 def test_reductions(
     static_mixer_case_session: Any, static_mixer_case_session2: Any
 ) -> None:
