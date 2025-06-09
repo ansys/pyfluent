887ce434360a2fd3e09e25380e9d9656fdd5bcc4
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2025-06-09 15:28:47 +0530

    Update src/ansys/fluent/core/services/datamodel_se.py

--- src/ansys/fluent/core/services/datamodel_se.py
+++ src/ansys/fluent/core/services/datamodel_se.py
@@ -1974,7 +1974,8 @@ class PyCommand:
             logger.warning(
                 "datamodels_se.PyCommand was unable to construct command arguments. "
                 "This may be due to gRPC issues or unsupported Fluent version (23.1+ required). "
-                "Error details: %s", e
+                "Error details: %s", 
+                e
             )
 
     def create_instance(self) -> "PyCommandArguments":
