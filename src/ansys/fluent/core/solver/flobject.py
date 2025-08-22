58676db981a454d973a4971954fdfddd89dde907
Mainak Kundu <mainak.kundu@ansys.com>
2025-08-22 10:32:09 -0400

    feat: Add _has_migration_adapter bool to flobject class

--- src/ansys/fluent/core/solver/flobject.py
+++ src/ansys/fluent/core/solver/flobject.py
@@ -2282,7 +2282,7 @@ def get_cls(name, info, parent=None, version=None, parent_taboo=None):
                 )
             cls._allowed_values = allowed_values
 
-        has_migration_adapter = info.get("has_migration_adapter", False)
+        has_migration_adapter = info.get("has-migration-adapter?", False)
         if has_migration_adapter:
             cls._has_migration_adapter = True
 
