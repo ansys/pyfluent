e04909221193722a06e25ab5fe721caf4d263a2a
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2025-10-16 13:27:47 +0530

    Update logic.

--- src/ansys/fluent/core/utils/deprecate.py
+++ src/ansys/fluent/core/utils/deprecate.py
@@ -99,18 +99,18 @@ def deprecate_arguments(
                     stacklevel=2,
                 )
 
-            sig = inspect.signature(converter)
-            params = sig.parameters
-            n_params = len(params)
-            if n_params == 1:
-                kwargs = converter(kwargs)
-            elif n_params == 3:
-                kwargs = converter(kwargs, old_args, new_args)
-            else:
-                raise TypeError(
-                    f"Converter must accept either (kwargs) or (kwargs, old_args, new_args), "
-                    f"but got {n_params} parameter(s): {list(params)}"
-                )
+                sig = inspect.signature(converter)
+                params = sig.parameters
+                n_params = len(params)
+                if n_params == 1:
+                    kwargs = converter(kwargs)
+                elif n_params == 3:
+                    kwargs = converter(kwargs, old_args, new_args)
+                else:
+                    raise TypeError(
+                        f"Converter must accept either (kwargs) or (kwargs, old_args, new_args), "
+                        f"but got {n_params} parameter(s): {list(params)}"
+                    )
             return deprecated_func(*args, **kwargs)
 
         return wrapper
