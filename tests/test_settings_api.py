ee3978686062e593528e7bbe57dfbea6541d8b8c
Mainak Kundu <mainak.kundu@ansys.com>
2025-12-03 14:24:10 -0500

    fix: unskip-tests

--- tests/test_settings_api.py
+++ tests/test_settings_api.py
@@ -639,6 +639,7 @@ def test_deprecated_command_arguments(mixing_elbow_case_data_session):
     }
 
 
+@pytest.mark.skip(reason="https://github.com/ansys/pyfluent/issues/4298")
 @pytest.mark.fluent_version(">=25.2")
 def test_return_types_of_operations_on_named_objects(mixing_elbow_settings_session):
     solver = mixing_elbow_settings_session
