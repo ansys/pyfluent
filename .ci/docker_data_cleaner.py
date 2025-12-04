c2e19107ef9cbe041e9981f5c4970e4fa601f986
Mainak Kundu <mainak.kundu@ansys.com>
2025-12-03 20:01:26 -0500

    feat: Remove old Fluent version support

--- .ci/docker_data_cleaner.py
+++ .ci/docker_data_cleaner.py
@@ -13,25 +13,32 @@ IMAGE_TAGS_TO_RETAIN = ["v24.2.5", "v25.1.4", "v25.2.3"]
 def clean_docker_data():
     """Cleans up all docker data except for specified images."""
     # Stop and remove all containers
-    subprocess.run("docker ps -aq | xargs -r docker rm -f", shell=True, check=True)
+    container_ids = subprocess.check_output(["docker", "ps", "-aq"]).decode().split()
+    for container_id in container_ids:
+        subprocess.run(["docker", "rm", "-f", container_id], check=True)
 
     # Remove all images except those in IMAGE_TAGS_TO_RETAIN and the dev image
     images_to_retain = [f"ghcr.io/ansys/fluent:{tag}" for tag in IMAGE_TAGS_TO_RETAIN]
     dev_image_sha = os.environ["FLUENT_STABLE_IMAGE_DEV"]
     images_output = subprocess.check_output(
-        "docker images --format '{{.Repository}}:{{.Tag}} {{.ID}}' --no-trunc",
-        shell=True,
+        [
+            "docker",
+            "images",
+            "--format",
+            "{{.Repository}}:{{.Tag}} {{.ID}}",
+            "--no-trunc",
+        ]
     ).decode()
     for line in images_output.splitlines():
-        image, image_id = line.split()
+        image, image_id = line.strip().split()
         if image not in images_to_retain and image_id != dev_image_sha:
-            subprocess.run(f"docker rmi -f {image_id}", shell=True, check=True)
+            subprocess.run(["docker", "rmi", "-f", image_id], check=True)
 
     # Remove dangling images
-    subprocess.run("docker image prune -f", shell=True, check=True)
+    subprocess.run(["docker", "image", "prune", "-f"], check=True)
 
     # Remove everything else (networks, volumes)
-    subprocess.run("docker system prune -f --volumes", shell=True, check=True)
+    subprocess.run(["docker", "system", "prune", "-f", "--volumes"], check=True)
 
 
 if __name__ == "__main__":
