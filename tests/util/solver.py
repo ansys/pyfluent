9afbe312b122070e25141ad45d5c023ed6172f38
Mainak Kundu <94432368+mkundu1@users.noreply.github.com>
2025-12-10 16:00:52 -0500

    fix: Fix test_cad_to_post tests [skip tests] (#4718)
    In [PR
    4690](https://github.com/ansys/pyfluent/pull/4690/files#diff-875c798eaa6ca5dfeb9fc13a45790312f810bb57f4203933c39f7690603b0d40L478),
    I removed some Fluent version checks within cad_to_post tests which
    makes some settings API code to run with all supported Fluent versions.
    That code was specific to Fluent 23.1. In this PR, I have updated that
    code for Fluent >= 24.2.
    
    
    Nightly dev-testing run -
    https://github.com/ansys/pyfluent/actions/runs/20070867784
    
    ---------
    
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- tests/util/solver.py
+++ tests/util/solver.py
@@ -25,7 +25,7 @@ def check_report_definition_result(
     report_definitions, report_definition_name, expected_result
 ):
     assert (
-        report_definitions.compute(report_defs=[report_definition_name])[
+        report_definitions.compute(report_defs=[report_definition_name])[0][
             report_definition_name
         ][0]
         == expected_result
