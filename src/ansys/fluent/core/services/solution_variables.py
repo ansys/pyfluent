bc787f16e9d2326b20e1d6ab82b75222bed5ba18
Gobot1234 <gobot1234yt@gmail.com>
2026-02-17 15:54:54 +0000

    fix the typing only symbol

--- src/ansys/fluent/core/services/solution_variables.py
+++ src/ansys/fluent/core/services/solution_variables.py
@@ -109,9 +109,7 @@ class SolutionVariableInfo:
         class SolutionVariable:
             """Class containing information for single solution variable."""
 
-            def __init__(
-                self, solution_variable_info: SvarProtoModule.global___SvarInfo
-            ):
+            def __init__(self, solution_variable_info: SvarProtoModule.SvarInfo):
                 """Initialize SolutionVariable."""
                 self.name = solution_variable_info.name
                 self.dimension = solution_variable_info.dimension
@@ -122,9 +120,7 @@ class SolutionVariableInfo:
             def __repr__(self):
                 return f"name:{self.name} dimension:{self.dimension} field_type:{self.field_type}"
 
-        def __init__(
-            self, solution_variables_info: Sequence[SvarProtoModule.global___SvarInfo]
-        ):
+        def __init__(self, solution_variables_info: Sequence[SvarProtoModule.SvarInfo]):
             """Initialize SolutionVariables."""
             self._solution_variables_info: dict[
                 str, "SolutionVariableInfo.SolutionVariables.SolutionVariable"
