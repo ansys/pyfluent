5dd5f83ddd8c3c0dbc7d7ceaa57388873f87bb26
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2026-02-23 16:10:48 +0530

    Handle: B301.

--- src/ansys/fluent/core/utils/dump_session_data.py
+++ src/ansys/fluent/core/utils/dump_session_data.py
@@ -131,7 +131,7 @@ class DumpDataReader:
             str(Path(file_name).resolve()),
             "rb",
         ) as pickle_obj:
-            self._session_data = pickle.load(pickle_obj)
+            self._session_data = pickle.load(pickle_obj)  # nosec B301
 
     def get_session_data(self):
         """Get session data."""
