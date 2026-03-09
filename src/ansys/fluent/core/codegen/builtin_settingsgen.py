625f9dac3b184024ae72ad3063b201dbaa02b0dc
mayankansys <mayank.kumar@ansys.com>
2026-03-09 15:19:14 +0530

    Apply suggestion from @prmukherj
    Co-authored-by: Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>

--- src/ansys/fluent/core/codegen/builtin_settingsgen.py
+++ src/ansys/fluent/core/codegen/builtin_settingsgen.py
@@ -38,7 +38,6 @@ _PYI_FILE = config.codegen_outdir / "solver" / "settings_builtin.pyi"
 
 
 def _get_settings_root(version: str):
-    from ansys.fluent.core.module_config import config
     from ansys.fluent.core.utils import load_module as _load_module
 
     settings = _load_module(
