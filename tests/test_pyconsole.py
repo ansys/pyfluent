75e8bf8bb567b108ac8ebc580d359644f9c3147a
Mainak Kundu <mainak.kundu@synopsys.com>
2026-06-05 14:36:10 -0400

    tests: Add pyconsole test

--- tests/test_pyconsole.py
+++ tests/test_pyconsole.py
@@ -21,6 +21,9 @@ def _get_grpc_version_in_fluent_env():
     session.scheme_eval.scheme_eval('(%py-exec "import grpc")')
     return session.scheme_eval.scheme_eval('(%py-eval "grpc.__version__")')
 
+def _is_pyconsole_activated(session):
+    return session.scheme_eval("(%cx-pyconsole-activated?)")
+
 
 # Note: this test won't work with editable install of PyFluent because the PyFluent package files are not copied
 # to the expected location in the Fluent container. To run this test, install PyFluent without the -e option.
@@ -43,12 +46,14 @@ def test_pyconsole_launch():
     src_pyfluent_dir = str(Path(pyfluent.__file__).parents[3])
     version_for_file_name = pyfluent.FluentVersion.current_dev().number
     dst_pyfluent_dir = f"/ansys_inc/v{version_for_file_name}/commonfiles/CPython/3_10/linx64/Release/Ansys/PyFluentCore"
-    print(f"src_pyfluent_dir: {src_pyfluent_dir}")
-    print(f"dst_pyfluent_dir: {dst_pyfluent_dir}")
-    container_dict = pyfluent.launch_fluent(start_container=True, dry_run=True)
-    container_dict["volumes"].append(f"{src_pyfluent_dir}:{dst_pyfluent_dir}")
-    session = pyfluent.launch_fluent(container_dict=container_dict, py=True)
-    import pdb
+    solver_container_dict = pyfluent.launch_fluent(start_container=True, dry_run=True, py=True)
+    solver_container_dict["volumes"].append(f"{src_pyfluent_dir}:{dst_pyfluent_dir}")
+    solver_session = pyfluent.launch_fluent(container_dict=solver_container_dict)
+    assert solver_session is not None
+    assert _is_pyconsole_activated(solver_session) is True
 
-    pdb.set_trace()
-    assert session is not None
+    meshing_container_dict = pyfluent.launch_fluent(start_container=True, dry_run=True, py=True, mode=pyfluent.FluentMode.MESHER)
+    meshing_container_dict["volumes"].append(f"{src_pyfluent_dir}:{dst_pyfluent_dir}")
+    meshing_session = pyfluent.launch_fluent(container_dict=meshing_container_dict)
+    assert meshing_session is not None
+    assert _is_pyconsole_activated(meshing_session) is True
