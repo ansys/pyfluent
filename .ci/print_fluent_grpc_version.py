b5445725d33c4ee3f7735903cc0d87510251b5bb
Mainak Kundu <mainak.kundu@ansys.com>
2026-06-08 14:45:33 -0400

    fix: CI

--- .ci/print_fluent_grpc_version.py
+++ .ci/print_fluent_grpc_version.py
@@ -1,8 +1,10 @@
 """Get gRPC version from Fluent image and print it to the console."""
 
 import ansys.fluent.core as pyfluent
+from ansys.fluent.core.docker.utils import get_grpc_launcher_args_for_gh_runs
 
 if __name__ == "__main__":
-    session = pyfluent.launch_fluent()
+    kwds = get_grpc_launcher_args_for_gh_runs()
+    session = pyfluent.launch_fluent(**kwds)
     session.scheme_eval.scheme_eval('(%py-exec "import grpc")')
     print(session.scheme_eval.scheme_eval('(%py-eval "grpc.__version__")'))
