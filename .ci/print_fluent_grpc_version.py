a291e139ccf3098895b80b5fabf9b84b622bc2c2
Mainak Kundu <mainak.kundu@ansys.com>
2026-06-08 16:37:08 -0400

    fix: CI

--- .ci/print_fluent_grpc_version.py
+++ .ci/print_fluent_grpc_version.py
@@ -4,6 +4,8 @@ import ansys.fluent.core as pyfluent
 from ansys.fluent.core.docker.utils import get_grpc_launcher_args_for_gh_runs
 
 if __name__ == "__main__":
+    pyfluent.set_console_logging_level("ERROR")
+    pyfluent.warning.disable()
     kwds = get_grpc_launcher_args_for_gh_runs()
     session = pyfluent.launch_fluent(**kwds)
     session.scheme.eval('(%py-exec "import grpc")')
