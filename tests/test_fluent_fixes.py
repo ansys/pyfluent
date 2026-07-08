8c0687e7998657c4a758de64f04e45bc18c62484
Prithwish Mukherjee <prmukher@synopsys.com>
2026-07-08 15:34:04 +0530

    Update year.

--- tests/test_fluent_fixes.py
+++ tests/test_fluent_fixes.py
@@ -1,5 +1,6 @@
-# Copyright (C) 2023 - 2026 Synopsys, Inc. and ANSYS, Inc. All rights reserved.
+# Copyright (C) 2021 - 2026 Synopsys, Inc. and ANSYS, Inc. All rights reserved.
 # SPDX-License-Identifier: MIT
+#
 #
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
