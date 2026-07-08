e72bc940d0b071e59c27065cf58242c7e0c94c86
Prithwish Mukherjee <109645853+prmukherj@users.noreply.github.com>
2026-07-08 17:40:40 +0530

    chore: Update license header via upgrading pre-commit hook. (#5240)
    As title says.
    
    ---------
    
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>

--- tests/test_launcher_remote.py
+++ tests/test_launcher_remote.py
@@ -1,5 +1,7 @@
 ﻿# Copyright (C) 2021 - 2026 ANSYS, Inc. and/or its affiliates.
 
+# Copyright (C) 2023 - 2026 Synopsys, Inc. and ANSYS, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
 # SPDX-License-Identifier: MIT
 #
 #
