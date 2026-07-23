1f59b8394ebbab9bde91fe0affebed65561ef6e5
Prithwish Mukherjee <prmukher@synopsys.com>
2026-07-23 16:00:00 +0530

    doc updates.

--- src/ansys/fluent/core/services/abstract_monitors.py
+++ src/ansys/fluent/core/services/abstract_monitors.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""Abstract monitor wrapper."""
+"""Abstract monitors wrapper."""
 
 from abc import ABC, abstractmethod
 
