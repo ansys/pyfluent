a55c0bbd7f8d3c340ea68f5df58be69b8cd85580
Prithwish Mukherjee <prmukher@synopsys.com>
2026-08-21 12:55:22 +0530

    Fix FileSession.

--- src/ansys/fluent/core/file_session.py
+++ src/ansys/fluent/core/file_session.py
@@ -28,7 +28,6 @@ import warnings
 from deprecated.sphinx import deprecated
 import numpy as np
 
-from ansys.api.fluent.v1.field_data_pb2 import DataLocation
 from ansys.fluent.core import PyFluentDeprecationWarning
 from ansys.fluent.core.fields.field_data_interfaces import (
     BaseFieldInfo,
@@ -152,14 +151,7 @@ class BatchFieldData:
         scalar_field_data = self.data[
             (
                 ("type", "scalar-field"),
-                (
-                    "dataLocation",
-                    (
-                        DataLocation.DATA_LOCATION_NODES
-                        if node_value
-                        else DataLocation.DATA_LOCATION_ELEMENTS
-                    ),
-                ),
+                ("dataLocation", 1 if node_value else 0),
                 ("boundaryValues", boundary_value),
             )
         ]
@@ -605,14 +597,7 @@ class Batch(FieldBatch):
         for batch in self._scalar_field_batches:
             scalar_field_tag = (
                 ("type", "scalar-field"),
-                (
-                    "dataLocation",
-                    (
-                        DataLocation.DATA_LOCATION_NODES
-                        if batch.node_value
-                        else DataLocation.DATA_LOCATION_ELEMENTS
-                    ),
-                ),
+                ("dataLocation", 1 if batch.node_value else 0),
                 ("boundaryValues", batch.boundary_value),
             )
             field_data_surface = field_data.setdefault(scalar_field_tag, {})
