a732ef0768a34ab9020d066e52c3e0250d247b57
Prithwish Mukherjee <prmukher@synopsys.com>
2026-08-31 18:56:44 +0530

    Added test.

--- tests/test_lispy.py
+++ tests/test_lispy.py
@@ -44,6 +44,11 @@ scm_pys = (
     ("(x 1)", ["x", 1]),
     ('(x . "1.0 [m/s]")', ("x", '"1.0 [m/s]"')),
     ("(define x 1)", ["define", "x", 1]),
+)
+
+extra_scm_pys = (
+    ("(define x)", ["define", "x", None]),
+    ('(define "x")', []),
     # A line nested within a multi-line buffer closes one string
     # and opens another. This results in an even quote count for that specific
     # line alone, but the cumulative buffer count remains odd.
@@ -55,13 +60,7 @@ scm_pys = (
     (
         '("string1\n string2"\n "string3\n string4")',
         ['"string1\n string2"', '"string3\n string4"'],
-        '("string1\n string2" "string3\n string4")',
     ),
-)
-
-extra_scm_pys = (
-    ("(define x)", ["define", "x", None]),
-    ('(define "x")', []),
 )
 
 
