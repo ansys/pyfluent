a0a2ba89a6524b6b3d05080ab8a87d873a46b4cd
mayankansys <mayank.kumar@ansys.com>
2026-09-08 16:06:00 +0530

    REST : Removed the lightweight name & kept it as like it was previous

--- tests/conftest.py
+++ tests/conftest.py
@@ -412,7 +412,7 @@ def solver_session_grpc_rest(request):
 
 
 @pytest.fixture
-def mixing_elbow_grpc_rest_lightweight_session(solver_session_grpc_rest):
+def mixing_elbow_settings_session_grpc_rest(solver_session_grpc_rest):
     solver = solver_session_grpc_rest
     case_name = download_file("mixing_elbow.cas.h5", "pyfluent/mixing_elbow")
     solver.settings.file.read(
@@ -437,18 +437,6 @@ def mixing_elbow_case_session_grpc_rest(solver_session_grpc_rest):
     solver = solver_session_grpc_rest
     case_name = download_file("mixing_elbow.cas.h5", "pyfluent/mixing_elbow")
     solver.settings.file.read(file_type="case", file_name=case_name)
-    return solver
-
-
-@pytest.fixture
-def mixing_elbow_settings_session_grpc_rest(solver_session_grpc_rest):
-    solver = solver_session_grpc_rest
-    case_name = download_file("mixing_elbow.cas.h5", "pyfluent/mixing_elbow")
-    solver.settings.file.read(
-        file_type="case",
-        file_name=case_name,
-        lightweight_setup=True,
-    )
     return solver
 
 
