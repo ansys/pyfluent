c92ada9c0156727efc50e0fddfb6cbcf9913e7bf
mayankansys <mayank.kumar@ansys.com>
2026-09-08 12:30:10 +0530

    CI: Add REST env vars (FLUENT_REST_URL, FLUENT_REST_TOKEN) to Unit Testing step

--- tests/conftest.py
+++ tests/conftest.py
@@ -382,11 +382,11 @@ def http_solver_session():
     """
     rest_url = os.getenv("FLUENT_REST_URL")
     rest_token = os.getenv("FLUENT_REST_TOKEN")
-    # if not rest_url or not rest_token:
-    #     pytest.skip(
-    #         "REST live server not configured. "
-    #         "Set FLUENT_REST_URL and FLUENT_REST_TOKEN environment variables."
-    #     )
+    if not rest_url or not rest_token:
+        pytest.skip(
+            "REST live server not configured. "
+            "Set FLUENT_REST_URL and FLUENT_REST_TOKEN environment variables."
+        )
     solver = Solver.from_http(url=rest_url, token=rest_token)
     yield solver
     solver.exit()
